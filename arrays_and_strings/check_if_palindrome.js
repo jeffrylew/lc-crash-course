61ec3d0f4a87e9ed963b0cc36ae3e4a05b057847
Jeffry <jeffryklew@gmail.com>
2022-12-08 20:10:29 -0500

    Implement arrays_and_strings/check_if_palindrome.js

---
+++ arrays_and_strings/check_if_palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkIfPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
