397c763f188a19bbc854bdf17674d65a66b448d4
Jeffry <jeffryklew@gmail.com>
2022-12-08 20:21:41 -0500

    Add type hints to check_if_palindrome.py

--- arrays_and_strings/check_if_palindrome.py
+++ arrays_and_strings/check_if_palindrome.py
@@ -1,10 +1,11 @@
-def check_if_palindrome(s):
+def check_if_palindrome(s: str) -> bool:
     left = 0
     right = len(s) - 1
 
     while left < right:
         if s[left] != s[right]:
             return False
+
         left += 1
         right -= 1
 
