0c47f120ca22a369d1190f893900aa60d06e5b9a
Jeffry <jeffryklew@gmail.com>
2022-12-10 09:44:56 -0500

    Add Doxygen comment and arrow function to arrays_and_strings/check_for_target.js

--- arrays_and_strings/check_for_target.js
+++ arrays_and_strings/check_for_target.js
@@ -1,3 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {boolean}
+ */
+
+/**
+ * Alternatively can use ES6 arrow function,
+ * let checkForTarget = (nums, target) => { ... }
+ */
 var checkForTarget = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
