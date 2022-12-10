672716ad9132fcf9ffd0e722251417df40a4b04d
Jeffry <jeffryklew@gmail.com>
2022-12-10 10:01:51 -0500

    Implement arrays_and_strings/combine.py

---
+++ arrays_and_strings/combine.py
@@ -0,0 +1,21 @@
+def combine(arr1: list[int], arr2: list[int]) -> list[int]:
+    ans = []
+    i = j = 0
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+    
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+    
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+    
+    return ans
