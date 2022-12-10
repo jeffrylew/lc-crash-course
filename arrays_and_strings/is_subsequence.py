e09231b89bce27fcd3c4116176815e5336c3c170
Jeffry <jeffryklew@gmail.com>
2022-12-10 10:26:21 -0500

    Implement arrays_and_strings/is_subsequence.py

---
+++ arrays_and_strings/is_subsequence.py
@@ -0,0 +1,8 @@
+def is_subsequence(s: str, t: str) -> bool:
+    i = j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    
+    return i == len(s)
