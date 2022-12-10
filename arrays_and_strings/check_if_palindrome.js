dfce45edcbc63134c685c9e1944fcec2cefcb5ca
Jeffry <jeffryklew@gmail.com>
2022-12-10 09:46:01 -0500

    Add arrow function to arrays_and_strings/check_if_palindrome.js

--- arrays_and_strings/check_if_palindrome.js
+++ arrays_and_strings/check_if_palindrome.js
@@ -2,6 +2,11 @@
  * @param {string} s
  * @return {boolean}
  */
+
+/**
+ * Alternatively can use ES6 arrow function,
+ * let checkIfPalindrome = (s) => { ... }
+ */
 var checkIfPalindrome = function(s) {
     let left = 0;
     let right = s.length - 1;
