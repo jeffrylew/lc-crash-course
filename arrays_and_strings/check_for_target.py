b6985da1a3638f81bee6bad14abf749d77afb6bf
Jeffry <jeffryklew@gmail.com>
2022-12-10 08:49:37 -0500

    Implement arrays_and_strings/check_for_target.py

---
+++ arrays_and_strings/check_for_target.py
@@ -0,0 +1,14 @@
+def check_for_target(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        curr = nums[left] + nums[right]
+        if curr == target:
+            return True
+        if curr > target:
+            right -= 1
+        else:
+            left += 1
+    
+    return False
