4a94e60578a112ab7ef5fe04948ad7c81da34ad5
Jeffry <jeffryklew@gmail.com>
2022-12-16 21:42:14 -0500

    Implement arrays_and_strings/find_length_of_binary_str.py

---
+++ arrays_and_strings/find_length_of_binary_str.py
@@ -0,0 +1,14 @@
+def find_length_of_binary_str(s):
+    left = curr = max_substr_length = 0
+    for right in range(len(s)):
+        if s[right] == "0":
+            curr += 1
+        
+        while curr > 1:
+            if s[left] == "0":
+                curr -= 1
+            left += 1
+        
+        max_substr_length = max(max_substr_length, right - left + 1)
+
+    return max_substr_length
