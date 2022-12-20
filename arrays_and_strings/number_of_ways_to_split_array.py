eec209e607e40442563f473e7d59c0bee97248a0
Jeffry <jeffryklew@gmail.com>
2022-12-19 21:57:41 -0500

    Implement space complexity O(1) solution in number_of_ways_to_split_array.py

--- arrays_and_strings/number_of_ways_to_split_array.py
+++ arrays_and_strings/number_of_ways_to_split_array.py
@@ -11,3 +11,15 @@ def waysToSplitArray1(nums: List[int]) -> int:
             ans += 1
     
     return ans
+
+def waysToSplitArray2(nums: List[int]) -> int:
+    ans = left_section = 0
+    total = sum(nums)
+
+    for i in range(len(nums) - 1):
+        left_section += nums[i]
+        right_section = total - left_section
+        if left_section >= right_section:
+            ans += 1
+
+    return ans
