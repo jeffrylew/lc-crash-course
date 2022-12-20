2263e5533ea2525dcd97a64cfc11f9c9824bb394
Jeffry <jeffryklew@gmail.com>
2022-12-19 21:48:54 -0500

    Implement space complexity O(1) solution in number_of_ways_to_split_array.js

--- arrays_and_strings/number_of_ways_to_split_array.js
+++ arrays_and_strings/number_of_ways_to_split_array.js
@@ -23,3 +23,24 @@ var waysToSplitArray1 = function(nums) {
 
     return ans;
 }
+
+var waysToSplitArray2 = function(nums) {
+    let ans = 0, leftSection = 0, total = 0;
+
+    for (const num of nums)
+    {
+        total += num;
+    }
+
+    for (let i = 0; i < nums.length - 1; i++)
+    {
+        leftSection += nums[i];
+        const rightSection = total - leftSection;
+        if (leftSection >= rightSection)
+        {
+            ans++;
+        }
+    }
+
+    return ans;
+}
