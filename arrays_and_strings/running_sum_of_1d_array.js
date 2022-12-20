ceef9104e3ec97895eefeb0d48a917ca0274a780
Jeffry <jeffryklew@gmail.com>
2022-12-19 22:38:38 -0500

    Implement running_sum_of_1d_array.js

---
+++ arrays_and_strings/running_sum_of_1d_array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @returns {number[]}
+ */
+var runningSum = function(nums) {
+    for (let i = 1; i < nums.length; i++)
+    {
+        nums[i] += nums[i - 1];
+    }
+
+    return nums;
+}
