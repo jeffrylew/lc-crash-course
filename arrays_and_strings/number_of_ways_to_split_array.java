3a75e5d4d93cf4498db67af93e1f75b264f99f7d
Jeffry <jeffryklew@gmail.com>
2022-12-19 21:38:46 -0500

    Implement space complexity O(1) solution in number_of_ways_to_split_array.java

--- arrays_and_strings/number_of_ways_to_split_array.java
+++ arrays_and_strings/number_of_ways_to_split_array.java
@@ -21,3 +21,27 @@ public int waysToSplitArray1(int[] nums)
 
     return ans;
 }
+
+public int waysToSplitArray2(int[] nums)
+{
+    int  ans = 0;
+    long leftSection = 0;
+    long total = 0;
+
+    for (int num : nums)
+    {
+        total += num;
+    }
+
+    for (int i = 0; i < nums.length - 1; i++)
+    {
+        leftSection += nums[i];
+        const long rightSection = total - leftSection;
+        if (leftSection >= rightSection)
+        {
+            ans++;
+        }
+    }
+
+    return ans;
+}
