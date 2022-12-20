09e3e0c435f4451865546d1db3278affa04865c6
Jeffry <jeffryklew@gmail.com>
2022-12-19 19:01:47 -0500

    Implement maximum_consecutive_ones_3.py

---
+++ arrays_and_strings/maximum_consecutive_ones_3.py
@@ -0,0 +1,12 @@
+def longestOnes(nums: List[int], k: int) -> int:
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            k -= 1
+        
+        if k < 0:
+            if nums[left] == 0:
+                k += 1
+            left += 1
+    
+    return right - left + 1
