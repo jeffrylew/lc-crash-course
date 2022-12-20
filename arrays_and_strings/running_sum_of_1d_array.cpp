8e050850543d69a7b19400bb06a4bcf01221727a
Jeffry <jeffryklew@gmail.com>
2022-12-19 22:32:35 -0500

    Implement discussion solution in running_sum_of_1d_array.cpp

--- arrays_and_strings/running_sum_of_1d_array.cpp
+++ arrays_and_strings/running_sum_of_1d_array.cpp
@@ -5,6 +5,12 @@
 
 static std::vector<int> runningSumFA(std::vector<int> nums)
 {
+    //! @details https://leetcode.com/problems/running-sum-of-1d-array/
+    //!          solutions/1136761/official-solution/
+    //!
+    //!          Time complexity O(N) where N = nums.size()
+    //!          Space complexity O(1), output array space not considered
+
     std::vector<int> prefix {};
     prefix.reserve(nums.size());
     prefix.push_back(nums[0]);
@@ -17,6 +23,16 @@ static std::vector<int> runningSumFA(std::vector<int> nums)
     return prefix;
 }
 
+static std::vector<int> runningSumDS(std::vector<int> nums)
+{
+    for (int i = 1; i < nums.size(); i++)
+    {
+        //! Result at index i is sum of result at i-1 and element at i
+        nums[i] += nums[i - 1];
+    }
+    return nums;
+}
+
 TEST(RunningSumTest, SampleTest)
 {
     const std::vector<int> expected_output {1, 3, 6, 10};
@@ -26,4 +42,10 @@ TEST(RunningSumTest, SampleTest)
     EXPECT_TRUE(std::equal(expected_output.cbegin(),
                            expected_output.cend(),
                            resultFA.cbegin()));
+
+    const auto resultDS = runningSumDS({1, 2, 3, 4});
+
+    EXPECT_TRUE(std::equal(expected_output.cbegin(),
+                           expected_output.cend(),
+                           resultDS.cbegin()));
 }
