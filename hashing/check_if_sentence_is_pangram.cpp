b35fd1a14918cee6bf8ad7d0b6a867c7f3e3f453
Jeffry <jeffryklew@gmail.com>
2022-12-22 17:15:30 -0500

    Add discussion solutions to hashing/check_if_sentence_is_pangram.cpp

--- hashing/check_if_sentence_is_pangram.cpp
+++ hashing/check_if_sentence_is_pangram.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <string>
+#include <unordered_set>
 
 static bool checkIfPangramFA(std::string sentence)
 {
@@ -19,7 +20,59 @@ static bool checkIfPangramFA(std::string sentence)
                         [](bool ele) -> bool { return ele == false; });
 }
 
+static bool checkIfPangramDS1(std::string sentence)
+{
+    //! @details Time complexity O(N) where N = sentence.size()
+    //!          Space complexity O(1) since there are 26 lowercase letters
+
+    //! Add every letter of sentence to hash set seen
+    std::unordered_set<char> seen(sentence.begin(), sentence.end());
+
+    //! If the size of seen is 26 then sentence is a pangram
+    return seen.size() == 26ULL;
+}
+
+static bool checkIfPangramDS2(std::string sentence)
+{
+    //! @details Map alphabet to bits, e.g. seen = 23 = (10111)_2 for a, b, c, e
+    //!          1 0 1 1 1
+    //!          e   c b a
+    //!          To indicate presence of e.g. 'f', get ASCII index 'f' - 'a' = 5
+    //!          Then left shift 1 by 5 bits: 1 << 5 = 32 = (100000)_2
+    //!          Use OR to combine:
+    //!                       0 1 0 1 1 1
+    //!                     + 1 0 0 0 0 0
+    //!          seen = 55 = (1 1 0 1 1 1)_2
+    //!          Finally, if all 26 letters are present then will have 26 bits
+    //!          of 1 in seen = [111...111]. seen + 1 = 1[000...000] = (1 << 26)
+    //!
+    //!          Time complexity O(N) to traverse over sentence once
+    //           Space complexity O(1) to update one number seen
+
+    //! Initialize seen = 0 since start with no letter
+    int seen {};
+
+    //! Iterate over sentence
+    for (const auto currChar : sentence)
+    {
+        //! Map each currChar to its index using its ASCII code
+        const auto mappedIndex = static_cast<int>(currChar - 'a');
+
+        //! currBit represents the bit for currChar
+        //! Left shifting by 1 equals multiplying by 2
+        const int currBit = 1 << mappedIndex;
+
+        //! Use OR operation
+        seen |= currBit;
+    }
+
+    //! Once finish iterating, check if seen contains 26 bits of 1
+    return seen + 1 == (1 << 26);
+}
+
 TEST(CheckIfPangramTest, SampleTest)
 {
     EXPECT_TRUE(checkIfPangramFA("thequickbrownfoxjumpsoverthelazydog"));
+    EXPECT_TRUE(checkIfPangramDS1("thequickbrownfoxjumpsoverthelazydog"));
+    EXPECT_TRUE(checkIfPangramDS2("thequickbrownfoxjumpsoverthelazydog"));
 }
