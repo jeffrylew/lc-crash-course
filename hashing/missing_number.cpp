0ed69e26238b0497f7f6f6f67d25c195e419cc3d
Jeffry <jeffryklew@gmail.com>
2022-12-24 17:57:45 -0500

    Implement first discussion solution in hashing/missing_number.cpp

--- hashing/missing_number.cpp
+++ hashing/missing_number.cpp
@@ -2,6 +2,8 @@
 
 #include <cmath>
 #include <cstdint>
+#include <tuple>
+#include <unordered_set>
 #include <vector>
 
 //! @brief First attempt solution using bit manipulation can't do 99+ shifts
@@ -86,8 +88,39 @@ static int missingNumberSA(std::vector<int> nums)
 
 } // static int missingNumberSA( ...
 
+//! @brief First discussion solution
+//! @param[in] nums Vector of n distinct numbers in range [0, n]
+//! @return The only number in the range that is missing from the input vector
+static int missingNumberDS1(std::vector<int> nums)
+{
+    //! @details https://leetcode.com/problems/missing-number/solutions/127673/
+    //!          official-solution/
+    //!          Time complexity O(N)
+    //!          Space complexity O(N)
+
+    std::unordered_set<int> numSet {};
+
+    for (const auto num : nums)
+    {
+        std::ignore = numSet.emplace(num);
+    }
+
+    const auto expectedNumCount = 1 + static_cast<int>(nums.size());
+    for (int number = 0; number < expectedNumCount; ++number)
+    {
+        if (numSet.count(number) == 0)
+        {
+            return number;
+        }
+    }
+
+    return -1;
+
+} // static int missingNumberDS1( ...
+
 TEST(MissingNumberTest, SampleTest)
 {
     EXPECT_EQ(2, missingNumberFA({3, 0, 1}));
     EXPECT_EQ(2, missingNumberSA({3, 0, 1}));
+    EXPECT_EQ(2, missingNumberDS1({3, 0, 1}));
 }
