f4f9b163626ca2b3d51ce05bf8bf1bbbb71b811a
Jeffry <jeffryklew@gmail.com>
2022-12-25 14:13:23 -0500

    Add big O analysis to hashing/counting_elements.cpp

--- hashing/counting_elements.cpp
+++ hashing/counting_elements.cpp
@@ -8,6 +8,14 @@
 //! @return Number of elements x where x + 1 is also in arr
 static int countElementsFA(std::vector<int> arr)
 {
+    //! @details Time complexity O(N). Creating set takes O(N) time and looping
+    //!          over N integers to check if x + 1 is in the set takes N * O(1).
+    //!          Overall, O(N) + N * O(1) = O(N) + O(N) = O(N)
+    //!          Space complexity O(N) for storing each unique integer from arr.
+    //!          In the worst case, all integers are unique. O(N) is an upper
+    //!          bound - if U is the number of unique integers in arr then the
+    //!          space complexity could be more accurately represented as O(U)
+
     int num_elements {};
 
     const std::unordered_set<int> unique_nums(arr.begin(), arr.end());
