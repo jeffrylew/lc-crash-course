cf5fdee16deb3949582c9e611e39643ce52168ed
Jeffry <jeffryklew@gmail.com>
2022-12-25 15:41:42 -0500

    Implement discussion solution in hashing/counting_elements.cpp

--- hashing/counting_elements.cpp
+++ hashing/counting_elements.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <unordered_set>
 #include <vector>
 
@@ -32,7 +33,39 @@ static int countElementsFA(std::vector<int> arr)
 
 } // static int countElementsFA( ...
 
+//! @brief Discussion solution using search with sorted array
+//! @param[in] arr Vector of integers
+//! @return Number of elements x where x + 1 is also in arr
+static int countElementsDS(std::vector<int> arr)
+{
+    //! @details Time complexity O(N log N) for std::sort. Single pass through
+    //!          arr has cost of O(N) so O(N log N) + O(N) = O(N log N)
+    //!          Space complexity O(1)
+
+    std::sort(arr.begin(), arr.end());
+
+    int count {};
+    int runLength {1};
+
+    for (int i = 1; i < static_cast<int>(arr.size()); ++i)
+    {
+        if (arr[i - 1] != arr[i])
+        {
+            if (arr[i - 1] + 1 == arr[i])
+            {
+                count += runLength;
+            }
+            runLength = 0;
+        }
+        ++runLength;
+    }
+
+    return count;
+
+} // static int countElementsDS( ...
+
 TEST(CountElementsTest, SampleTest)
 {
     EXPECT_EQ(2, countElementsFA({1, 2, 3}));
+    EXPECT_EQ(2, countElementsDS({1, 2, 3}));
 }
