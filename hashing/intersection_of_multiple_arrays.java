a2eccb6b8d559e3e522f5f96e67545f541576b2c
Jeffry <jeffryklew@gmail.com>
2022-12-26 11:04:55 -0500

    Implement hashing/intersection_of_multiple_arrays.java

---
+++ hashing/intersection_of_multiple_arrays.java
@@ -0,0 +1,24 @@
+public List<Integer> intersection(int[][] nums)
+{
+    Map<Integer, Integer> counts = new HashMap<>();
+    for (int[] arr : nums)
+    {
+        for (int x : arr)
+        {
+            counts.put(x, counts.getOrDefault(x, 0) + 1);
+        }
+    }
+    
+    int n = nums.length;
+    List<Integer> ans = new ArrayList<>();
+    for (int key : counts.keySet())
+    {
+        if (counts.get(key) == n)
+        {
+            ans.add(key);
+        }
+    }
+
+    Collections.sort(ans);
+    return ans;
+}
