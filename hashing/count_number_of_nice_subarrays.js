97af54f79fb04149509d23c21e32aed6ebcaadff
Jeffry <jeffryklew@gmail.com>
2022-12-29 17:52:19 -0500

    Implement hashing/count_number_of_nice_subarrays.js

---
+++ hashing/count_number_of_nice_subarrays.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number}
+ */
+var numberOfSubarrays = function(nums, k) {
+    let counts = new Map();
+    counts.set(0, 1);
+
+    let ans = 0;
+    let curr = 0;
+
+    for (const num of nums)
+    {
+        curr += num % 2;
+        ans += counts.get(curr - k) || 0;
+        counts.set(curr, (counts.get(curr) || 0) + 1);
+    }
+
+    return ans;
+}
