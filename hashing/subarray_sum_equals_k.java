8ecc84a161a06215d086adba0da31f5f1daf4203
Jeffry <jeffryklew@gmail.com>
2022-12-28 20:24:46 -0500

    Implement hashing/subarray_sum_equals_k.java

---
+++ hashing/subarray_sum_equals_k.java
@@ -0,0 +1,17 @@
+public int subarraySum(int[] nums, int k)
+{
+    Map<Integer, Integer> counts = new HashMap<>();
+    counts.put(0, 1);
+
+    int ans = 0;
+    int curr = 0;
+
+    for (int num : nums)
+    {
+        curr += num;
+        ans += counts.getOrDefault(curr - k, 0);
+        counts.put(curr, counts.getOrDefault(curr, 0) + 1);
+    }
+
+    return ans;
+}
