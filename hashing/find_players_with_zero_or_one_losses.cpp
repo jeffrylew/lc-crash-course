56a268468ba423b398b542d6814bfe0b8467ac4a
Jeffry <jeffryklew@gmail.com>
2022-12-29 23:44:41 -0500

    Implement first discussion solution in hashing/find_players_with_zero_or_one_losses.cpp

--- hashing/find_players_with_zero_or_one_losses.cpp
+++ hashing/find_players_with_zero_or_one_losses.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <tuple>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 //! @brief First attempt solution to find players with zero or one losses
@@ -40,6 +42,53 @@ static std::vector<std::vector<int>> findWinnersFA(
 
 } // static std::vector<std::vector<int>> findWinnersFA( ...
 
+//! @brief First discussion solution to find players with zero or one losses
+//! @param[in] matches Vector of matches
+//! @return List of players who have not lost and list of players that lost once
+static std::vector<std::vector<int>> findWinnersDS1(
+    std::vector<std::vector<int>> matches)
+{
+    //! @details https://leetcode.com/problems/
+    //!          find-players-with-zero-or-one-losses/solutions/2655744/
+    //!          find-players-with-zero-or-one-losses/
+    //!
+    //!          Time complexity O(N log N), N = matches.size(). Iteration over
+    //!          matches takes O(N) and need to store/sort players in two arrays
+    //!          where worst case is O(N) players in them.
+    //!          Space complexity O(N) for hash set/map with O(N) players
+
+    std::unordered_set<int> seen {};
+    std::unordered_map<int, int> lossesCount {};
+
+    for (const auto& match : matches)
+    {
+        const int winner = match[0];
+        const int loser  = match[1];
+        std::ignore = seen.insert(winner);
+        std::ignore = seen.insert(loser);
+        ++lossesCount[loser];
+    }
+
+    //! Add players with 0 or 1 loss to corresponding list
+    std::vector<std::vector<int>> answer(2ULL, std::vector<int>{});
+    for (const auto player : seen)
+    {
+        if (lossesCount.find(player) == lossesCount.end())
+        {
+            answer[0].push_back(player);
+        }
+        else if (lossesCount[player] == 1)
+        {
+            answer[1].push_back(player);
+        }
+    }
+
+    std::sort(answer[0].begin(), answer[0].end());
+    std::sort(answer[1].begin(), answer[1].end());
+    return answer;
+
+} // static std::vector<std::vector<int>> findWinnersDS1( ...
+
 TEST(FindWinnersTest, SampleTest)
 {
     const std::vector<std::vector<int>> input {{1, 3},
@@ -64,4 +113,13 @@ TEST(FindWinnersTest, SampleTest)
     EXPECT_TRUE(std::equal(expected_output2.cbegin(),
                            expected_output2.cend(),
                            resultFA.back().cbegin()));
+    
+    const auto resultDS1 = findWinnersDS1(input);
+
+    EXPECT_TRUE(std::equal(expected_output1.cbegin(),
+                           expected_output1.cend(),
+                           resultDS1.front().cbegin()));
+    EXPECT_TRUE(std::equal(expected_output2.cbegin(),
+                           expected_output2.cend(),
+                           resultDS1.back().cbegin()));
 }
