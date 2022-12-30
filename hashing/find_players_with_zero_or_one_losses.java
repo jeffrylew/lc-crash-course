397e20af75f3184464e8acaf1cb0f1e24a7e9dca
Jeffry <jeffryklew@gmail.com>
2022-12-30 15:27:43 -0500

    Implement find_players_with_zero_or_one_losses.java

---
+++ hashing/find_players_with_zero_or_one_losses.java
@@ -0,0 +1,30 @@
+public List<List<Integer>> findWinners(int[][] matches)
+{
+    Map<Integer, Integer> lossesCount = new HashMap<>();
+    for (int[] match : matches)
+    {
+        int winner = match[0];
+        int loser  = match[1];
+        lossesCount.put(winner, lossesCount.getOrDefault(winner, 0));
+        lossesCount.put(loser, lossesCount.getOrDefault(loser, 0) + 1);
+    }
+
+    List<List<Integer>> answer = new Arrays.asList(new ArrayList<>(),
+                                                   new ArrayList<>());
+    for (Integer player : lossesCount.keySet())
+    {
+        if (lossesCount.get(player) == 0)
+        {
+            answer.get(0).add(player);
+        }
+        else if (lossesCount.get(player) == 1)
+        {
+            answer.get(1).add(player);
+        }
+    }
+
+    Collections.sort(answer.get(0));
+    Collections.sort(answer.get(1));
+    
+    return answer;
+}
