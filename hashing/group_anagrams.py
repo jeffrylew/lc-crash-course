66f9d0dc83ec98dd4c1fe89cc9f36ef2c8d750fc
Jeffry <jeffryklew@gmail.com>
2022-12-31 15:36:40 -0500

    Implement hashing/group_anagrams.py

---
+++ hashing/group_anagrams.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    groups = defaultdict(list)
+    for s in strs:
+        key = "".join(sorted(s))
+        groups[key].append(s)
+    
+    return groups.values()
