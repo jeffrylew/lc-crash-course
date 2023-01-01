d2f76dc10ddf63eaf24cf8be68ced3d096caa5e0
Jeffry <jeffryklew@gmail.com>
2022-12-31 21:07:32 -0500

    Implement solution 2 in minimum_consecutive_cards_to_pick_up.java

--- hashing/minimum_consecutive_cards_to_pick_up.java
+++ hashing/minimum_consecutive_cards_to_pick_up.java
@@ -28,3 +28,28 @@ public int minimumCardPickup1(int[] cards)
 
     return ans;
 }
+
+public int minimumCardPickup2(int[] cards)
+{
+    Map<Integer, Integer> dic = new HashMap<>();
+    
+    int ans = Integer.MAX_VALUE;
+
+    for (int i = 0; i < cards.length; i++)
+    {
+        int num = cards[i];
+        if (dic.containsKey(num))
+        {
+            ans = Math.min(ans, i - dic.get(num) + 1);
+        }
+
+        dic.put(num, i);
+    }
+
+    if (ans == Integer.MAX_VALUE)
+    {
+        return -1;
+    }
+
+    return ans;
+}
