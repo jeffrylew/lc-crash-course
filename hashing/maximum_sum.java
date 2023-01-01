7331eb1a67f6279a563f3fd03a3ed00b5e4b0a3c
Jeffry <jeffryklew@gmail.com>
2023-01-01 13:12:16 -0500

    Implement solution 1 in hashing/maximum_sum.java

---
+++ hashing/maximum_sum.java
@@ -0,0 +1,38 @@
+public int getDigitSum(int num)
+{
+    int digitSum = 0;
+    while (num > 0)
+    {
+        digitSum += num % 10;
+        num /= 10;
+    }
+
+    return digitSum;
+}
+
+public int maximumSum1(int[] nums)
+{
+    Map<Integer, List<Integer>> dic = new HashMap<>();
+    for (int num : nums)
+    {
+        int digitSum = getDigitSum(num);
+        if (!dic.containsKey(digitSum))
+        {
+            dic.put(digitSum, new ArrayList<Integer>());
+        }
+        dic.get(digitSum).add(num);
+    }
+
+    int ans = -1;
+    for (int key : dic.keySet())
+    {
+        List<Integer> curr = dic.get(key);
+        if (curr.size() > 1)
+        {
+            Collections.sort(curr, Collections.reverseOrder());
+            ans = Math.max(ans, curr.get(0) + curr.get(1));
+        }
+    }
+
+    return ans;
+}
