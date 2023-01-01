a6540c8cec89ecf93c6998450c69269ef288b480
Jeffry <jeffryklew@gmail.com>
2022-12-31 22:20:40 -0500

    Implement solution 2 in minimum_consecutive_cards_to_pick_up.py

--- hashing/minimum_consecutive_cards_to_pick_up.py
+++ hashing/minimum_consecutive_cards_to_pick_up.py
@@ -13,3 +13,16 @@ def minimumCardPickup1(cards: List[int]) -> int:
             ans = min(ans, arr[i + 1] - arr[i] + 1)
     
     return ans if ans < float("inf") else -1
+
+def minimumCardPickup2(cards: List[int]) -> int:
+    dic = defaultdict(int)
+    ans = float("inf")
+
+    for i in range(len(cards)):
+        num = cards[i]
+        if num in dic:
+            ans = min(ans, i - dic[num] + 1)
+        
+        dic[num] = i
+    
+    return ans if ans < float("inf") else -1
