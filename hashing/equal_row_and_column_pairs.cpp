34fc4dd2ecf217c85ada73e22bd70ce52ad3d0b4
Jeffry <jeffryklew@gmail.com>
2023-01-02 16:41:38 -0500

    Implement discussion solution in equal_row_and_column_pairs.cpp

--- hashing/equal_row_and_column_pairs.cpp
+++ hashing/equal_row_and_column_pairs.cpp
@@ -3,7 +3,7 @@
 #include <map>
 #include <vector>
 
-//! @brief Get number of pairs (R, c) where row R and col C are equal
+//! @brief Get number of pairs (R, C) where row R and col C are equal
 //! @param[in] grid N by N grid of integers
 //! @return Number of pairs (R, C)
 static int equalPairs(std::vector<std::vector<int>> grid)
@@ -46,9 +46,47 @@ static int equalPairs(std::vector<std::vector<int>> grid)
 
 } // static int equalPairs( ...
 
+//! @brief Discussion solution to get number of pairs (R, C)
+//! @param[in] grid N by N grid of integers
+//! @return Number of pairs (R, C) where row R and col C are equal
+static int equalPairsDS(std::vector<std::vector<int>> grid)
+{
+    //! @details https://leetcode.com/problems/equal-row-and-column-pairs/
+    //!          solutions/2324872/100-fast-easy-understanding/
+
+    int num_equal_pairs {};
+
+    std::map<std::vector<int>, int> row_dic {};
+
+    //! Store each row frequency in the map
+    for (const auto& row : grid)
+    {
+        ++row_dic[row];
+    }
+
+    for (int col = 0; col < static_cast<int>(grid[0].size()); ++col)
+    {
+        std::vector<int> col_vec {};
+        for (int row = 0; row < static_cast<int>(grid.size()); ++row)
+        {
+            col_vec.push_back(grid[row][col]);
+        }
+
+        //! Add number of times that column appeared as a row
+        num_equal_pairs += row_dic[col_vec];
+    }
+
+    return num_equal_pairs;
+
+} // static int equalPairsDS( ...
+
 TEST(EqualPairsTest, SampleTest)
 {
     EXPECT_EQ(1, equalPairs({{3, 2, 1},
                              {1, 7, 6},
                              {2, 7, 7}}));
+    
+    EXPECT_EQ(1, equalPairsDS({{3, 2, 1},
+                               {1, 7, 6},
+                               {2, 7, 7}}));
 }
