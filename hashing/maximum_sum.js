f07ef771a7dd10b5d5796c16c140af79948fd716
Jeffry <jeffryklew@gmail.com>
2023-01-02 13:58:29 -0500

    Implement solution 2 in hashing/maximum_sum.js

--- hashing/maximum_sum.js
+++ hashing/maximum_sum.js
@@ -37,3 +37,33 @@ var maximumSum1 = function(nums) {
 
     return ans;
 }
+
+var maximumSum2 = function(nums) {
+    let getDigitSum = num => {
+        let digitSum = 0;
+        while (num > 0)
+        {
+            digitSum += num % 10;
+            num = Math.floor(num / 10);
+        }
+
+        return digitSum;
+    }
+
+    let dic = new Map();
+    let ans = -1;
+
+    for (const num of nums)
+    {
+        const digitSum = getDigitSum(num);
+        if (dic.has(digitSum))
+        {
+            ans = Math.max(ans, num + dic.get(digitSum));
+        }
+
+        dic.set(digitSum, Math.max(dic.get(digitSum) || 0,
+                                   num));
+    }
+
+    return ans;
+}
