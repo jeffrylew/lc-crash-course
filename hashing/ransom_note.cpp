49697534320c4bae202f55313131076ef85d9360
Jeffry <jeffryklew@gmail.com>
2023-01-03 13:57:24 -0500

    Add big O analysis and minor optimization to hashing/ransom_note.cpp

--- hashing/ransom_note.cpp
+++ hashing/ransom_note.cpp
@@ -5,6 +5,24 @@
 
 static bool canConstruct(std::string ransomNote, std::string magazine)
 {
+    //! @details https://leetcode.com/problems/ransom-note/solutions/540284/
+    //!          official-solution/
+    //!
+    //!          Let M = magazine.size() and N = ransomNote.size()
+    //!          Let K = number of unique characters across the ransom note and
+    //!          magazine (won't be more than 26)
+    //!          Time complexity O(M), if M < N then return false. The worst
+    //!          case occurs when M >= N. Creating hash map of counts for
+    //!          magazine is O(M). Iterating over ransom note is O(N) but M >= N
+    //!          so this simplifies to O(M)
+    //!          Space complexity O(K) or O(1). K is never more than 26, which
+    //!          is a constant.
+
+    if (ransomNote.size() > magazine.size())
+    {
+        return false;
+    }
+
     std::unordered_map<char, int> magazine_dic {};
     for (const char c : magazine)
     {
