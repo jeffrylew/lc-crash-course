c3bf4fa27c3201fd187bc20dd1aa9454059d37cb
Jeffry <jeffryklew@gmail.com>
2023-01-05 16:27:57 -0500

    Implement discussion solution 2 in hashing/length_of_longest_substring.cpp

--- hashing/length_of_longest_substring.cpp
+++ hashing/length_of_longest_substring.cpp
@@ -73,8 +73,44 @@ static int lengthOfLongestSubstringDS1(std::string s)
 
 } // static int lengthOfLongestSubstringDS1( ...
 
+//! @brief Optimized discussion solution to find length of longest substring
+//! @param[in] s String to check
+//! @return Length of longest substring without repeating characters
+static int lengthOfLongestSubstringDS2(std::string s)
+{
+    //! @details https://leetcode.com/problems/
+    //!          longest-substring-without-repeating-characters/solutions/
+    //!          127839/official-solution/
+    //!
+    //!          Time complexity O(N) where N = s.size(). Index left iterates
+    //!          N times.
+    //!          Space complexity O(min(M, N)), need O(K) space for the sliding
+    //!          window where K is size of hash map. K is upper bounded by
+    //!          N = s.size() and M = size of the charset/alphabet
+
+    int substr_len {};
+    int left {};
+
+    std::unordered_map<char, int> char_idx {};
+
+    for (int right = 0; right < static_cast<int>(s.size()); ++right)
+    {
+        if (char_idx[s[right]] > 0)
+        {
+            left = std::max(char_idx[s[right]], left);
+        }
+
+        substr_len        = std::max(substr_len, right - left + 1);
+        char_idx[s[right]] = right + 1;
+    }
+
+    return substr_len;
+
+} // static int lengthOfLongestSubstringDS2( ...
+
 TEST(LengthOfLongestSubstringTest, SampleTest)
 {
     EXPECT_EQ(3, lengthOfLongestSubstringFA("abcabcbb"));
     EXPECT_EQ(3, lengthOfLongestSubstringDS1("abcabcbb"));
+    EXPECT_EQ(3, lengthOfLongestSubstringDS2("abcabcbb"));
 }
