c11fdf39f62cc5596b4ce1f52e8219f240aa7880
Jeffry <jeffryklew@gmail.com>
2023-01-09 19:13:44 -0500

    Add set solution to linked_lists/has_cycle.py

--- linked_lists/has_cycle.py
+++ linked_lists/has_cycle.py
@@ -12,3 +12,14 @@ def hasCycle(head: Optional[ListNode]) -> bool:
             return True
     
     return False
+
+def hasCycleSet(head: Optional[ListNode]) -> bool:
+    seen = set()
+    while head:
+        if head in seen:
+            return True
+        
+        seen.add(head)
+        head = head.next
+    
+    return False
