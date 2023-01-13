bfe1407f8d523f61f75dd2c184ed5e23b52670e5
Jeffry <jeffryklew@gmail.com>
2023-01-13 08:26:19 -0500

    Implement discussion solution in remove_duplicates_from_sorted_list.cpp

--- linked_lists/remove_duplicates_from_sorted_list.cpp
+++ linked_lists/remove_duplicates_from_sorted_list.cpp
@@ -34,6 +34,32 @@ static ListNode* deleteDuplicates(ListNode* head)
 
 } // static ListNode* deleteDuplicates( ...
 
+//! @brief Discussion solution to delete all duplicates in sorted linked list
+//! @param[in] head Pointer to head of sorted linked list
+//! @return Pointer to head of sorted linked list without duplicates
+static ListNode* deleteDuplicatesDS(ListNode* head)
+{
+    //! @details https://leetcode.com/problems/
+    //!          remove-duplicates-from-sorted-list/solutions/127847/
+    //!          official-solution/
+    
+    auto curr = head;
+    while (curr != nullptr && curr->next != nullptr)
+    {
+        if (curr->next->val == curr->val)
+        {
+            curr->next = curr->next->next;
+        }
+        else
+        {
+            curr = curr->next;
+        }
+    }
+
+    return head;
+
+} // static ListNode* deleteDuplicatesDS( ...
+
 TEST(DeleteDuplicatesTest, SampleTest)
 {
     ListNode one {1};
@@ -45,6 +71,22 @@ TEST(DeleteDuplicatesTest, SampleTest)
 
     auto head     = &one;
     auto new_head = deleteDuplicates(head);
+    EXPECT_EQ(one.val, new_head->val);
+    new_head = new_head->next;
+    EXPECT_EQ(two.val, new_head->val);
+}
+
+TEST(DeleteDuplicatesTest, DSTest)
+{
+    ListNode one {1};
+    ListNode one_duplicated {1};
+    ListNode two {2};
+
+    one.next            = &one_duplicated;
+    one_duplicated.next = &two;
+
+    auto head     = &one;
+    auto new_head = deleteDuplicatesDS(head);
     EXPECT_EQ(one.val, new_head->val);
     new_head = new_head->next;
     EXPECT_EQ(two.val, new_head->val);
