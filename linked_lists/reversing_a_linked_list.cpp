eab63281fe73fc5c71599e1ab6994fb1de486bf0
Jeffry <jeffryklew@gmail.com>
2023-01-16 20:15:05 -0500

    Add big O analysis to reversing_a_linked_list.cpp

--- linked_lists/reversing_a_linked_list.cpp
+++ linked_lists/reversing_a_linked_list.cpp
@@ -7,8 +7,18 @@ struct ListNode
     ListNode (int val) : val {val} {}
 };
 
+//! @brief Reverse a linked list
+//! @param[in] head Pointer to head of linked list
+//! @return Pointer to head of reversed linked list
 static ListNode* reverseList(ListNode* head)
 {
+    //! @details https://leetcode.com/explore/interview/card/
+    //!          leetcodes-interview-crash-course-data-structures-and-algorithms
+    //!          /704/linked-lists/4600/
+    //!
+    //!          Time complexity O(N) where N = number of nodes in linked list
+    //!          Space complexity O(1)
+
     ListNode* prev = nullptr;
     ListNode* curr = head;
     while (curr != nullptr)
