f3f5de43dea0db2ba70f02bf6a267d090f155a54
Jeffry <jeffryklew@gmail.com>
2023-02-14 21:19:57 -0500

    Implement stacks_and_queues/remove_all_adjacent_duplicates.py

---
+++ stacks_and_queues/remove_all_adjacent_duplicates.py
@@ -0,0 +1,9 @@
+def removeDuplicates(s: str) -> str:
+    stack = []
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    
+    return "".join(stack)
