d788a154fb969d5af150575d588fa00fd909b493
Jeffry <jeffryklew@gmail.com>
2023-02-16 17:32:07 -0500

    Implement stacks_and_queues/backspace_string_compare.java

---
+++ stacks_and_queues/backspace_string_compare.java
@@ -0,0 +1,22 @@
+public String build(String s)
+{
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray())
+    {
+        if (c != '#')
+        {
+            stack.push(c);
+        }
+        else if (!stack.empty())
+        {
+            stack.pop();
+        }
+    }
+
+    return String.valueOf(stack);
+}
+
+public boolean backspaceCompare(String s, String t)
+{
+    return build(s).equals(build(t));
+}
