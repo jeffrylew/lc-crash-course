3f80df6976480c45de9a53505e589ba573e549c6
Jeffry <jeffryklew@gmail.com>
2023-02-28 20:40:16 -0500

    Implement stacks_and_queues/queue_example.java

---
+++ stacks_and_queues/queue_example.java
@@ -0,0 +1,19 @@
+public static void main(String[] args)
+{
+    Queue<Integer> queue = new LinkedList<>();
+
+    queue.offer(1);
+    queue.offer(2);
+    queue.offer(3);
+
+    while (!queue.isEmpty())
+    {
+        // poll() removes and returns from the front of the queue
+        System.out.println(queue.poll());
+    }
+
+    if (queue.isEmpty())
+    {
+        System.out.println("Queue is empty!");
+    }
+}
