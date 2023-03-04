ca332df5e8dd0d5514dc2a33d0ff628c7fb84163
Jeffry <jeffryklew@gmail.com>
2023-03-04 16:03:24 -0500

    Implement circular queue solution in moving_average_from_data_stream.py

--- stacks_and_queues/moving_average_from_data_stream.py
+++ stacks_and_queues/moving_average_from_data_stream.py
@@ -6,12 +6,17 @@ class MovingAverage:
         self.queueDS1 = deque()
         self.window_sumDS1 = 0
         self.countDS1 = 0
+
+        self.circ_queueDS2 = [0] * self.size
+        self.headDS2 = 0
+        self.window_sumDS2 = 0
+        self.countDS2 = 0
     
     def nextDS1(self, val: int) -> float:
         '''
         Double-ended queue discussion solution
         '''
-        
+
         self.countDS1 += 1
         # Calculate the new sum by shifting the window
         self.queueDS1.append(val)
@@ -20,3 +25,17 @@ class MovingAverage:
         self.window_sumDS1 = self.window_sumDS1 - tail + val
 
         return self.window_sumDS1 / min(self.size, self.countDS1)
+    
+    def nextDS2(self, val: int) -> float:
+        '''
+        Circular queue discussion solution
+        '''
+
+        self.countDS2 += 1
+        # Calculate the new sum by shifting the window
+        tail = (self.headDS2 + 1) % self.size
+        self.window_sumDS2 = self.window_sumDS2 - self.circ_queueDS2[tail] + val
+        # Move on to the next head
+        self.headDS2 = tail
+        self.circ_queueDS2[self.headDS2] = val
+        return self.window_sumDS2 / min(self.size, self.countDS2)
