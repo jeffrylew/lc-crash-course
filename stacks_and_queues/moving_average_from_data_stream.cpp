09a29b8e3fe45778fb8a266fedfcac79028e105d
Jeffry <jeffryklew@gmail.com>
2023-03-03 20:19:58 -0500

    Implement first discussion solution in moving_average_from_data_stream.cpp

--- stacks_and_queues/moving_average_from_data_stream.cpp
+++ stacks_and_queues/moving_average_from_data_stream.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <deque>
 #include <queue>
 
 class MovingAverage
@@ -7,10 +9,20 @@ class MovingAverage
     int    window_size {};
     double window_sum {};
 
+    int size_DS1 {};
+    int window_sum_DS1 {};
+    int count_DS1 {};
+
     std::queue<int> window {};
 
+    std::deque<int> deque_DS1 {};
+
     public:
-        MovingAverage(int size) : window_size {size} {}
+        MovingAverage(int size)
+            : window_size {size}
+            , size_DS1 {size}
+        {
+        }
 
         //! @brief First attempt to get moving average of last size values
         //! @param[in] val Next value in stream
@@ -28,6 +40,31 @@ class MovingAverage
 
             return window_sum / static_cast<double>(window.size());
         }
+
+        //! @brief Double-ended queue LC discussion solution
+        //! @param[in] val Next integer in stream
+        //! @return Moving average of size values as a double
+        double nextDS1(int val)
+        {
+            //! @details https://leetcode.com/problems/
+            //!          moving-average-from-data-stream/editorial/
+            //!
+            //!          Time complexity O(1)
+            //!          Space complexity O(N) where N is size of moving window
+
+            int tail {};
+            deque_DS1.push_back(val);
+
+            if (++count > size_DS1)
+            {
+                tail = deque_DS1.front();
+                deque_DS1.pop_front();
+            }
+
+            window_sum_DS1 = window_sum_DS1 - tail + val;
+
+            return window_sum_DS1 * 1.0 / std::min(size_DS1, count);
+        }
 };
 
 TEST(MovingAverageTest, TestNext)
@@ -38,4 +75,9 @@ TEST(MovingAverageTest, TestNext)
     EXPECT_DOUBLE_EQ(5.5, avg.nextFA(10));
     EXPECT_NEAR(4.66667, avg.nextFA(3));
     EXPECT_DOUBLE_EQ(6.0, avg.nextFA(5));
+
+    EXPECT_DOUBLE_EQ(1.0, avg.nextDS1(1));
+    EXPECT_DOUBLE_EQ(5.5, avg.nextDS1(10));
+    EXPECT_NEAR(4.66667, avg.nextDS1(3));
+    EXPECT_DOUBLE_EQ(6.0, avg.nextDS1(5));
 }
