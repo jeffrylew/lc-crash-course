294108d28f003bc193bc00f147612618ac3e5865
Jeffry <jeffryklew@gmail.com>
2023-03-04 14:56:47 -0500

    Implement circular queue discussion solution in moving_average_from_data_stream.cpp

--- stacks_and_queues/moving_average_from_data_stream.cpp
+++ stacks_and_queues/moving_average_from_data_stream.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <deque>
 #include <queue>
+#include <vector>
 
 class MovingAverage
 {
@@ -13,14 +14,23 @@ class MovingAverage
     int window_sum_DS1 {};
     int count_DS1 {};
 
+    int size_DS2 {};
+    int head_DS2 {};
+    int window_sum_DS2 {};
+    int count_DS2 {};
+
     std::queue<int> window {};
 
     std::deque<int> deque_DS1 {};
+
+    std::vector<int> circ_queue_DS2 {};
 
     public:
         MovingAverage(int size)
             : window_size {size}
             , size_DS1 {size}
+            , size_DS2 {size}
+            , circ_queue_DS2(static_cast<std::size_t>(size))
         {
         }
 
@@ -65,6 +75,30 @@ class MovingAverage
 
             return window_sum_DS1 * 1.0 / std::min(size_DS1, count);
         }
+
+        //! @brief Circular queue LC discussion solution
+        //! @param[in] val Next integer in stream
+        //! @return Moving average of size values as a double
+        double nextDS2(int val)
+        {
+            //! @details https://leetcode.com/problems/
+            //!          moving-average-from-data-stream/editorial/
+            //!
+            //!          Time complexity O(1)
+            //!          Space complexity O(N) where N is size of circular queue
+
+            ++count_DS2;
+
+            //! Calculate new sum by shifting the window
+            const int tail = (head_DS2 + 1) % size_DS2;
+            window_sum_DS2 = window_sum_DS2 - circ_queue_DS2[tail] + val;
+
+            //! Move on to next head
+            head_DS2                 = tail;
+            circ_queue_DS2[head_DS2] = val;
+
+            return window_sum_DS2 * 1.0 / std::min(size_DS2, count_DS2);
+        }
 };
 
 TEST(MovingAverageTest, TestNext)
@@ -80,4 +114,9 @@ TEST(MovingAverageTest, TestNext)
     EXPECT_DOUBLE_EQ(5.5, avg.nextDS1(10));
     EXPECT_NEAR(4.66667, avg.nextDS1(3));
     EXPECT_DOUBLE_EQ(6.0, avg.nextDS1(5));
+
+    EXPECT_DOUBLE_EQ(1.0, avg.nextDS2(1));
+    EXPECT_DOUBLE_EQ(5.5, avg.nextDS2(10));
+    EXPECT_NEAR(4.66667, avg.nextDS2(3));
+    EXPECT_DOUBLE_EQ(6.0, avg.nextDS2(5));
 }
