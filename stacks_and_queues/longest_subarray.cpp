a6f294a09a6c95dcd862f529d4a105dcaab0dcc6
Jeffry <jeffryklew@gmail.com>
2023-03-08 20:24:54 -0500

    Add big O analysis and tests to stacks_and_queues/longest_subarray.cpp

--- stacks_and_queues/longest_subarray.cpp
+++ stacks_and_queues/longest_subarray.cpp
@@ -4,8 +4,20 @@
 #include <deque>
 #include <vector>
 
+//! @brief Get size of longest subarray such that absolute diff is <= limit
+//! @param[in] nums  Vector of integers
+//! @param[in] limit Absolute difference between any two elements must be <= lim
+//! @return Size of longest non-empty subarray
 static int longestSubarray(std::vector<int> nums, int limit)
 {
+    //! @details https://leetcode.com/explore/interview/card/
+    //!          leetcodes-interview-crash-course-data-structures-and-algorithms
+    //!          /706/stacks-and-queues/4517/
+    //!
+    //!          Time complexity O(N) where N is the size of nums. Each for loop
+    //!          iteration is amortized O(N)
+    //!          Space complexity O(N) since the deques can grow to size N
+
     std::deque<int> increasing;
     std::deque<int> decreasing;
     int left {};
@@ -43,4 +55,19 @@ static int longestSubarray(std::vector<int> nums, int limit)
     }
 
     return ans;
+}
+
+TEST(LongestSubarrayTest, TestCase1)
+{
+    EXPECT_EQ(2, longestSubarray({8, 2, 4, 7}, 4));
+}
+
+TEST(LongestSubarrayTest, TestCase2)
+{
+    EXPECT_EQ(4, longestSubarray({10, 1, 2, 4, 7, 2}, 5));
+}
+
+TEST(LongestSubarrayTest, TestCase3)
+{
+    EXPECT_EQ(3, longestSubarray({4, 2, 2, 2, 4, 4, 2, 2}, 0));
 }
