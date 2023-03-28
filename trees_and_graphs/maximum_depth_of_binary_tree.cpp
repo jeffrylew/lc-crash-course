2f0984e4582f93b55d7667e223af397ed8c3c072
Jeffry <jeffryklew@gmail.com>
2023-03-27 20:35:11 -0400

    Implement iterative solution in maximum_depth_of_binary_tree.cpp

--- trees_and_graphs/maximum_depth_of_binary_tree.cpp
+++ trees_and_graphs/maximum_depth_of_binary_tree.cpp
@@ -3,6 +3,8 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <stack>
+#include <utility>
 
 //! @brief Recursive solution to find length of longest path from root to leaf
 //! @param[in] root Pointer to root of binary tree
@@ -21,6 +23,53 @@ static int maxDepthRecursive(TreeNode* root)
     //! (number of nodes on path from root to leaf, need to include root)
     return std::max(left, right) + 1;
 }
+
+//! @brief Iterative solution to find length of longest path from root to leaf
+//! @param[in] root Pointer to root of binary tree
+//! @return Length of longest path from root to leaf
+static int maxDepthIterative(TreeNode* root)
+{
+    //! @details https://leetcode.com/explore/interview/card/
+    //!          leetcodes-interview-crash-course-data-structures-and-algorithms
+    //!          /707/traversals-trees-graphs/4686/
+    //!
+    //!          Time complexity O(N) where N = total number of nodes. Each node
+    //!          is visited once and at each node, O(1) work is done.
+    //!          Space complexity O(N) in worst case if tree is a straight line.
+    //!          O(log N) for a complete tree (all nodes have 0 or 2 children
+    //!          and each level except the last is full).
+
+    if (root == nullptr)
+    {
+        return 0;
+    }
+
+    std::stack<std::pair<TreeNode*, int>> stack {};
+    stack.emplace(root, 1);
+
+    int ans {};
+
+    while (not stack.empty())
+    {
+        auto [node, depth] = stack.top();
+        stack.pop();
+
+        ans = std::max(ans, depth);
+
+        if (node->left != nullptr)
+        {
+            stack.emplace(node->left, depth + 1);
+        }
+
+        if (node->right != nullptr)
+        {
+            stack.emplace(node->right, depth + 1);
+        }
+    }
+
+    return ans;
+
+} // static int maxDepthIterative( ...
 
 TEST(MaxDepthTest, SampleTest)
 {
@@ -43,6 +92,7 @@ TEST(MaxDepthTest, SampleTest)
     five.right = &six;
 
     EXPECT_EQ(4, maxDepthRecursive(&zero));
+    EXPECT_EQ(4, maxDepthIterative(&zero));
 }
 
 TEST(MaxDepthTest, TestCase1)
@@ -61,4 +111,5 @@ TEST(MaxDepthTest, TestCase1)
     twenty.right = &seven;
 
     EXPECT_EQ(3, maxDepthRecursive(&three));
+    EXPECT_EQ(3, maxDepthIterative(&three));
 }
