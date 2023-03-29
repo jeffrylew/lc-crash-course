9623dfccb8a019af88a5085cb3d0e896510e9801
Jeffry <jeffryklew@gmail.com>
2023-03-29 08:23:30 -0400

    Implement iterative solution in maximum_depth_of_binary_tree.js

--- trees_and_graphs/maximum_depth_of_binary_tree.js
+++ trees_and_graphs/maximum_depth_of_binary_tree.js
@@ -8,3 +8,31 @@ var maxDepthRecursive = function(root) {
     const right = maxDepthRecursive(root.right);
     return Math.max(left, right) + 1;
 };
+
+var maxDepthIterative = function(root) {
+    if (!root)
+    {
+        return 0;
+    }
+
+    let stack = [[root, 1]];
+    let ans = 0;
+
+    while (stack.length)
+    {
+        const [node, depth] = stack.pop();
+        ans = Math.max(ans, depth);
+
+        if (node.left)
+        {
+            stack.push([node.left, depth + 1]);
+        }
+
+        if (node.right)
+        {
+            stack.push([node.right, depth + 1]);
+        }
+    }
+
+    return ans;
+};
