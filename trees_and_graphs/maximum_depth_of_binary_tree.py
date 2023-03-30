424d5f93121bb5243971b2b9e935c2d6237d6be3
Jeffry <jeffryklew@gmail.com>
2023-03-30 19:39:00 -0400

    Implement iterative solution in maximum_depth_of_binary_tree.py

--- trees_and_graphs/maximum_depth_of_binary_tree.py
+++ trees_and_graphs/maximum_depth_of_binary_tree.py
@@ -7,3 +7,22 @@ def maxDepthRecursive(root: treenode.TreeNode) -> int:
     left = maxDepthRecursive(root.left)
     right = maxDepthRecursive(root.right)
     return max(left, right) + 1
+
+def maxDepthIterative(root: treenode.TreeNode) -> int:
+    if not root:
+        return 0
+    
+    stack = [(root, 1)]
+    ans = 0
+
+    while stack:
+        node, depth = stack.pop()
+        ans = max(ans, depth)
+
+        if node.left:
+            stack.append((node.left, depth + 1))
+        
+        if node.right:
+            stack.append((node.right, depth + 1))
+    
+    return ans
