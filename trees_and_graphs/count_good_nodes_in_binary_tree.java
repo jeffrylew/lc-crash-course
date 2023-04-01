4dff77c3285e54f684ffc75d95f71f2d63c14303
Jeffry <jeffryklew@gmail.com>
2023-04-01 19:50:09 -0400

    Implement iterative solution in count_good_nodes_in_binary_tree.java

--- trees_and_graphs/count_good_nodes_in_binary_tree.java
+++ trees_and_graphs/count_good_nodes_in_binary_tree.java
@@ -20,3 +20,50 @@ public int goodNodesRecursive(TreeNode root)
 {
     return dfs(root, Integer.MIN_VALUE);
 }
+
+class Pair
+{
+    TreeNode node;
+    int maxSoFar;
+
+    Pair(TreeNode node, int maxSoFar)
+    {
+        this.node = node;
+        this.maxSoFar = maxSoFar;
+    }
+}
+
+public int goodNodesIterative(TreeNode root)
+{
+    if (root == null)
+    {
+        return 0;
+    }
+
+    Stack<Pair> stack = new Stack<>();
+    stack.push(new Pair(root, Integer.MIN_VALUE));
+    int ans = 0;
+
+    while (!stack.empty())
+    {
+        Pair pair = stack.pop();
+        TreeNode node = pair.node;
+        int maxSoFar = pair.maxSoFar;
+        if (node.val >= maxSoFar)
+        {
+            ans++;
+        }
+
+        if (node.left != null)
+        {
+            stack.push(new Pair(node.left, Math.max(maxSoFar, node.val)));
+        }
+
+        if (node.right != null)
+        {
+            stack.push(new Pair(node.right, Math.max(maxSoFar, node.val)));
+        }
+    }
+
+    return ans;
+}
