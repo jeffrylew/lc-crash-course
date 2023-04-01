10054a20aa9420f366dc9c6be719458dfde434d6
Jeffry <jeffryklew@gmail.com>
2023-04-01 15:27:24 -0400

    Implement iterative solution in trees_and_graphs/path_sum.java

--- trees_and_graphs/path_sum.java
+++ trees_and_graphs/path_sum.java
@@ -20,3 +20,52 @@ public boolean hasPathSumRecursive(TreeNode root, int targetSum)
 {
     return dfs(root, targetSum, 0);
 }
+
+class Pair
+{
+    TreeNode node;
+    int curr;
+    Pair(TreeNode node, int curr)
+    {
+        this.node = node;
+        this.curr = curr;
+    }
+}
+
+public boolean hasPathSumIterative(TreeNode root, int targetSum)
+{
+    if (root == null)
+    {
+        return false;
+    }
+
+    Stack<Pair> stack = new Stack<>();
+    stack.push(new Pair(root, 0));
+
+    while (!stack.empty())
+    {
+        Pair pair = stack.pop();
+        TreeNode node = pair.node;
+        int curr = pair.curr;
+
+        if (node.left == null
+            && node.right == null
+            && curr + node.val == targetSum)
+        {
+            return true;
+        }
+
+        curr += node.val;
+        if (node.left != null)
+        {
+            stack.push(new Pair(node.left, curr));
+        }
+
+        if (node.right != null)
+        {
+            stack.push(new Pair(node.right, curr));
+        }
+    }
+
+    return false;
+}
