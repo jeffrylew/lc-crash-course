8f1cb4d70aa656222fbd087ab8177fe013e8d36b
Jeffry <jeffryklew@gmail.com>
2023-04-01 15:40:47 -0400

    Implement iterative solution in trees_and_graphs/path_sum.js

--- trees_and_graphs/path_sum.js
+++ trees_and_graphs/path_sum.js
@@ -25,3 +25,40 @@ var hasPathSumRecursive = function(root, targetSum)
 
     return dfs(root, 0);
 };
+
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @returns {boolean}
+ */
+var hasPathSumIterative = function(root, targetSum)
+{
+    if (!root)
+    {
+        return false;
+    }
+
+    let stack = [[root, 0]];
+    while (stack.length)
+    {
+        let [node, curr] = stack.pop();
+
+        // If both children are null then the node is a leaf
+        if (!node.left && !node.right && curr + node.val == targetSum)
+        {
+            return true;
+        }
+
+        curr += node.val;
+        if (node.left)
+        {
+            stack.push([node.left, curr]);
+        }
+        if (node.right)
+        {
+            stack.push([node.right, curr]);
+        }
+    }
+
+    return false;
+};
