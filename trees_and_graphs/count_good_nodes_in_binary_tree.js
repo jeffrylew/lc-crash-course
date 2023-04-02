c2adafe57f7e54bb1794e53dd870746b7620fd59
Jeffry <jeffryklew@gmail.com>
2023-04-01 20:06:25 -0400

    Implement iterative solution in count_good_nodes_in_binary_tree.js

--- trees_and_graphs/count_good_nodes_in_binary_tree.js
+++ trees_and_graphs/count_good_nodes_in_binary_tree.js
@@ -23,3 +23,36 @@ var goodNodesRecursive = function(root)
 
     return dfs(root, -Infinity);
 };
+
+
+var goodNodesIterative = function(root)
+{
+    if (!root)
+    {
+        return 0;
+    }
+
+    let stack = [[root, -Infinity]];
+    let ans = 0;
+
+    while (stack.length)
+    {
+        const [node, maxSoFar] = stack.pop();
+        if (node.val >= maxSoFar)
+        {
+            ans++;
+        }
+
+        if (node.left)
+        {
+            stack.push([node.left, Math.max(maxSoFar, node.val)]);
+        }
+
+        if (node.right)
+        {
+            stack.push([node.right, Math.max(maxSoFar, node.val)]);
+        }
+    }
+
+    return ans;
+};
