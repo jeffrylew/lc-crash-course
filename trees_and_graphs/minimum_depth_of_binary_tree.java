69b1e9b2d887b75d61f006fe90f23c82c124cfc5
Jeffry <jeffryklew@gmail.com>
2023-04-04 06:34:42 -0400

    Implement iterative BFS solution in minimum_depth_of_binary_tree.java

--- trees_and_graphs/minimum_depth_of_binary_tree.java
+++ trees_and_graphs/minimum_depth_of_binary_tree.java
@@ -71,3 +71,39 @@ public int minDepthDFSIterative(TreeNode root)
 
     return min_depth;
 }
+
+public int minDepthBFSIterative(TreeNode root)
+{
+    if (root == null)
+    {
+        return 0;
+    }
+
+    Queue<Pair> queue = new LinkedList<>();
+    queue.offer(new Pair(root, 1));
+
+    int current_depth = 0;
+    while (!queue.isEmpty())
+    {
+        Pair pair = queue.poll();
+        TreeNode node = pair.node;
+        current_depth = pair.depth;
+
+        if (node.left == null && node.right == null)
+        {
+            break;
+        }
+
+        if (node.left != null)
+        {
+            queue.offer(new Pair(node.left, current_depth + 1));
+        }
+
+        if (node.right != null)
+        {
+            queue.offer(new Pair(node.right, current_depth + 1));
+        }
+    }
+    
+    return current_depth;
+}
