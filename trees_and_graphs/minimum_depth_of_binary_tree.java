79d663fd006ea0f4afe22a43a1f669d8402a7cad
Jeffry <jeffryklew@gmail.com>
2023-04-03 20:34:54 -0400

    Implement iterative DFS solution in minimum_depth_of_binary_tree.java

--- trees_and_graphs/minimum_depth_of_binary_tree.java
+++ trees_and_graphs/minimum_depth_of_binary_tree.java
@@ -23,3 +23,51 @@ public int minDepthRecursive(TreeNode root)
 
     return min_depth + 1;
 }
+
+class Pair
+{
+    TreeNode node;
+    int depth;
+
+    Pair(TreeNode node, int depth)
+    {
+        this.node = node;
+        this.depth = depth;
+    }
+}
+
+public int minDepthDFSIterative(TreeNode root)
+{
+    if (root == null)
+    {
+        return 0;
+    }
+
+    Stack<Pair> stack = new Stack<>();
+    stack.push(new Pair(root, 1));
+
+    int min_depth = Integer.MAX_VALUE;
+    while (!stack.empty())
+    {
+        Pair current = stack.pop();
+        TreeNode node = current.node;
+        int current_depth = current.depth;
+
+        if (node.left == null && node.right == null)
+        {
+            min_depth = Math.min(min_depth, current_depth);
+        }
+
+        if (node.left != null)
+        {
+            stack.push(new Pair(node.left, current_depth + 1));
+        }
+
+        if (node.right != null)
+        {
+            stack.push(new Pair(node.right, current_depth + 1));
+        }
+    }
+
+    return min_depth;
+}
