a281c657f45b8eef612b13a8a38f6795f11d2f00
Jeffry <jeffryklew@gmail.com>
2023-04-05 07:17:36 -0400

    Implement iterative DFS solution in minimum_depth_of_binary_tree.js

--- trees_and_graphs/minimum_depth_of_binary_tree.js
+++ trees_and_graphs/minimum_depth_of_binary_tree.js
@@ -23,3 +23,34 @@ const minDepthRecursive = (root) => {
 
     return min_depth + 1;
 };
+
+const minDepthDFSIterative = (root) => {
+    if (!root)
+    {
+        return 0;
+    }
+
+    let stack = [[root, 1]];
+    let min_depth = Number.MAX_SAFE_INTEGER;
+
+    while (stack.length)
+    {
+        const [node, current_depth] = stack.pop();
+        if (!node.left && !node.right)
+        {
+            min_depth = Math.min(min_depth, current_depth);
+        }
+
+        if (!node.left)
+        {
+            stack.push([node.left, current_depth + 1]);
+        }
+
+        if (!node.right)
+        {
+            stack.push([node.right, current_depth + 1]);
+        }
+    }
+
+    return min_depth;
+};
