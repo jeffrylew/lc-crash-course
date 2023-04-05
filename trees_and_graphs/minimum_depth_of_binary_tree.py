75a0fbcec45d1ef1b01ff95b8677b5c216d282e0
Jeffry <jeffryklew@gmail.com>
2023-04-05 18:25:42 -0400

    Implement iterative DFS solution in minimum_depth_of_binary_tree.py

--- trees_and_graphs/minimum_depth_of_binary_tree.py
+++ trees_and_graphs/minimum_depth_of_binary_tree.py
@@ -20,3 +20,26 @@ def minDepthRecursive(root: treenode.TreeNode) -> int:
             min_depth = min(minDepthRecursive(node), min_depth)
 
     return min_depth + 1
+
+def minDepthDFSIterative(root: treenode.TreeNode) -> int:
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    if not root:
+        return 0
+    
+    stack, min_depth = [(root, 1),], float('inf')
+
+    while stack:
+        curr, current_depth = stack.pop()
+        children = [curr.left, curr.right]
+
+        if not any(children):
+            min_depth = min(min_depth, current_depth)
+        
+        for node in children:
+            if node:
+                stack.append((node, current_depth + 1))
+    
+    return min_depth
