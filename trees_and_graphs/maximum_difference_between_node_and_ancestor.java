0d83f3b7b48e4da12b7d2547b7f00b5f2f876f12
Jeffry <jeffryklew@gmail.com>
2023-04-08 13:40:57 -0400

    Implement second recursive discussion solution in maximum_difference_between_node_and_ancestor.java

--- trees_and_graphs/maximum_difference_between_node_and_ancestor.java
+++ trees_and_graphs/maximum_difference_between_node_and_ancestor.java
@@ -34,4 +34,32 @@ class Solution
         helper1(root, root.val, root.val);
         return result;
     }
+
+    public int helper2(TreeNode node, int curMax, int curMin)
+    {
+        // If encounter a leaf return max - min along the path
+        if (node == null)
+        {
+            return curMax - curMin;
+        }
+
+        // Else update max and min and return max of left and right subtrees
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+
+        int left = helper2(node.left, curMax, curMin);
+        int right = helper2(node.right, curMax, curMin);
+
+        return Math.max(left, right);
+    }
+
+    public int maxAncestorDiff2(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        return helper2(root, root.val, root.val);
+    }
 }
