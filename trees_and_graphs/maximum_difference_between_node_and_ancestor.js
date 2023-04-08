bd41ad12530ac022b192d776b51d9ae013f24c46
Jeffry <jeffryklew@gmail.com>
2023-04-08 14:02:53 -0400

    Implement second recursive discussion solution in maximum_difference_between_node_and_ancestor.js

--- trees_and_graphs/maximum_difference_between_node_and_ancestor.js
+++ trees_and_graphs/maximum_difference_between_node_and_ancestor.js
@@ -37,4 +37,34 @@ class Solution
         this.helper1(root, root.val, root.val);
         return this.result;
     };
+
+    helper2(node, curMax, curMin) {
+        // If encounter a leaf return max - min along the path
+        if (!node)
+        {
+            return curMax - curMin;
+        }
+
+        // Else update max and min and return max of left and right subtrees
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+
+        const left = this.helper2(node.left, curMax, curMin);
+        const right = this.helper2(node.right, curMax, curMin);
+
+        return Math.max(left, right);
+    };
+
+    /**
+     * @param {TreeNode} root
+     * @returns {number}
+     */
+    maxAncestorDiff2(root) {
+        if (!root)
+        {
+            return 0;
+        }
+
+        return this.helper2(root, root.val, root.val);
+    }
 }
