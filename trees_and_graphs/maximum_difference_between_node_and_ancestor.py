da451d6bad187605f4ef202d4c672cd5fb28cfb2
Jeffry <jeffryklew@gmail.com>
2023-04-08 14:12:21 -0400

    Implement second recursive discussion solution in maximum_difference_between_node_and_ancestor.py

--- trees_and_graphs/maximum_difference_between_node_and_ancestor.py
+++ trees_and_graphs/maximum_difference_between_node_and_ancestor.py
@@ -26,3 +26,22 @@ class Solution:
 
         helper(root, root.val, root.val)
         return self.result
+    
+    def maxAncestorDiff2(self, root: treenode.TreeNode) -> int:
+        if not root:
+            return 0
+        
+        def helper(node, cur_max, cur_min):
+            # If encounter a leaf return max - min along the path
+            if not node:
+                return cur_max - cur_min
+            
+            # Else update max and min and return max of left and right subtrees
+            cur_max = max(cur_max, node.val)
+            cur_min = min(cur_min, node.val)
+
+            left = helper(node.left, cur_max, cur_min)
+            right = helper(node.right, cur_max, cur_min)
+            return max(left, right)
+        
+        return helper(root, root.val, root.val)
