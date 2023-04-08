ae768ca0aea40ffb0543aa2641286c87724223da
Jeffry <jeffryklew@gmail.com>
2023-04-08 17:38:18 -0400

    Implement discussion solution in trees_and_graphs/diameter_of_binary_tree.cpp

--- trees_and_graphs/diameter_of_binary_tree.cpp
+++ trees_and_graphs/diameter_of_binary_tree.cpp
@@ -40,6 +40,52 @@ static int diameterOfBinaryTreeFARecursive(TreeNode* root)
     return helperFARecursive(root, 0);
 }
 
+//! @brief Recursive helper function to get longest path from node to leaf
+//! @param[in]  node     Current node in binary tree
+//! @param[out] diameter Tracks longest path updated with sum of left and right
+//! @reurn Length of longest branch between a node's left and right branches
+static int longestPath(TreeNode* node, int& diameter)
+{
+    if (node == nullptr)
+    {
+        return 0;
+    }
+
+    //! Recursively find the longest path in both left child and right child
+    const int leftPath = longestPath(node->left, diameter);
+    const int rightPath = longestPath(node->right, diameter);
+
+    //! Update the diameter if leftPath + rightPath is larger
+    //! diameter keeps track of longest path at each node with sum of node's
+    //! left and right branches
+    diameter = std::max(diameter, leftPath + rightPath);
+
+    //! Return the longer between leftPath and rightPath
+    //! Remember to add 1 for the path connecting the node and its parent
+    return std::max(leftPath, rightPath) + 1;
+}
+
+//! @brief Get length of diameter of the tree
+//! @param[in] root Pointer to root of binary tree
+//! @return Length of longest path between any two nodes in a tree
+static int diameterOfBinaryTreeDS(TreeNode* root)
+{
+    //! @details https://leetcode.com/problems/diameter-of-binary-tree/
+    //!          editorial/
+    //!
+    //!          Time complexity O(N) where N is number of nodes in tree.
+    //!          In longestPath, only enter and exit from each node once - each
+    //!          node is entered from its parent, and in a tree, nodes only have
+    //!          one parent.
+    //!          Space complexity O(N). Depends on size of call stack during DFS
+    //!          which relates to height of tree. In worst case, tree is skewed
+    //!          so height is O(N). If tree is balanced it'd be O(log N)
+
+    int diameter {};
+    longestPath(root, diameter);
+    return diameter;
+}
+
 TEST(DiameterOfBinaryTreeTest, SampleTest)
 {
     TreeNode one {1};
@@ -58,4 +104,6 @@ TEST(DiameterOfBinaryTreeTest, SampleTest)
     //! First attempt solution is not correct
     EXPECT_NE(3, diameterOfBinaryTreeFARecursive(&one));
     EXPECT_EQ(5, diameterOfBinaryTreeFARecursive(&one));
+
+    EXPECT_EQ(3, diameterOfBinaryTreeDS(&one));
 }
