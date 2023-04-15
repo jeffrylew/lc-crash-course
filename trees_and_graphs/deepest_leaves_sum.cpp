44289c2d2367fdc09d3f38b8fbb447a5914ff007
Jeffry <jeffryklew@gmail.com>
2023-04-15 12:27:32 -0400

    Implement iterative DFS preorder traversal solution in deepest_leaves_sum.cpp

--- trees_and_graphs/deepest_leaves_sum.cpp
+++ trees_and_graphs/deepest_leaves_sum.cpp
@@ -3,6 +3,8 @@
 #include <gtest/gtest.h>
 
 #include <queue>
+#include <stack>
+#include <utility>
 
 //! @brief First attempt solution to get sum of values of deepest leaves
 //! @param[in] root Pointer to root of binary tree
@@ -42,6 +44,66 @@ static int deepestLeavesSumFA(TreeNode* root)
 
 } // static int deepestLeavesSumFA( ...
 
+//! @brief Get deepest leaves sum with iterative DFS Preorder Traversal
+//! @param[in] root Pointer to root of binary tree
+//! @return Sum of values of deepest leaves
+static int deepestLeavesSumDS1(TreeNode* root)
+{
+    //! @details Time complexity O(N) since one has to visit each node
+    //!          Space complexity up to O(H) to keep the stack, where H is tree
+    //!          height
+
+    int deepestSum {};
+    int depth {};
+    int currDepth {};
+
+    std::stack<std::pair<TreeNode*, int>> stack {};
+    stack.emplace(root, 0);
+
+    while (not stack.empty())
+    {
+        const auto p = stack.top();
+        stack.pop();
+
+        root      = p.first;
+        currDepth = p.second;
+
+        if (root->left == nullptr && root->right == nullptr)
+        {
+            //! If this leaf is the deepest one seen so far
+            if (depth < currDepth)
+            {
+                //! Start new sum
+                deepestSum = root->val;
+
+                //! Note new depth
+                depth = currDepth;
+            }
+            else if (depth == currDepth)
+            {
+                //! If there were already leaves at this depth
+                //! Update existing sum
+                deepestSum += root->val;
+            }
+        }
+        else
+        {
+            if (root->right != nullptr)
+            {
+                stack.emplace(root->right, currDepth + 1);
+            }
+
+            if (root->left != nullptr)
+            {
+                stack.emplace(root->left, currDepth + 1);
+            }
+        }
+    }
+
+    return deepestSum;
+
+} // static int deepestLeavesSumDS1( ...
+
 TEST(DeepestLeavesSumTest, SampleTest)
 {
     TreeNode one {1};
@@ -67,4 +129,5 @@ TEST(DeepestLeavesSumTest, SampleTest)
     six.right = &eight;
 
     EXPECT_EQ(15, deepestLeavesSumFA(&one));
+    EXPECT_EQ(15, deepestLeavesSumDS1(&one));
 }
