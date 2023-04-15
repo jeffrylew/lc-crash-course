444beb82f63684acd196832ae8e2227d1431fbff
Jeffry <jeffryklew@gmail.com>
2023-04-15 06:29:51 -0400

    Implement trees_and_graphs/find_largest_value_in_each_tree_row.java

---
+++ trees_and_graphs/find_largest_value_in_each_tree_row.java
@@ -0,0 +1,37 @@
+public List<Integer> largestValues(TreeNode root)
+{
+    if (root == null)
+    {
+        return new ArrayList<>();
+    }
+
+    List<Integer> ans = new ArrayList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty())
+    {
+        int currentLength = queue.size();
+        int currMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < currentLength; i++)
+        {
+            TreeNode node = queue.remove();
+            currMax = Math.max(currMax, node.val);
+
+            if (node.left != null)
+            {
+                queue.add(node.left);
+            }
+
+            if (node.right != null)
+            {
+                queue.add(node.right);
+            }
+        }
+
+        ans.add(currMax);
+    }
+
+    return ans;
+}
