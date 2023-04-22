4a3e13bf334454a92f4ec49f6d960ef2c3fa6602
Jeffry <jeffryklew@gmail.com>
2023-04-22 15:26:13 -0400

    Implement DFS discussion solution in binary_tree_zigzag_level_order_traversal.js

--- trees_and_graphs/binary_tree_zigzag_level_order_traversal.js
+++ trees_and_graphs/binary_tree_zigzag_level_order_traversal.js
@@ -59,3 +59,48 @@ var zigzagLevelOrderBFS = function(root)
 
     return results;
 };
+
+var DFS_helper = function(node, level, results)
+{
+    if (level >= results.length)
+    {
+        results.push([node.val]);
+    }
+    else
+    {
+        if (level % 2 == 0)
+        {
+            results[level].push(node.val);
+        }
+        else
+        {
+            results[level].unshift(node.val);
+        }
+    }
+
+    if (node.left)
+    {
+        DFS_helper(node.left, level + 1, results);
+    }
+
+    if (node.right)
+    {
+        DFS_helper(node.right, level + 1, results);
+    }
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrderDFS = function(root)
+{
+    if (!root)
+    {
+        return [];
+    }
+
+    let results = [];
+    DFS_helper(root, 0, results);
+    return results;
+};
