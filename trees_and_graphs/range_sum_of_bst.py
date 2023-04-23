e06592c8349ec2cdcec3057829d912ca101f528d
Jeffry <jeffryklew@gmail.com>
2023-04-23 16:08:44 -0400

    Implement iterative discussion solution in range_sum_of_bst.py

--- trees_and_graphs/range_sum_of_bst.py
+++ trees_and_graphs/range_sum_of_bst.py
@@ -15,3 +15,23 @@ def rangeSumBSTRecursive(root: treenode.TreeNode, low: int, high: int) -> int:
         ans += rangeSumBSTRecursive(root.right, low, high)
 
     return ans
+
+def rangeSumBSTIterative(root: treenode.TreeNode, low: int, high: int) -> int:
+    if not root:
+        return 0
+    
+    stack = [root]
+    ans = 0
+    while stack:
+        node = stack.pop()
+
+        if low <= node.val <= high:
+            ans += node.val
+
+        if node.left and low < node.val:
+            stack.append(node.left)
+        
+        if node.right and node.val < high:
+            stack.append(node.right)
+    
+    return ans
