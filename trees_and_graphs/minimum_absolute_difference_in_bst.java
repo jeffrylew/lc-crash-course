e3233db51c9adfbe46a74f1007d09a187c769a99
Jeffry <jeffryklew@gmail.com>
2023-04-23 16:55:28 -0400

    Implement iterative solution in minimum_absolute_difference_in_bst.java

--- trees_and_graphs/minimum_absolute_difference_in_bst.java
+++ trees_and_graphs/minimum_absolute_difference_in_bst.java
@@ -24,3 +24,39 @@ public int getMinimumDifferenceRecursive(TreeNode root)
 
     return ans;
 }
+
+public List<Integer> iterativeInorder(TreeNode root)
+{
+    Stack<TreeNode> stack = new Stack<>();
+    List<Integer> values = new ArrayList<>();
+    TreeNode curr = root;
+
+    while (!stack.isEmpty() || curr != null)
+    {
+        if (curr != null)
+        {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        else
+        {
+            curr = stack.pop();
+            values.add(curr.val);
+            curr = curr.right;
+        }
+    }
+
+    return values;
+}
+
+public int getMinimumDifferenceIterative(TreeNode root)
+{
+    List<Integer> values = iterativeInorder(root);
+    int ans = Integer.MAX_VALUE;
+    for (int i = 1; i < values.size(); i++)
+    {
+        ans = Math.min(ans, values.get(i) - values.get(i - 1));
+    }
+
+    return ans;
+}
