eb51af118a5802b9253301727029815f8fd75ec9
Jeffry <jeffryklew@gmail.com>
2023-04-23 16:01:08 -0400

    Implement iterative discussion solution in range_sum_of_bst.js

--- trees_and_graphs/range_sum_of_bst.js
+++ trees_and_graphs/range_sum_of_bst.js
@@ -29,3 +29,41 @@ var rangeSumBSTRecursive = function(root, low, high)
 
     return ans;
 };
+
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBSTIterative = function(root, low, high)
+{
+    if (!root)
+    {
+        return 0;
+    }
+
+    let stack = [root];
+    let ans = 0;
+
+    while (stack.length)
+    {
+        const node = stack.pop();
+        if (low <= node.val && node.val <= high)
+        {
+            ans += node.val;
+        }
+
+        if (node.left && low < node.val)
+        {
+            stack.push(node.left);
+        }
+
+        if (node.right && node.val < high)
+        {
+            stack.push(node.right);
+        }
+    }
+
+    return ans;
+};
