c0d7954d33241321382bef2f39c5c87047192b36
Jeffry <jeffryklew@gmail.com>
2023-04-25 19:24:45 -0400

    Implement iterative solution in minimum_absolute_difference_in_bst.js

--- trees_and_graphs/minimum_absolute_difference_in_bst.js
+++ trees_and_graphs/minimum_absolute_difference_in_bst.js
@@ -27,3 +27,42 @@ var getMinimumDifferenceRecursive = function(root)
 
     return ans;
 };
+
+/**
+ * @param {TreeNode} root
+ * @returns {number}
+ */
+var getMinimumDifferenceIterative = function(root)
+{
+    let iterativeInorder = root => {
+        let stack = [];
+        let values = [];
+        let curr = root;
+
+        while (stack.length || curr)
+        {
+            if (curr)
+            {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            else
+            {
+                curr = stack.pop();
+                values.push(curr.val);
+                curr = curr.right;
+            }
+        }
+
+        return values;
+    }
+
+    let values = iterativeInorder(root);
+    let ans = Infinity;
+    for (let i = 1; i < values.length; i++)
+    {
+        ans = Math.min(ans, values[i] - values[i - 1]);
+    }
+
+    return ans;
+};
