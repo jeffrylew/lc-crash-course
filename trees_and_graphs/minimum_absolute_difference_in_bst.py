e169605c65b989d64c1f31e7046a56b35077f97a
Jeffry <jeffryklew@gmail.com>
2023-04-27 21:31:45 -0400

    Implement iterative solution in minimum_absolute_difference_in_bst.py

--- trees_and_graphs/minimum_absolute_difference_in_bst.py
+++ trees_and_graphs/minimum_absolute_difference_in_bst.py
@@ -1,6 +1,6 @@
 import treenode
 
-def getMinimumDifference(root: treenode.TreeNode) -> int:
+def getMinimumDifferenceRecursive(root: treenode.TreeNode) -> int:
     def dfs(node: treenode.TreeNode) -> list[int]:
         if not node:
             return []
@@ -10,6 +10,30 @@ def getMinimumDifference(root: treenode.TreeNode) -> int:
         return left + [node.val] + right
     
     values = dfs(root)
+    ans = float("inf")
+    for i in range(1, len(values)):
+        ans = min(ans, values[i] - values[i - 1])
+    
+    return ans
+
+def getMinimumDifferenceIterative(root: treenode.TreeNode) -> int:
+    def iterative_inorder(root):
+        stack = []
+        values = []
+        curr = root
+
+        while stack or curr:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                values.append(curr.val)
+                curr = curr.right
+
+        return values
+    
+    values = iterative_inorder(root)
     ans = float("inf")
     for i in range(1, len(values)):
         ans = min(ans, values[i] - values[i - 1])
