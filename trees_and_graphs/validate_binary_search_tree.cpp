bf1eeb0dd1164795e05396e599b09d2e5c8517ab
Jeffry <jeffryklew@gmail.com>
2023-04-29 08:19:45 -0400

    Implement iterative solution in validate_binary_search_tree.cpp

--- trees_and_graphs/validate_binary_search_tree.cpp
+++ trees_and_graphs/validate_binary_search_tree.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <limits>
+#include <stack>
 
 //! @brief Recursive helper function to determine if tree rooted at node is BST
 //! @param[in] node Pointer to current node
@@ -30,13 +31,67 @@ static bool dfs(TreeNode* node, std::int64_t small, std::int64_t large)
     return left && right;
 }
 
-//! @brief Validate if binary tree is a valid BST
+//! @brief Recursive solution to validate if binary tree is a valid BST
 //! @param[in] root Pointer to root of binary tree
 //! @return True if binary tree is a valid BST, else false
 static bool isValidBSTRecursive(TreeNode* root)
 {
     return dfs(root,
                std::numeric_limits<std::int64_t>::min(), std::numeric_limits<std::int64_t>::max());
+}
+
+//! @struct Bound
+//! @brief Stores node and the lower and upper bounds (exclusive) for its value
+struct Bound
+{
+    TreeNode*    node {nullptr};
+    std::int64_t small {};
+    std::int64_t large {};
+
+    Bound()  = default;
+    ~Bound() = default;
+
+    Bound(TreeNode* node_in, std::int64_t small_in, std::int64_t large_in)
+        : node {node_in}
+        , small {small_in}
+        , large {large_in}
+    {
+    }
+};
+
+//! @brief Iterative solution to validate if binary tree is a valid BST
+//! @param[in] root Pointer to root of binary tree
+//! @return True if binary tree is a valid BST, else false
+static bool isValidBSTIterative(TreeNode* root)
+{
+    //! Alternatively could use std::tuple
+    std::stack<Bound> stack {};
+    stack.emplace(root,
+                  std::numeric_limits<std::int64_t>::min(),
+                  std::numeric_limits<std::int64_t>::max());
+    
+    while (not stack.empty())
+    {
+        const auto b = stack.top();
+        stack.pop();
+
+        if (b.small >= b.node->val || b.node->val >= b.large)
+        {
+            return false;
+        }
+
+        if (b.node->left != nullptr)
+        {
+            stack.emplace(b.node->left, b.small, b.node->val);
+        }
+
+        if (b.node->right != nullptr)
+        {
+            stack.emplace(b.node->right, b.node->val, b.large);
+        }
+    }
+
+    return true;
 }
 
 TEST(IsValidBSTTest, SampleTest)
@@ -58,4 +113,5 @@ TEST(IsValidBSTTest, SampleTest)
     twelve.right = &twenty_three;
 
     EXPECT_TRUE(isValidBSTRecursive(&ten));
+    EXPECT_TRUE(isValidBSTIterative(&ten));
 }
