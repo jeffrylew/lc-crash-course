ccb5a55f6c98dbe03a3fd2a2fbe14158a01da344
Jeffry <jeffryklew@gmail.com>
2023-04-29 08:33:33 -0400

    Implement iterative solution in validate_binary_search_tree.java

--- trees_and_graphs/validate_binary_search_tree.java
+++ trees_and_graphs/validate_binary_search_tree.java
@@ -19,3 +19,48 @@ public boolean isValidBSTRecursive(TreeNode root)
 {
     return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
 }
+
+class State
+{
+    TreeNode node;
+    long small;
+    long large;
+
+    State(TreeNode node, long small, long large)
+    {
+        this.node = node;
+        this.small = small;
+        this.large = large;
+    }
+}
+
+public boolean isValidBSTIterative(TreeNode root)
+{
+    Stack<State> stack = new Stack<>();
+    stack.push(new State(root, Long.MIN_VALUE, Long.MAX_VALUE));
+
+    while (!stack.empty())
+    {
+        State state = stack.pop();
+        TreeNode node = state.node;
+        long small = state.small;
+        long large = state.large;
+
+        if (small >= node.val || node.val >= large)
+        {
+            return false;
+        }
+
+        if (node.left != null)
+        {
+            stack.push(new State(node.left, small, node.val));
+        }
+
+        if (node.right != null)
+        {
+            stack.push(new State(node.right, node.val, large));
+        }
+    }
+
+    return true;
+}
