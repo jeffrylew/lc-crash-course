f75178fd49f807ca7cca8c220b849ff7d83d62ae
Jeffry <jeffryklew@gmail.com>
2023-04-29 08:48:14 -0400

    Implement iterative solution in validate_binary_search_tree.js

--- trees_and_graphs/validate_binary_search_tree.js
+++ trees_and_graphs/validate_binary_search_tree.js
@@ -24,3 +24,34 @@ var isValidBSTRecursive = function(root)
 
     return dfs(root, -Infinity, Infinity);
 };
+
+/**
+ * @param {TreeNode} root
+ * @returns {boolean}
+ */
+var isValidBSTIterative = function(root)
+{
+    let stack = [[root, -Infinity, Infinity]];
+
+    while (stack.length)
+    {
+        const [node, small, large] = stack.pop();
+
+        if (small >= node.val || node.val >= large)
+        {
+            return false;
+        }
+
+        if (node.left)
+        {
+            stack.push([node.left, small, node.val]);
+        }
+
+        if (node.right)
+        {
+            stack.push([node.right, node.val, large]);
+        }
+    }
+
+    return true;
+};
