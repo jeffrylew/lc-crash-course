515f7da197b438201c1fdd43e3379103fb4c2d3a
Jeffry <jeffryklew@gmail.com>
2023-04-29 08:55:55 -0400

    Implement iterative solution in validate_binary_search_tree.py

--- trees_and_graphs/validate_binary_search_tree.py
+++ trees_and_graphs/validate_binary_search_tree.py
@@ -15,3 +15,19 @@ def isValidBSTRecursive(root: treenode.TreeNode) -> bool:
         return left and right
     
     return dfs(root, float("-inf"), float("inf"))
+
+def isValidBSTIterative(root: treenode.TreeNode) -> bool:
+    stack = [(root, float("-inf"), float("inf"))]
+
+    while stack:
+        node, small, large = stack.pop()
+        if not (small < node.val < large):
+            return False
+        
+        if node.left:
+            stack.append((node.left, small, node.val))
+
+        if node.right:
+            stack.append((node.right, node.val, large))
+
+    return True
