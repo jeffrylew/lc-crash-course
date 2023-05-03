dd290c8235ba1fcf0cbba9e24255834377f256b4
Jeffry <jeffryklew@gmail.com>
2023-05-03 07:06:31 -0400

    Implement iterative solution in insert_into_a_binary_search_tree.js

--- trees_and_graphs/insert_into_a_binary_search_tree.js
+++ trees_and_graphs/insert_into_a_binary_search_tree.js
@@ -1,5 +1,10 @@
 import TreeNode from "./treenode";
 
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @returns {TreeNode}
+ */
 var insertIntoBSTRecursive = function(root, val)
 {
     if (!root)
@@ -19,4 +24,47 @@ var insertIntoBSTRecursive = function(root, val)
     }
 
     return root;
+};
+
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @returns {TreeNode}
+ */
+var insertIntoBSTIterative = function(root, val)
+{
+    let node = root;
+
+    while (node)
+    {
+        if (val > node.val)
+        {
+            // Insert into the right subtree
+            if (!node.right)
+            {
+                // Insert right now
+                node.right = new TreeNode(val);
+                return root;
+            }
+            else
+            {
+                node = node.right;
+            }
+        }
+        else
+        {
+            // Insert into the left subtree
+            if (!node.left)
+            {
+                node.left = new TreeNode(val);
+                return root;
+            }
+            else
+            {
+                node = node.left;
+            }
+        }
+    }
+
+    return new TreeNode(val);
 };
