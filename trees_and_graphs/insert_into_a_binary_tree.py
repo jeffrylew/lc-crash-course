5cbbd67e3781a34cd718e0d9d53c5d367a10fd97
Jeffry <jeffryklew@gmail.com>
2023-05-05 07:50:14 -0400

    Implement iterative solution in insert_into_a_binary_tree.py

--- trees_and_graphs/insert_into_a_binary_tree.py
+++ trees_and_graphs/insert_into_a_binary_tree.py
@@ -14,3 +14,28 @@ def insertIntoBSTRecursive(
         root.left = insertIntoBSTRecursive(root.left, val)
     
     return root
+
+def insertIntoBSTIterative(
+    root: treenode.TreeNode, val: int
+) -> treenode.TreeNode:
+    node = root
+
+    while node:
+        # Insert into the right subtree
+        if val > node.val:
+            # Insert right now
+            if not node.right:
+                node.right = treenode.TreeNode(val)
+                return root
+            else:
+                node = node.right
+        # Insert into the left subtree
+        else:
+            # Insert right now
+            if not node.left:
+                node.left = treenode.TreeNode(val)
+                return root
+            else:
+                node = node.left
+        
+    return treenode.TreeNode(val)
