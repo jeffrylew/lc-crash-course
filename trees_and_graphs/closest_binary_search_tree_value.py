24208d1311edbbaf730e372eb5cdba57d9336461
Jeffry <jeffryklew@gmail.com>
2023-05-06 17:43:08 -0400

    Implement iterative solution in closest_binary_search_tree_value.py

--- trees_and_graphs/closest_binary_search_tree_value.py
+++ trees_and_graphs/closest_binary_search_tree_value.py
@@ -5,3 +5,21 @@ def closestValueRecursive(root: treenode.TreeNode, target: float) -> int:
         return inorder(r.left) + [r.val] + inorder(r.right) if r else []
     
     return min(inorder(root), key = lambda x: abs(target - x))
+
+
+def closestValueIterative(root: treenode.TreeNode, target: float) -> int:
+    stack, pred = [], float('-inf')
+
+    while stack or root:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+
+        if pred <= target and target < root.val:
+            return min(pred, root.val, key = lambda x: abs(target - x))
+        
+        pred = root.val
+        root = root.right
+    
+    return pred
