a84547fac7b5d63bf1ef8199eb95720d3b732044
Jeffry <jeffryklew@gmail.com>
2023-05-06 18:07:16 -0400

    Implement binary search solution in closest_binary_search_tree_value.py

--- trees_and_graphs/closest_binary_search_tree_value.py
+++ trees_and_graphs/closest_binary_search_tree_value.py
@@ -23,3 +23,17 @@ def closestValueIterative(root: treenode.TreeNode, target: float) -> int:
         root = root.right
     
     return pred
+
+
+def closestValueBinarySearch(root: treenode.TreeNode, target: float) -> int:
+    closest = root.val
+    while root:
+        # Editorial is incorrect here. If have root 4 with right child 5 and
+        # target is 4.5 then the following will set closest to 5 instead of 4
+        # closest = min(root.val, closest, key = lambda x: abs(target - x))
+
+        arg = (root.val, closest) if root.val < closest else (closest, root.val)
+        closest = min(*arg, key = lambda x: abs(target - x))
+
+        root = root.left if target < root.val else root.right
+    return closest
