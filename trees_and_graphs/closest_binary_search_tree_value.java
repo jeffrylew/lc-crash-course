8d4925ed3105e8ce73d0d2e77c49d376c405b210
Jeffry <jeffryklew@gmail.com>
2023-05-06 08:53:20 -0400

    Implement iterative solution in closest_binary_search_tree_value.java

--- trees_and_graphs/closest_binary_search_tree_value.java
+++ trees_and_graphs/closest_binary_search_tree_value.java
@@ -25,4 +25,31 @@ class Solution
             }
         });
     }
+
+    public int closestValueIterative(TreeNode root, double target)
+    {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        long pred = Long.MIN_VALUE;
+
+        while (!stack.isEmpty() || root != null)
+        {
+            while (root != null)
+            {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+
+            if (pred <= target && target < root.val)
+            {
+                return Math.abs(pred - target) < Math.abs(root.val - target)
+                    ? (int)pred : root.val;
+            }
+
+            pred = root.val;
+            root = root.right;
+        }
+
+        return (int)pred;
+    }
 }
