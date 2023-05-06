89fa1ca1f806361b1c0b60d7df373077fb443fea
Jeffry <jeffryklew@gmail.com>
2023-05-06 09:56:31 -0400

    Implement iterative solution in closest_binary_search_tree_value.js

--- trees_and_graphs/closest_binary_search_tree_value.js
+++ trees_and_graphs/closest_binary_search_tree_value.js
@@ -25,3 +25,37 @@ var closestValueRecursive = function(root, target)
         (prev, curr) => Math.abs(prev - target) <= Math.abs(curr - target)
         ? prev : curr);
 };
+
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValueIterative = function(root, target)
+{
+    let stack = [];
+    let pred = Number.MIN_SAFE_INTEGER;
+
+    while (stack.length || root)
+    {
+        while (root)
+        {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        
+        if (pred <= target && target < root.val)
+        {
+            // Need to use <= instead of < to get smallest value that is closest
+            // to the target. e.g. target = 3.5, BST contains 3 and 4, return 3
+            return Math.abs(pred - target) <= Math.abs(root.val - target)
+                ? pred : root.val;
+        }
+
+        pred = root.val;
+        root = root.right;
+    }
+    
+    return pred;
+};
