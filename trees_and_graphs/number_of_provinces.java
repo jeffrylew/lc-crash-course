4a6921b83d1684567b24702b0d8a311dac580850
Jeffry <jeffryklew@gmail.com>
2023-05-18 06:22:03 -0400

    Implement recursive dfs solution in trees_and_graphs/number_of_provinces.java

---
+++ trees_and_graphs/number_of_provinces.java
@@ -0,0 +1,59 @@
+class Solution
+{
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+    boolean[] seen;
+
+    public void dfsRecursive(int node)
+    {
+        for (int neighbor: graph.get(node))
+        {
+            if (!seen[neighbor])
+            {
+                seen[neighbor] = true;
+                dfsRecursive(neighbor);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected)
+    {
+        // Build the graph
+        for (int i = 0; i < n; i++)
+        {
+            if (!graph.containsKey(i))
+            {
+                graph.put(i, new ArrayList<>());
+            }
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (!graph.containsKey(j))
+                {
+                    graph.put(j, new ArrayList<>());
+                }
+
+                if (isConnected[i][j] == 1)
+                {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
+            }
+        }
+
+        seen = new boolean[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!seen[i])
+            {
+                // Add all nodes of a connected component to the set
+                ans++;
+                seen[i] = true;
+                dfsRecursive(i);
+            }
+        }
+
+        return ans;
+    }
+}
