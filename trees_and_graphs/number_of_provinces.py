e0c826a9bc243c7702d6c617229ff3c942945805
Jeffry <jeffryklew@gmail.com>
2023-05-19 15:28:43 -0400

    Implement recursive dfs solution in number_of_provinces.py

---
+++ trees_and_graphs/number_of_provinces.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+class Solution:
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        def dfsRecursive(node):
+            for neighbor in graph[node]:
+                # The next 2 lines are needed to prevent cycles
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfsRecursive(neighbor)
+        
+        # Build the graph
+        n = len(isConnected)
+        graph = defaultdict(list)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+        
+        seen = set()
+        ans = 0
+
+        for i in range(n):
+            if i not in seen:
+                # Add all nodes of a connected component to the set
+                ans += 1
+                seen.add(i)
+                dfsRecursive(i)
+        
+        return ans
