a4dd1bb6fdf37a9bb0c8887c922168e4cae0e2ab
Jeffry <jeffryklew@gmail.com>
2023-05-21 09:58:38 -0400

    Implement recursive dfs solution in trees_and_graphs/number_of_islands.java

---
+++ trees_and_graphs/number_of_islands.java
@@ -0,0 +1,60 @@
+class Solution
+{
+    int m;
+    int n;
+    int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    Set<String> seen = new HashSet<>();
+
+    public String convertToHash(int row, int col)
+    {
+        return String.valueOf(row) + "," + String.valueOf(col);
+    }
+
+    public boolean valid(int row, int col, char[][] grid)
+    {
+        return 0 <= row
+            && row < m
+            && 0 <= col
+            && col < n
+            && grid[row][col] == '1';
+    }
+
+    public void dfsRecursive(int row, int col, char[][] grid)
+    {
+        for (int[] direction : directions)
+        {
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
+            String hash = convertToHash(nextRow, nextCol);
+
+            if (valid(nextRow, nextCol, grid) && !seen.contains(hash))
+            {
+                seen.add(hash);
+                dfsRecursive(nextRow, nextCol, grid);
+            }
+        }
+    }
+
+    public int numIslands(char[][] grid)
+    {
+        int ans = 0;
+        m = grid.length;
+        n = grid[0].length;
+
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                String hash = convertToHash(row, col);
+                if (grid[row][col] == '1' && !seen.contains(hash))
+                {
+                    ans++;
+                    seen.add(hash);
+                    dfsRecursive(row, col, grid);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
