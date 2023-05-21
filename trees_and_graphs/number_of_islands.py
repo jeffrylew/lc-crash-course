4c214dc49dd156deb2f93df00617b3dad0d5e2d2
Jeffry <jeffryklew@gmail.com>
2023-05-21 18:40:38 -0400

    Implement iterative dfs solution in trees_and_graphs/number_of_islands.py

--- trees_and_graphs/number_of_islands.py
+++ trees_and_graphs/number_of_islands.py
@@ -9,6 +9,16 @@ def numIslands(grid: list[list[str]]) -> int:
                 seen.add((next_row, next_col))
                 dfsRecursive(next_row, next_col)
     
+    def dfsIterative(start_row, start_col):
+        stack = [(start_row, start_col)]
+        while stack:
+            row, col = stack.pop()
+            for drow, dcol in directions:
+                next_row, next_col = row + drow, col + dcol
+                if valid(next_row, next_col) and (next_row, next_col) not in seen:
+                    seen.add((next_row, next_col))
+                    stack.append((next_row, next_col))
+    
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     seen = set()
     ans = 0
@@ -21,5 +31,6 @@ def numIslands(grid: list[list[str]]) -> int:
                 ans += 1
                 seen.add((row, col))
                 dfsRecursive(row, col)
+                # dfsIterative(row, col)
     
     return ans
