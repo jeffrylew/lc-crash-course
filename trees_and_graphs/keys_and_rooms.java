672f40ac8f727bf0fb23b36c634feb4df2e90ca0
Jeffry <jeffryklew@gmail.com>
2023-05-22 18:57:25 -0400

    Implement iterative dfs solution in trees_and_graphs/keys_and_rooms.java

--- trees_and_graphs/keys_and_rooms.java
+++ trees_and_graphs/keys_and_rooms.java
@@ -14,10 +14,30 @@ class Solution
         }
     }
 
+    public void dfsIterative(int node, List<List<Integer>> rooms)
+    {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        while (!stack.empty())
+        {
+            int node = stack.pop();
+            for (int neighbor : rooms.get(node))
+            {
+                if (!seen.contains(neighbor))
+                {
+                    seen.add(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms)
     {
         seen.add(0);
         dfsRecursive(0, rooms);
+        // dfsIterative(0, rooms);
         return seen.size() == rooms.size();
     }
 }
