54b888492a6f7e0ea1fcdb6c63cb0ec349017fa1
Jeffry <jeffryklew@gmail.com>
2023-05-22 19:09:05 -0400

    Implement iterative dfs solution in trees_and_graphs/keys_and_rooms.py

--- trees_and_graphs/keys_and_rooms.py
+++ trees_and_graphs/keys_and_rooms.py
@@ -5,7 +5,17 @@ class Solution:
                 if neighbor not in seen:
                     seen.add(neighbor)
                     dfsRecursive(neighbor)
+
+        def dfsIterative(start):
+            stack = [start]
+            while stack:
+                node = stack.pop()
+                for neighbor in rooms[node]:
+                    if neighbor not in seen:
+                        seen.add(neighbor)
+                        stack.append(neighbor)
         
         seen = {0}
         dfsRecursive(0)
+        # dfsIterative(0)
         return len(seen) == len(rooms)
