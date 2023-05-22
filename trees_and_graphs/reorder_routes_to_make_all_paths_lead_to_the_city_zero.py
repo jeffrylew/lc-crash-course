a8ab69f622cc1a31cb13f742f82b3ae798f9353a
Jeffry <jeffryklew@gmail.com>
2023-05-21 20:26:22 -0400

    Implement recursive dfs solution in reorder_routes_to_make_all_paths_lead_to_the_city_zero.py

---
+++ trees_and_graphs/reorder_routes_to_make_all_paths_lead_to_the_city_zero.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+def minReorder(n: int, connections: list[list[int]]) -> int:
+    roads = set()
+    graph = defaultdict(list)
+
+    for x, y in connections:
+        graph[x].append(y)
+        graph[y].append(x)
+        roads.add((x, y))
+    
+    def dfsRecursive(node):
+        ans = 0
+        for neighbor in graph[node]:
+            if neighbor not in seen:
+                if (node, neighbor) in roads:
+                    ans += 1
+                
+                seen.add(neighbor)
+                ans += dfsRecursive(neighbor)
+
+        return ans
+    
+    seen = {0}
+    return dfsRecursive(0)
