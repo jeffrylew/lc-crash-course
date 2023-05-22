98e513c83a2ea1216f1feae85075c2db72b12cd2
Jeffry <jeffryklew@gmail.com>
2023-05-22 19:03:49 -0400

    Implement iterative dfs solution in trees_and_graphs/keys_and_rooms.js

--- trees_and_graphs/keys_and_rooms.js
+++ trees_and_graphs/keys_and_rooms.js
@@ -14,7 +14,25 @@ var canVisitAllRooms = function(rooms) {
         }
     }
 
+    const dfsIterative = start => {
+        let stack = [start];
+
+        while (stack.length)
+        {
+            const node = stack.pop();
+            for (const neighbor of rooms[node])
+            {
+                if (!seen.has(neighbor))
+                {
+                    seen.add(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+
     let seen = new Set([0]);
     dfsRecursive(0);
+    // dfsIterative(0);
     return seen.size = rooms.length;
 }
