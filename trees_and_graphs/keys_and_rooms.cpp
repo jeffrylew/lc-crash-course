c978fc198f171ea863935b61afa0d511f1d43be5
Jeffry <jeffryklew@gmail.com>
2023-05-22 18:40:19 -0400

    Implement iterative dfs solution in trees_and_graphs/keys_and_rooms.cpp

--- trees_and_graphs/keys_and_rooms.cpp
+++ trees_and_graphs/keys_and_rooms.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <stack>
 #include <unordered_set>
 #include <vector>
 
@@ -16,6 +17,28 @@ static void dfsRecursive(int                                  node,
     }
 }
 
+static void dfsIterative(int                                  start,
+                         std::unordered_set<int>&             seen,
+                         const std::vector<std::vector<int>>& rooms)
+{
+    std::stack<int> stack {};
+    stack.emplace(start);
+
+    while (not stack.empty())
+    {
+        const int node {stack.top()};
+        stack.pop();
+
+        for (const int neighbor : rooms.at(node))
+        {
+            if (seen.emplace(neighbor).second)
+            {
+                stack.push(neighbor);
+            }
+        }
+    }
+}
+
 //! @brief Determine if all locked rooms can be visited
 //! @param[in] room Adjacency list where rooms[i] is a vector of visitable rooms
 //! @return True if can visit all locked rooms, else false
@@ -27,6 +50,7 @@ static bool canVisitAllRooms(const std::vector<std::vector<int>>& rooms)
 
     std::unordered_set<int> seen {0};
     dfsRecursive(0, seen, rooms);
+    // dfsIterative(0, seen, rooms);
     return seen.size() == rooms.size();
 }
 
