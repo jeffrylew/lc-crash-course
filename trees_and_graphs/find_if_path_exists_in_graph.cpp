4003280ca63c189a57fc63b6a3d44e792a6a3f6b
Jeffry <jeffryklew@gmail.com>
2023-05-24 18:01:38 -0400

    Add BFS discussion solution to find_if_path_exists_in_graph.cpp

--- trees_and_graphs/find_if_path_exists_in_graph.cpp
+++ trees_and_graphs/find_if_path_exists_in_graph.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <queue>
 #include <unordered_map>
 #include <vector>
 
@@ -60,10 +61,75 @@ static bool validPathFA(int                                  n,
 
 } // static bool validPathFA( ...
 
+//! @brief Breadth First Search solution to determine if there is a valid path
+//! @param[in] n           Number of vertices
+//! @param[in] edges       Vector of bi-directional edges
+//! @param[in] source      Starting vertex of path
+//! @param[in] destination Ending vertex of path
+//! @return True if there is a valid path between source and destination
+static bool validPathBFS(int                                  n,
+                         const std::vector<std::vector<int>>& edges,
+                         int                                  source,
+                         int                                  destination)
+{
+    //! @details https://leetcode.com/problems/find-if-path-exists-in-graph/
+    //!          editorial/
+    //!
+    //!          Time complexity O(N + E) where N = number of nodes and
+    //!          E = number of edges. Building a list of adjacent edges in graph
+    //!          takes O(E). Each node is pushed and popped from the queue which
+    //!          takes O(N) to handle all nodes.
+    //!          Space complexity O(N + E). Used a hash map graph to store all
+    //!          edges, which requires O(E) space. Used seen vector to record
+    //!          visited nodes, which requires O(N) space. There may be up to N
+    //!          nodes stored in queue, requiring O(N) space.
+
+    //! Store all edges in 'graph'
+    std::unordered_map<int, std::vector<int>> graph;
+    for (const auto& edge : edges)
+    {
+        const int a {edge.front()};
+        const int b {edge.back()};
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    //! Store all the nodes to be visited in 'bfsQueue'
+    std::vector<bool> seen(n);
+    seen[source] = true;
+
+    std::queue<int> bfsQueue {};
+    bfsQueue.push(source);
+
+    while (not bfsQueue.empty())
+    {
+        const int currNode {bfsQueue.front()};
+        bfsQueue.pop();
+
+        if (currNode == destination)
+        {
+            return true;
+        }
+
+        for (const int nextNode : graph[currNode])
+        {
+            if (not seen[nextNode])
+            {
+                seen[nextNode] = true;
+                bfsQueue.push(nextNode);
+            }
+        }
+    }
+
+    return false;
+
+} // static bool validPathBFS( ...
+
 TEST(ValidPathTest, SampleTest1)
 {
     const std::vector<std::vector<int>> edges {{0, 1}, {1, 2}, {2, 0}};
     EXPECT_TRUE(validPathFA(3, edges, 0, 2));
+    EXPECT_TRUE(validPathBFS(3, edges, 0, 2));
 }
 
 TEST(ValidPathTest, SampleTest2)
@@ -71,4 +137,5 @@ TEST(ValidPathTest, SampleTest2)
     const std::vector<std::vector<int>> edges {
         {0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}};
     EXPECT_FALSE(validPathFA(6, edges, 0, 5));
+    EXPECT_FALSE(validPathBFS(6, edges, 0, 5));
 }
