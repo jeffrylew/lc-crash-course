34f12d8bbf6bcb2aae9e7f1d6253238b5dbbfcfb
Jeffry <jeffryklew@gmail.com>
2023-05-27 12:24:35 -0400

    Implement iterative dfs solution in trees_and_graphs/max_area_of_island.js

--- trees_and_graphs/max_area_of_island.js
+++ trees_and_graphs/max_area_of_island.js
@@ -41,3 +41,63 @@ var maxAreaOfIslandDFSRecursive = function(grid) {
 
     return ans;
 };
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIslandDFSIterative = function(grid) {
+    let seen = new Array(grid.length);
+    for (let row = 0; row < grid.length; row++)
+    {
+        seen[row] = new Array(grid[0].length).fill(false);
+    }
+
+    const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+
+    let ans = 0;
+
+    for (let row = 0; row < grid.length; row++)
+    {
+        for (let col = 0; col < grid[0].length; col++)
+        {
+            if (seen[row][col] || grid[row][col] == 0)
+            {
+                continue;
+            }
+
+            let curr_area = 0;
+            let stack = [[row, col]];
+            seen[row][col] = true;
+
+            while (stack.length)
+            {
+                const [curr_row, curr_col] = stack.pop();
+                curr_area++;
+
+                for (const [drow, dcol] of directions)
+                {
+                    const neighbor_row = curr_row + drow;
+                    const neighbor_col = curr_col + dcol;
+
+                    if (neighbor_row < 0
+                        || neighbor_row >= grid.length
+                        || neighbor_col < 0
+                        || neighbor_col >= grid[0].length
+                        || seen[neighbor_row][neighbor_col]
+                        || grid[neighbor_row][neighbor_col] == 0)
+                    {
+                        continue;
+                    }
+
+                    seen[neighbor_row][neighbor_col] = true;
+                    stack.push([neighbor_row, neighbor_col]);
+                }
+            }
+
+            ans = Math.max(ans, curr_area);
+        }
+    }
+
+    return ans;
+};
