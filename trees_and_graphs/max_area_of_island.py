409a2ff549a6b276a96f90a22fff66bee7122766
Jeffry <jeffryklew@gmail.com>
2023-05-27 12:52:26 -0400

    Implement iterative dfs solution in trees_and_graphs/max_area_of_island.py

--- trees_and_graphs/max_area_of_island.py
+++ trees_and_graphs/max_area_of_island.py
@@ -13,3 +13,28 @@ def maxAreaOfIslandDFSRecursive(grid):
     return max(area(r, c)
                for r in range(len(grid))
                for c in range(len(grid[0])))
+
+def maxAreaOfIslandDFSIterative(grid):
+    seen = set()
+    ans = 0
+
+    for r0, row in enumerate(grid):
+        for c0, val in enumerate(row):
+            if val == 1 and (r0, c0) not in seen:
+                shape = 0
+                stack = [(r0, c0)]
+                seen.add((r0, c0))
+
+                while stack:
+                    r, c = stack.pop()
+                    shape += 1
+
+                    for nr, nc in (
+                        (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)):
+                        if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
+                            and grid[nr][nc] == 1 and (nr, nc) not in seen):
+                            stack.append((nr, nc))
+                            seen.add((nr, nc))
+                ans = max(ans, shape)
+    
+    return ans
