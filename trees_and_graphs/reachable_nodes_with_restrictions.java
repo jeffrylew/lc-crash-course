75331a54d49e017292250bc509a6919813f28153
Jeffry <jeffryklew@gmail.com>
2023-05-29 19:57:51 -0400

    Implement iterative dfs solution in reachable_nodes_with_restrictions.java

--- trees_and_graphs/reachable_nodes_with_restrictions.java
+++ trees_and_graphs/reachable_nodes_with_restrictions.java
@@ -89,3 +89,49 @@ class Solution
         return ans;
     }
 }
+
+public int reachableNodesIterative(int n, int[][] edges, int[] restricted)
+{
+    // Store all edges according to nodes in neighbors
+    Map<Integer, List<Integer>> neighbors = new HashMap<>();
+    for (int[] edge : edges)
+    {
+        int a = edge[0];
+        int b = edge[1];
+
+        neighbors.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+        neighbors.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+    }
+
+    // Mark the nodes in restricted as visited
+    Set<Integer> seen = new HashSet<>();
+    for (int node : restricted)
+    {
+        seen.add(node);
+    }
+
+    // Use stack to store all nodes to be visited
+    int ans = 0;
+    seen.add(0);
+    Stack<Integer> stack = new Stack<>();
+    stack.add(0);
+
+    while (!stack.isEmpty())
+    {
+        int currNode = stack.pop();
+        ans++;
+
+        // Add all unvisited neighbors of current node to stack
+        // and mark it as visited
+        for (int nextNode : neighbors.get(curNode))
+        {
+            if (!seen.contains(nextNode))
+            {
+                seen.add(nextNode);
+                stack.add(nextNode);
+            }
+        }
+    }
+
+    return ans;
+}
