48a2795a74ca64d8b76f7f6ced5345224891072b
Jeffry <jeffryklew@gmail.com>
2023-05-29 17:10:28 -0400

    Implement recursive dfs solution in reachable_nodes_with_restrictions.java

--- trees_and_graphs/reachable_nodes_with_restrictions.java
+++ trees_and_graphs/reachable_nodes_with_restrictions.java
@@ -41,3 +41,51 @@ public int reachableNodesBFS(int n, int[][] edges, int[] restricted)
 
     return ans;
 }
+
+class Solution
+{
+    private int ans = 0;
+
+    private void dfsRecursive(int                         currNode,
+                              Map<Integer, List<Integer>> neighbors,
+                              Set<Integer>                seen)
+    {
+        // Mark currNode as visited and increment ans by 1
+        ans++;
+        seen.add(currNode);
+
+        // Go for unvisited neighbors of currNode
+        for (int nextNode : neighbors.get(currNode))
+        {
+            if (!seen.contains(nextNode))
+            {
+                dfsRecursive(nextNode, neighbors, seen);
+            }
+        }
+    }
+
+    public int reachableNodesRecursive(int n, int[][] edges, int[] restricted)
+    {
+        // Store all edges according to nodes in neighbors
+        Map<Integer, List<Integer>> neighbors = new HashMap<>();
+
+        for (int[] edge : edges)
+        {
+            int a = edge[0];
+            int b = edge[1];
+
+            neighbors.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+            neighbors.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+        }
+
+        // Mark nodes in restricted as visited
+        Set<Integer> seen = new HashSet<>();
+        for (int node : restricted)
+        {
+            seen.add(node);
+        }
+
+        dfsRecursive(0, neighbors, seen);
+        return ans;
+    }
+}
