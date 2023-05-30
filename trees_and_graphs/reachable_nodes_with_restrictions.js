fa959e6207a1d525cd9245572581ce9a8f568b7b
Jeffry <jeffryklew@gmail.com>
2023-05-29 20:23:43 -0400

    Implement bfs solution in reachable_nodes_with_restrictions.js

---
+++ trees_and_graphs/reachable_nodes_with_restrictions.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} restricted
+ * @return {number}
+ */
+var reachableNodesBFS = function(n, edges, restricted) {
+    let neighbors = new Map();
+    for (let node = 0; node < n; node++)
+    {
+        neighbors.set(node, []);
+    }
+
+    for (const [nodeA, nodeB] of edges)
+    {
+        neighbors.get(nodeA).push(nodeB);
+        neighbors.get(nodeB).push(nodeA);
+    }
+
+    let seen = new Array(n).fill(false);
+    for (const node of restricted)
+    {
+        seen[node] = true;
+    }
+
+    let ans = 0;
+    let queue = [0];
+    seen[0] = true;
+
+    while (queue.length)
+    {
+        const currNode = queue.shift();
+        ans++;
+
+        for (const nextNode of neighbors.get(currNode))
+        {
+            if (!seen[nextNode])
+            {
+                seen[nextNode] = true;
+                queue.push(nextNode);
+            }
+        }
+    }
+
+    return ans;
+};
