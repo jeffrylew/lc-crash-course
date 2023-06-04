e96610577d62f2065c8290bf38760365fd7b7182
Jeffry <jeffryklew@gmail.com>
2023-06-04 18:14:58 -0400

    Implement second discussion solution in shortest_path_in_a_grid_with_obstacles_elimination.java

--- trees_and_graphs/shortest_path_in_a_grid_with_obstacles_elimination.java
+++ trees_and_graphs/shortest_path_in_a_grid_with_obstacles_elimination.java
@@ -25,6 +25,8 @@ class Solution
         return 0 <= row && row < m && 0 <= col && col < n;
     }
 
+    // Runtime 29 ms: Beats 57.3%
+    // Memory 44.8 MB: Beats 26.9%
     public int shortestPath1(int[][] grid, int k)
     {
         m = grid.length;
@@ -72,6 +74,71 @@ class Solution
                         queue.add(
                             new State(nextRow, nextCol, remain - 1, steps + 1));
                     }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // Runtime 19 ms: Beats 66.75%
+    // Memory 43.8 MB: Beats 45.1%
+    public int shortestPath2(int[][] grid, int k)
+    {
+        m = grid.length;
+        n = grid[0].length;
+
+        int[][] seen = new int[m][n];
+        for (int row = 0; row < m; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                seen[row][col] = -1;
+            }
+        }
+
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new state(0, 0, k, 0));
+
+        while (!queue.isEmpty())
+        {
+            State state = queue.remove();
+            int row = state.row;
+            int col = state.col;
+            int remain = state.remain;
+            int steps = state.steps;
+
+            if (row == m - 1 && col == n - 1)
+            {
+                return steps;
+            }
+
+            if (grid[row][col] == 1)
+            {
+                if (remain == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    remain--;
+                }
+            }
+
+            if (seen[row][col] >= remain)
+            {
+                continue;
+            }
+
+            seen[row][col] = remain;
+            for (int[] direction : directions)
+            {
+                int nextRow = row + direction[0];
+                int nextCol = col + direction[1];
+
+                if (valid(nextRow, nextCol))
+                {
+                    queue.add(new State(nextRow, nextCol, remain, steps + 1));
                 }
             }
         }
