5574b0e50d8d2481d3ec5f61bf398cd5f20e07ef
Jeffry <jeffryklew@gmail.com>
2023-06-12 20:39:29 -0400

    Add test and big O analysis to trees_and_graphs/open_the_lock.cpp

--- trees_and_graphs/open_the_lock.cpp
+++ trees_and_graphs/open_the_lock.cpp
@@ -16,6 +16,9 @@ static std::vector<std::string> neighbors(std::string node)
         const auto num = static_cast<int>(node[i]);
         for (const int change : {-1, 1})
         {
+            //! Handle wrap-around case
+            //! decrement(x) = (x - 1) % 10, decrement(0) = 9
+            //! increment(x) = (x + 1) % 10, increment(9) = 0
             const int x {
                 (num - static_cast<int>('0') + change) % 10
                 + static_cast<int>('0')};
@@ -28,9 +31,27 @@ static std::vector<std::string> neighbors(std::string node)
     return ans;
 }
 
+//! @brief Get min moves to make lock read target
+//! @param[in] deadends Vector of blocked codes - lock can no longer turn
+//! @param[in] target   Target combination
+//! @return Minimum number of moves to make lock read target
 static int openLock(
     const std::vector<std::string>& deadends, std::string target)
 {
+    //! @details https://leetcode.com/explore/interview/card/
+    //!          leetcodes-interview-crash-course-data-structures-and-algorithms
+    //!          /707/traversals-trees-graphs/4635/
+    //!
+    //!          Time complexity O(d) where d = deadends.length for converting
+    //!          deadends into a set. Everything else in the problem is constant
+    //!          (4 slots, 10 digits). However, if the lock had N slots then the
+    //!          time complexity would be O(10^N * N^2 + d). There are 10^N
+    //!          different states because each slot has 10 options. At each
+    //!          state, we perform O(N^2) work because we loop over the N slots
+    //!          while performing string concatenation, which is O(N) for
+    //!          immutable strings
+    //!          Space complexity O(d) for deadends set
+
     if (std::find(deadends.cbegin(), deadends.cend(), "0000")
         != deadends.cend())
     {
@@ -62,4 +83,9 @@ static int openLock(
     }
 
     return -1;
+}
+
+TEST(OpenLockTest, SampleTest1)
+{
+    EXPECT_EQ(6, openLock({"0201", "0101", "0102", "1212", "2002"}, "0202"));
 }
