54a51dc428907ae07f65386f4033013d35be8f74
Jeffry <jeffryklew@gmail.com>
2023-07-02 09:21:28 -0400

    Implement BFS solution in trees_and_graphs/jump_game_iii.cpp

--- trees_and_graphs/jump_game_iii.cpp
+++ trees_and_graphs/jump_game_iii.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 //! @brief First attempt solution to check if can reach any index with value 0
-//! @param[in] arr Vector of non-negative integers
+//! @param[in] arr   Vector of non-negative integers
 //! @param[in] start Initial index in arr
 //! @return True if can reach any index with value 0, else false
 static bool canReachFA(std::vector<int> arr, int start)
@@ -41,6 +41,58 @@ static bool canReachFA(std::vector<int> arr, int start)
     return false;
 
 } // static bool canReachFA( ...
+
+//! @brief BFS solution to check if can reach any index with value 0
+//! @param[in] arr   Vector of non-negative integers
+//! @param[in] start Initial index in arr
+//! @return True if can reach any index with value 0, else false
+static bool canReachBFS(std::vector<int> arr, int start)
+{
+    //! @details https://leetcode.com/problems/jump-game-iii/editorial/
+    //!
+    //!          Time complexity O(N) where N = arr.size(). We visit every index
+    //!          at most once.
+    //!          Space complexity O(N) since need queue to store next index.
+    //!          queue keeps at most two levels of nodes and each node has two
+    //!          childeren at most. Traversal of this solution is a binary tree.
+    //!          Max number of nodes in a single level would be N / 2 so the max
+    //!          length of queue is O(N / 2) + O(N / 2) = O(N)
+
+    const auto n = static_cast<int>(arr.size());
+
+    std::queue<int> queue({start});
+
+    while (not queue.empty())
+    {
+        const int node {queue.front()};
+        queue.pop();
+
+        //! Check if we reached zero
+        if (arr.at(node) == 0)
+        {
+            return true;
+        }
+
+        if (arr.at(node) > 0)
+        {
+            //! Check available next steps
+            if (node + arr.at(node) < n)
+            {
+                queue.push(node + arr.at(node));
+            }
+
+            if (node - arr.at(node) >= 0)
+            {
+                queue.push(node - arr.at(node));
+            }
+
+            //! Mark as visited
+            arr[node] = -arr[node];
+        }
+    }
+
+    return false;
+}
 
 TEST(CanReachTest, SampleTest)
 {
