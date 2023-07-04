1665af8dfef611f065497968e47d9b0032973e0f
Jeffry <jeffryklew@gmail.com>
2023-07-04 10:42:46 -0400

    Implement DFS solution in trees_and_graphs/jump_game_iii.java

--- trees_and_graphs/jump_game_iii.java
+++ trees_and_graphs/jump_game_iii.java
@@ -1,4 +1,4 @@
-public boolean canReach(int[] arr, int start)
+public boolean canReachBFS(int[] arr, int start)
 {
     int n = arr.length;
 
@@ -33,6 +33,23 @@ public boolean canReach(int[] arr, int start)
 
         //! Mark as visited
         arr[node] = -arr[node];
+    }
+
+    return false;
+}
+
+public boolean canReachDFS(int[] arr, int start)
+{
+    if (start >= 0 && start < arr.length && arr[start] >= 0)
+    {
+        if (arr[start] == 0)
+        {
+            return true;
+        }
+
+        arr[start] = -arr[start];
+        return canReachDFS(arr, start + arr[start])
+            || canReachDFS(arr, start - arr[start]);
     }
 
     return false;
