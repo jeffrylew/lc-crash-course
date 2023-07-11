b1da92f5f5b173ace387f21b57300e04e1982663
Jeffry <jeffryklew@gmail.com>
2023-07-11 19:08:23 -0400

    Implement bidirectional BFS solution in trees_and_graphs/word_ladder.java

--- trees_and_graphs/word_ladder.java
+++ trees_and_graphs/word_ladder.java
@@ -63,3 +63,114 @@ public int ladderLength(String beginWord, String endWord, List<String> wordList)
 
     return 0;
 }
+
+class Solution
+{
+    private int L;
+    private Map<String, List<String>> allComboDict;
+
+    Solution()
+    {
+        this.L = 0;
+
+        this.allComboDict = new HashMap<>();
+    }
+
+    private int visitWordNode(
+        Queue<Pair<String, Integer>> queue,
+        Map<String, Integer> visited,
+        Map<String, Integer> othersVisited)
+    {
+        int initQueueLen = queue.length();
+
+        for (int j = 0; j < initQueueLen; j++)
+        {
+            Pair<String, Integer> node = queue.remove();
+            String word = node.getKey();
+            int level = node.getValue();
+
+            for (int i = 0; i < this.L; i++)
+            {
+                String newWord =
+                    word.substring(0, i) + '*' + word.substring(i + 1, L);
+                
+                for (String adjacentWord
+                    : this.allComboDict.getOrDefault(newWord, new ArrayList<>()))
+                {
+                    if (othersVisited.containsKey(adjacentWord))
+                    {
+                        return level + othersVisited.get(adjacentWord);
+                    }
+
+                    if (!visited.containsKey(adjacentWord))
+                    {
+                        visited.put(adjacentWord, level + 1);
+                        queue.add(new Pair(adjacentWord, level + 1));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int ladderLengthBidirBFS(
+        String       beginWord,
+        String       endWord,
+        List<String> wordList)
+    {
+        if (!wordList.contains(endWord))
+        {
+            return 0;
+        }
+
+        this.L = beginWord.length();
+
+        wordList.forEach( word -> {
+            for (int i = 0; i < L; i++)
+            {
+                String newWord =
+                    word.substring(0, i) + '*' + word.substring(i + 1, L);
+                
+                List<String> transformations =
+                    this.allComboDict.getOrDefault(newWord, new ArrayList<>());
+                transformations.add(word);
+
+                this.allComboDict.put(newWord, transformations);
+            }
+        });
+
+        Queue<Pair<String, Integer>> qBegin = new LinkedList<>();
+        Queue<Pair<String, Integer>> qEnd = new LinkedList<>();
+
+        qBegin.add(new Pair(beginWord, 1));
+        qEnd.add(new Pair(endWord, 1));
+
+        Map<String, Integer> visitedBegin = new HashMap<>();
+        Map<String, Integer> visitedEnd = new HashMap<>();
+
+        visitedBegin.put(beginWord, 1);
+        visitedEnd.put(endWord, 1);
+
+        int ans = -1;
+
+        while (!qBegin.isEmpty() && !qEnd.isEmpty())
+        {
+            if (qBegin.size() <= qEnd.size())
+            {
+                ans = visitWordNode(qBegin, visitedBegin, visitedEnd);
+            }
+            else
+            {
+                ans = visitWordNode(qEnd, visitedEnd, visitedBegin);
+            }
+
+            if (ans > -1)
+            {
+                return ans;
+            }
+        }
+
+        return 0;
+    }
+}
