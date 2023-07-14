d49d36512863cf29c849477e7328e03a0f359d6d
Jeffry <jeffryklew@gmail.com>
2023-07-13 23:13:22 -0400

    WIP Implement bidirectional BFS solution in trees_and_graphs/word_ladder.js

--- trees_and_graphs/word_ladder.js
+++ trees_and_graphs/word_ladder.js
@@ -70,3 +70,106 @@ var ladderLengthBFS = function(beginWord, endWord, wordList)
 
     return 0;
 };
+
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLengthBidirBFS = function(beginWord, endWord, wordList)
+{
+    if (!wordList.includes(endWord))
+    {
+        return 0;
+    }
+
+    const L = beginWord.length;
+
+    let allComboMap = new Map();
+    for (const word of wordList)
+    {
+        for (let i = 0; i < L; i++)
+        {
+            const newWord =
+                word.substring(0, i) + "*" + word.substring(i + 1);
+
+            if (!allComboMap.has(newWord))
+            {
+                allComboMap.set(newWord, [word]);
+            }
+            else
+            {
+                allComboMap.get(newWord).push(word);
+            }
+        }
+    }
+
+    let qBegin = [[beginWord, 1]];
+    let qEnd = [[endWord, 1]];
+
+    let visitedBegin = new Map();
+    let visitedEnd = new Map();
+    visitedBegin.set(beginWord, 1);
+    visitedEnd.set(endWord, 1);
+
+    const visitWordNode = (queue, visited, othersVisited) => {
+        const currQueueLength = queue.length;
+        let nextQueue = [];
+
+        for (let j = 0; j < currQueueLength; j++)
+        {
+            const [word, level] = queue[j];
+
+            for (let i = 0; i < L; i++)
+            {
+                const newWord =
+                    word.substring(0, i) + "*" + word.substring(i + 1);
+                
+                if (!allComboMap.has(newWord))
+                {
+                    continue;
+                }
+
+                for (const adjacentWord of allComboMap.get(newWord))
+                {
+                    if (othersVisited.has(adjacentWord))
+                    {
+                        return level + othersVisited.get(adjacentWord);
+                    }
+
+                    if (!visited.has(adjacentWord))
+                    {
+                        visited.set(adjacentWord, level + 1);
+                        nextQueue.push([adjacentWord, level + 1]);
+                    }
+                }
+            }
+        }
+
+        queue = nextQueue;
+
+        return -1;
+    }
+
+    let ans = -1;
+
+    while (qBegin.length && qEnd.length)
+    {
+        if (qBegin.length <= qEnd.length)
+        {
+            ans = visitWordNode(qBegin, visitedBegin, visitedEnd);
+        }
+        else
+        {
+            ans = visitWordNode(qEnd, visitedEnd, visitedBegin);
+        }
+
+        if (ans > -1)
+        {
+            return ans;
+        }
+    }
+
+    return 0;
+};
