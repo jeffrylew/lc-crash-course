e464e5c72dbc6836c0761b547e9346f8c66ae9d5
Jeffry <jeffryklew@gmail.com>
2023-08-06 16:00:02 -0400

    Implement Quickselect solution in kth_largest_element_in_an_array.java

--- heaps/kth_largest_element_in_an_array.java
+++ heaps/kth_largest_element_in_an_array.java
@@ -22,3 +22,52 @@ public int findKthLargestDS2(int[] nums, int k)
 
     return minHeap.peek();
 }
+
+public int quickSelect(List<Integer> nums, int k)
+{
+    int pivotIndex = new Random().nextInt(nums.size());
+    int pivot = nums.get(pivotIndex);
+
+    List<Integer> left = new ArrayList<>();
+    List<Integer> mid = new ArrayList<>();
+    List<Integer> right = new ArrayList<>();
+
+    for (int num : nums)
+    {
+        if (num > pivot)
+        {
+            left.add(num);
+        }
+        else if (num < pivot)
+        {
+            right.add(num);
+        }
+        else
+        {
+            mid.add(num);
+        }
+    }
+
+    if (k <= left.size())
+    {
+        return quickSelect(left, k);
+    }
+
+    if (left.size() + mid.size() < k)
+    {
+        return quickSelect(right, k - left.size() - mid.size());
+    }
+
+    return pivot;
+}
+
+public int findKthLargestDS3(int[] nums, int k)
+{
+    List<Integer> list = new ArrayList<>();
+    for (int num : nums)
+    {
+        list.add(num);
+    }
+
+    return quickSelect(list, k);
+}
