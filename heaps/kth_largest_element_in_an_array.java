f7627be01fdc3b204a8cbe85f0810710c21493e2
Jeffry <jeffryklew@gmail.com>
2023-08-06 15:38:58 -0400

    Implement min heap solution in kth_largest_element_in_an_array.java

--- heaps/kth_largest_element_in_an_array.java
+++ heaps/kth_largest_element_in_an_array.java
@@ -6,3 +6,19 @@ public int findKthLargestDS1(int[] nums, int k)
     // Sort ascending and then return the kth element from the end
     return nums[nums.length - k];
 }
+
+public int findKthLargestDS2(int[] nums, int k)
+{
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    for (int num : nums)
+    {
+        minHeap.add(num);
+
+        if (minHeap.size() > k)
+        {
+            minHeap.remove();
+        }
+    }
+
+    return minHeap.peek();
+}
