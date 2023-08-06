8ea075897f90c59f332d10edd986a506e632f0df
Jeffry <jeffryklew@gmail.com>
2023-08-06 16:21:23 -0400

    Implement counting sort solution in kth_largest_element_in_an_array.java

--- heaps/kth_largest_element_in_an_array.java
+++ heaps/kth_largest_element_in_an_array.java
@@ -71,3 +71,34 @@ public int findKthLargestDS3(int[] nums, int k)
 
     return quickSelect(list, k);
 }
+
+public int findKthLargestDS4(int[] nums, int k)
+{
+    int minValue = Integer.MAX_VALUE;
+    int maxValue = Integer.MIN_VALUE;
+
+    for (int num : nums)
+    {
+        minValue = Math.min(minValue, num);
+        maxValue = Math.max(maxValue, num);
+    }
+
+    int[] count = new int[maxValue - minValue + 1];
+    for (int num : nums)
+    {
+        count[num - minValue]++;
+    }
+
+    int remain = k;
+    for (int num = count.length - 1; num >= 0; num--)
+    {
+        remain -= count[num];
+
+        if (remain <= 0)
+        {
+            return num + minValue;
+        }
+    }
+
+    return -1;
+}
