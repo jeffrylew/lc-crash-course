f329bb833a5a896a12e1496cd9647bb6d7cd8651
Jeffry <jeffryklew@gmail.com>
2023-08-06 16:29:42 -0400

    Implement min heap solution in kth_largest_element_in_an_array.py

--- heaps/kth_largest_element_in_an_array.py
+++ heaps/kth_largest_element_in_an_array.py
@@ -1,4 +1,17 @@
+from heapq import *
+
+
 def findKthLargestDS1(nums: list[int], k: int) -> int:
     nums.sort(reverse=True)
     return nums[k - 1]
 
+
+def findKthLargestDS2(nums: list[int], k: int) -> int:
+    min_heap = []
+    for num in nums:
+        heappush(min_heap, num)
+
+        if len(min_heap) > k:
+            heappop(min_heap)
+    
+    return min_heap[0]
