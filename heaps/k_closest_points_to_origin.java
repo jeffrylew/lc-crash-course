863f488108792513dc88cf4e3cf0daf5695123b8
Jeffry <jeffryklew@gmail.com>
2023-08-11 18:31:56 -0400

    Implement divide and conquer solution in k_closest_points_to_origin.java

--- heaps/k_closest_points_to_origin.java
+++ heaps/k_closest_points_to_origin.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public int dist(int[] point)
 {
     return point[0] * point[0] + point[1] * point[1];
@@ -28,4 +30,86 @@ public int[][] kClosestDS1(int[][] points, int k)
     }
 
     return ans;
+}
+
+class Solution
+{
+    int[][] points;
+
+    public int[][] kClosestDS2(int[][] points, int k)
+    {
+        this.points = points;
+        sort(0, points.length - 1, k);
+        return Arrays.copyOfRange(points, 0, k);
+    }
+
+    public void sort(int i, int j, int k)
+    {
+        if (i >= j)
+        {
+            return;
+        }
+
+        int idx = ThreadLocalRandom.current().nextInt(i, j);
+        swap(i, idx);
+
+        int mid = partition(i, j);
+        int leftLength = mid - i + 1;
+
+        if (k < leftLength)
+        {
+            sort(i, mid - 1, k);
+        }
+        else if (k > leftLength)
+        {
+            sort(mid + 1, j, k - leftLength);
+        }
+    }
+
+    public int partition(int i, int j)
+    {
+        int oi = i;
+        int pivot = dist(i);
+        i++;
+
+        while (true)
+        {
+            while (i < j && dist(i) < pivot)
+            {
+                i++;
+            }
+
+            while (i <= j && dist(j) > pivot)
+            {
+                j--;
+            }
+
+            if (i >= j)
+            {
+                break;
+            }
+
+            swap(i, j);
+        }
+
+        swap(oi, j);
+        return j;
+    }
+
+    public int dist(int i)
+    {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
+    }
+
+    public void swap(int i, int j)
+    {
+        int t0 = points[i][0];
+        int t1 = points[i][1];
+
+        points[i][0] = points[j][0];
+        points[i][1] = points[j][1];
+
+        points[j][0] = t0;
+        points[j][1] = t1;
+    }
 }
