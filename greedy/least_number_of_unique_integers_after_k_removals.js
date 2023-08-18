c5971345ac507323b8d07087d87a74af4e564bb7
Jeffry <jeffryklew@gmail.com>
2023-08-18 19:38:50 -0400

    Implement least_number_of_unique_integers_after_k_removals.js

---
+++ greedy/least_number_of_unique_integers_after_k_removals.js
@@ -0,0 +1,32 @@
+var findLeastNumOfUniqueInts = function(arr, k)
+{
+    let counts = new Map();
+
+    for (const num of arr)
+    {
+        counts.set(num, (counts.get(num) || 0) + 1);
+    }
+
+    let ordered = [];
+    for (const val of counts.values())
+    {
+        ordered.push(val);
+    }
+
+    ordered.sort((a, b) => b - a);
+    while (k > 0)
+    {
+        let val = ordered[ordered.length - 1];
+        if (val <= k)
+        {
+            k -= val;
+            ordered.pop();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return ordered.length;
+}
