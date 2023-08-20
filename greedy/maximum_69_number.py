c3052fb35eaa0893d553bc4e0396430838a22d88
Jeffry <jeffryklew@gmail.com>
2023-08-20 13:22:50 -0400

    Implement built-in function discussion solution in greedy/maximum_69_number.py

--- greedy/maximum_69_number.py
+++ greedy/maximum_69_number.py
@@ -11,3 +11,11 @@ def maximum69NumberDS1(num: int) -> int:
     
     # Convert the modified char list to integer and return it
     return int("".join(num_char_list))
+
+def maximum69NumberDS2(num: int) -> int:
+    # Convert the input num to the string num_string
+    num_string = str(num)
+
+    # Use the built-in function to replace the first 6 with 9
+    # Return the integer converted from the modified num_string
+    return int(num_string.replace('6', '9', 1))
