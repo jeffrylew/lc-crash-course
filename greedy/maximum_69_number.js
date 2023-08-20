b18d60a2d29d5f2c372b2bfb6e4fb39b9f31df2e
Jeffry <jeffryklew@gmail.com>
2023-08-20 12:47:35 -0400

    Implement check the remainder discussion solution in greedy/maximum_69_number.js

--- greedy/maximum_69_number.js
+++ greedy/maximum_69_number.js
@@ -20,3 +20,33 @@ var maximum69NumberDS1 = function(num)
 
     return num;
 };
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69NumberDS2 = function(num)
+{
+    // Since we start with the lowest digit, initialize currDigit = 0
+    let numCopy = num;
+    let idxFirstSix = -1;
+    let idxCurrDigit = 0;
+
+    // Check every digit of numCopy from low to high
+    while (numCopy > 0)
+    {
+        // If the current digit is 6, record it as the highest digit of 6
+        if (numCopy % 10 == 6)
+        {
+            idxFirstSix = idxCurrDigit;
+        }
+
+        // Move onto next digit
+        numCopy = Math.floor(numCopy / 10);
+        idxCurrDigit++;
+    }
+
+    // If don't find any digit of 6, return original number
+    // Else, increment num by the difference made by the first 6
+    return idxFirstSix == -1 ? num : num + 3 * (10 ** idxFirstSix);
+};
