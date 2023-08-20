42502823ff03993554aed9bb70e3e298e2e80e40
Jeffry <jeffryklew@gmail.com>
2023-08-20 12:09:20 -0400

    Implement check the remainder discussion solution in greedy/maximum_69_number.java

--- greedy/maximum_69_number.java
+++ greedy/maximum_69_number.java
@@ -28,3 +28,29 @@ public int maximum69NumberDS2(int num)
     // Return the integer converted from the modified numString
     return Integer.parseInt(numString.replaceFirst("6", "9")); 
 }
+
+public int maximum69NumberDS3(int num)
+{
+    // Since start with lowest digit, initialize currDigit = 0
+    int numCopy = num;
+    int idxSix = -1;
+    int currDigit = 0;
+
+    // Check every digit of numCopy from low to high
+    while (numCopy > 0)
+    {
+        // If the current digit is 6, record it as the highest digit for 6
+        if (numCopy % 10 == 6)
+        {
+            idxSix = currDigit;
+        }
+
+        // Move onto next digit
+        numCopy /= 10;
+        currDigit++;
+    }
+
+    // If don't find any digit of 6, return original number
+    // Else, increment num by the difference made by the first 6
+    return idxSix == -1 ? num : num + 3 * (int)Math.pow(10, idxSix);
+}
