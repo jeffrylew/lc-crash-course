210dc5f1fa7df623fb6082303b8fbdcf0c9bc079
Jeffry <jeffryklew@gmail.com>
2023-08-23 19:04:18 -0400

    Implement priority queue discussion solution in maximum_units_on_a_truck.java

--- greedy/maximum_units_on_a_truck.java
+++ greedy/maximum_units_on_a_truck.java
@@ -16,3 +16,25 @@ public int maximumUnitsDS1(int[][] boxTypes, int truckSize)
 
     return unitCount;
 }
+
+public int maximumUnitsDS2(int[][] boxTypes, int truckSize)
+{
+    PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b)->b[1] - a[1]);
+    maxHeap.addAll(Arrays.asList(boxTypes));
+
+    int unitCount = 0;
+    while (!maxHeap.isEmpty())
+    {
+        int[] top = queue.poll();
+        int boxCount = Math.min(truckSize, top[0]);
+        unitCount += boxCount * top[1];
+        truckSize -= boxCount;
+
+        if (truckSize == 0)
+        {
+            break;
+        }
+    }
+
+    return unitCount;
+}
