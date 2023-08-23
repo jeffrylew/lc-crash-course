5b5b1952eedd0a2a8c8e356ab118e2ebb9c804a5
Jeffry <jeffryklew@gmail.com>
2023-08-23 18:59:07 -0400

    Implement array sort discussion solution in maximum_units_on_a_truck.java

---
+++ greedy/maximum_units_on_a_truck.java
@@ -0,0 +1,18 @@
+public int maximumUnitsDS1(int[][] boxTypes, int truckSize)
+{
+    Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+
+    int unitCount = 0;
+    for (int[] boxType : boxTypes)
+    {
+        int boxCount = Math.min(truckSize, boxType[0]);
+        unitCount += boxCount * boxType[1];
+        truckSize -= boxCount;
+        if (truckSize == 0)
+        {
+            break;
+        }
+    }
+
+    return unitCount;
+}
