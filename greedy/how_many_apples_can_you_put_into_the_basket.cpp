097d9ae5dd66498190f64d42674f17fb8e114882
Jeffry <jeffryklew@gmail.com>
2023-08-25 06:39:45 -0400

    Implement sort discussion solution in how_many_apples_can_you_put_into_the_basket.cpp

--- greedy/how_many_apples_can_you_put_into_the_basket.cpp
+++ greedy/how_many_apples_can_you_put_into_the_basket.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <functional>
 #include <queue>
 #include <vector>
@@ -32,9 +33,43 @@ static int maxNumberOfApplesFA(std::vector<int> weight)
     return num_apples;
 }
 
+//! @brief Discussion solution: sort
+//! @param[in] weight Vector of apple weights
+//! @return Max number of apples that can be put in the basket of 5000 units
+static int maxNumberOfApplesDS1(std::vector<int> weight)
+{
+    //! @details https://leetcode.com/problems/
+    //!          how-many-apples-can-you-put-into-the-basket/editorial/
+    //!
+    //!          Time complexity O(N * log N) where N = weight.size() for sort.
+    //!          Space complexity O(log N) for quicksort variation (editorial
+    //!          says O(1) because no additional data structures are used)
+
+    std::sort(weight.begin(), weight.end());
+
+    int apples {};
+    int units {};
+
+    for (const auto weight : weights)
+    {
+        units += weight;
+
+        if (units > 5000)
+        {
+            break;
+        }
+
+        ++apples;
+    }
+
+    return apples;
+}
+
 TEST(MaxNumberOfApplesTest, SampleTest)
 {
     EXPECT_EQ(4, maxNumberOfApplesFA({100, 200, 150, 1000}));
+    EXPECT_EQ(4, maxNumberOfApplesDS1({100, 200, 150, 1000}));
 
     EXPECT_EQ(5, maxNumberOfApplesFA({900, 950, 800, 1000, 700, 800}));
+    EXPECT_EQ(5, maxNumberOfApplesDS1({900, 950, 800, 1000, 700, 800}));
 }
