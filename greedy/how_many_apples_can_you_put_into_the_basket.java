c5a7f4e1362c97b6bf0f840ea1cb5297bf74702b
Jeffry <jeffryklew@gmail.com>
2023-08-30 17:48:02 -0400

    Implement min heap discussion solution in how_many_apples_can_you_put_into_the_basket.java

--- greedy/how_many_apples_can_you_put_into_the_basket.java
+++ greedy/how_many_apples_can_you_put_into_the_basket.java
@@ -13,3 +13,20 @@ public int maxNumberOfApplesDS1(int[] weight)
 
     return apples;
 }
+
+public int maxNumberOfApplesDS2(int[] weight)
+{
+    List<Integer> weights = Arrays.stream(arr).boxed().collect(Collectors.toList());
+    Queue<Integer> minHeap = new PriorityQueue<>(weights);
+
+    int apples = 0;
+    int units = 0;
+
+    while (!minHeap.isEmpty() && units + minHeap.peek() <= 5000)
+    {
+        units += minHeap.remove();
+        apples++;
+    }
+
+    return apples;
+}
