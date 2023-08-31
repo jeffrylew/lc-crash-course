300c62b4a4ba6bab1d83ae8f7f99ceedc60b853f
Jeffry <jeffryklew@gmail.com>
2023-08-31 18:29:04 -0400

    Implement bucket sort discussion solution in how_many_apples_can_you_put_into_the_basket.java

--- greedy/how_many_apples_can_you_put_into_the_basket.java
+++ greedy/how_many_apples_can_you_put_into_the_basket.java
@@ -30,3 +30,43 @@ public int maxNumberOfApplesDS2(int[] weight)
 
     return apples;
 }
+
+public int maxNumberOfApplesDS3(int[] weight)
+{
+    // Initialize the bucket to store all elements
+    int size = -1;
+    for (Integer ele : weight)
+    {
+        size = Math.max(size, ele);
+    }
+
+    int[] bucket = new int[size + 1];
+    for (Integer ele : weight)
+    {
+        bucket[ele]++;
+    }
+
+    int apples = 0;
+    int units = 0;
+    for (int i = 0; i < size + 1; i++)
+    {
+        // If we have apples of i units of weight
+        if (bucket[i] != 0)
+        {
+            // We need to make sure that:
+            // 1. We do not take more apples than those provided
+            // 2. We do not exceed 5000 units of weight
+            int take = Math.min(bucket[i], (5000 - units) / i);
+
+            if (take == 0)
+            {
+                break;
+            }
+
+            units += take * i;
+            apples += take;
+        }
+    }
+
+    return apples;
+}
