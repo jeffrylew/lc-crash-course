9d0ccefd169f080528bd745442bdfb73d5dda899
Jeffry <jeffryklew@gmail.com>
2023-09-01 06:38:07 -0400

    Implement min heap discussion solution in how_many_apples_can_you_put_into_the_basket.py

--- greedy/how_many_apples_can_you_put_into_the_basket.py
+++ greedy/how_many_apples_can_you_put_into_the_basket.py
@@ -1,3 +1,6 @@
+from heapq import *
+
+
 def maxNumberOfApplesDS1(weight: list[int]) -> int:
     weight.sort()
     apples = units = 0
@@ -8,6 +11,18 @@ def maxNumberOfApplesDS1(weight: list[int]) -> int:
         if units > 5000:
             break
 
+        apples += 1
+    
+    return apples
+
+
+def maxNumberOfApplesDS2(weight: list[int]) -> int:
+    heapify(weight)
+    apples = units = 0
+
+    # Note that arr[0] always represents the smallest element in the min-heap
+    while weight and units + weight[0] <= 5000:
+        units += heappop(weight)
         apples += 1
     
     return apples
