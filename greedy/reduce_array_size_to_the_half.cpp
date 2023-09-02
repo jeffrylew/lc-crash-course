8d07a79ecc26b8f39c5e14a0d263acdb90c16164
Jeffry <jeffryklew@gmail.com>
2023-09-02 10:43:10 -0400

    Implement optimized sorting discussion solution in reduce_array_size_to_the_half.cpp

--- greedy/reduce_array_size_to_the_half.cpp
+++ greedy/reduce_array_size_to_the_half.cpp
@@ -111,11 +111,82 @@ static int minSetSizeDS1(std::vector<int> arr)
 
 } // static int minSetSizeDS1( ...
 
+//! @brief Discussion solution: Optimized sorting
+//! @param[in] arr Vector of ints
+//! @return Min set size so at least half of ints in arr are removed
+static int minSetSizeDS2(std::vector<int> arr)
+{
+    //! @details leetcode.com/problems/reduce-array-size-to-the-half/editorial/
+    //!
+    //!          Time complexity O(N * log N), see DS1
+    //!          Space complexity O(1). Optimization involves writing counts
+    //!          into the input vector arr using the two pointer technique. Any
+    //!          extra space at the end should be zeroed (or deleted if using a
+    //!          language like Python). If the input vector cannot be modified
+    //!          then an alternative optimization would be to use an O(1) space
+    //!          sorting algorithm like heapsort or in-place merge sort. Would
+    //!          need to implement these yourself.
+
+    const auto arr_size = static_cast<int>(arr.size());
+
+    //! Sort the input numbers
+    std::sort(arr.begin(), arr.end());
+
+    //! Space complexity optimization
+    int currentRun {1};
+    int currentRunIdx {};
+    for (int i = 1; i < arr_size; ++i)
+    {
+        if (arr[i] == arr[i - 1])
+        {
+            ++currentRun;
+            continue;
+        }
+
+        arr[currentRunIdx++] = currentRun;
+        currentRun           = 1;
+    }
+    arr[currentRunIdx++] = currentRun;
+
+    arr.erase(arr.begin() + currentRunIdx, arr.end());
+    /**
+     * Can also set to zero instead of deleting elements at currentRunIdx to end
+     * 
+     * for (int i = currentRunIdx; i < arr_size; ++i)
+     * {
+     *     arr[i] = 0;
+     * }
+     */
+
+    //! Sort arr in decreasing/reverse order
+    std::sort(arr.begin(), arr.end(), std::greater<int>());
+
+    //! Remove numbers until at least half are removed
+    int numbersRemovedFromArr {};
+    int setSize {};
+
+    for (const int count : arr)
+    {
+        numbersRemovedFromArr += count;
+        ++setSize;
+
+        if (numbersRemovedFromArr >= arr_size / 2)
+        {
+            break;
+        }
+    }
+
+    return setSize;
+
+} // static int minSetSizeDS2( ...
+
 TEST(MinSetSizeTest, SampleTest1)
 {
     EXPECT_EQ(2, minSetSizeFA({3, 3, 3, 3, 5, 5, 5, 2, 2, 7}));
     EXPECT_EQ(2, minSetSizeDS1({3, 3, 3, 3, 5, 5, 5, 2, 2, 7}));
+    EXPECT_EQ(2, minSetSizeDS2({3, 3, 3, 3, 5, 5, 5, 2, 2, 7}));
 
     EXPECT_EQ(1, minSetSizeFA({7, 7, 7, 7, 7, 7}));
     EXPECT_EQ(1, minSetSizeDS1({7, 7, 7, 7, 7, 7}));
+    EXPECT_EQ(1, minSetSizeDS2({7, 7, 7, 7, 7, 7}));
 }
