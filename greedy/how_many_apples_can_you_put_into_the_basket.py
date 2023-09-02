53863132468b75d767bb92e3d1570280668ef8b1
Jeffry <jeffryklew@gmail.com>
2023-09-02 10:24:40 -0400

    Implement bucket sort solution in how_many_apples_can_you_put_into_the_basket.py

--- greedy/how_many_apples_can_you_put_into_the_basket.py
+++ greedy/how_many_apples_can_you_put_into_the_basket.py
@@ -26,3 +26,28 @@ def maxNumberOfApplesDS2(weight: list[int]) -> int:
         apples += 1
     
     return apples
+
+
+def maxNumberOfApplesDS3(weight: list[int]) -> int:
+    # Initialize the bucket to store all elements
+    size = max(weight) + 1
+    bucket = [0] * size
+    for ele in weight:
+        bucket[ele] += 1
+    
+    apples = units = 0
+    for i in range(size):
+        # If we have apples of i units of weight
+        if bucket[i] != 0:
+            # We need to make sure that:
+            # 1. We do not take more apples than those provided
+            # 2. We do not exceed 5000 units of weight
+            take = min(bucket[i], (5000 - units) // i)
+
+            if take == 0:
+                break
+
+            apples += take
+            units += take * i
+    
+    return apples
