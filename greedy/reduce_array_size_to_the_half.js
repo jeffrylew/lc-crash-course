c71604d8756ee1695e3f484e0cd8eb14b72be6d3
Jeffry <jeffryklew@gmail.com>
2023-09-04 12:36:38 -0400

    Implement bucket sort discussion solution in reduce_array_size_to_the_half.js

--- greedy/reduce_array_size_to_the_half.js
+++ greedy/reduce_array_size_to_the_half.js
@@ -147,3 +147,70 @@ var minSetSizeDS3 = function(arr)
 
     return setSize;
 };
+
+class CounterDS4
+{
+    _counts = new Map();
+
+    add(item)
+    {
+        if (!this._counts.has(item))
+        {
+            this._counts.set(item, 0);
+        }
+        this._counts.set(item, this._counts.get(item) + 1);
+        return this._counts.get(item);
+    }
+
+    values()
+    {
+        return this._counts.values();
+    }
+}
+
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+var minSetSizeDS4 = function(arr)
+{
+    const counts = new CounterDS4();
+
+    let maxCount = 0;
+    for (const value of arr)
+    {
+        const newCount = counts.add(value);
+        maxCount = Math.max(newCount, maxCount);
+    }
+
+    if (maxCount > arr.length / 2)
+    {
+        return 1;
+    }
+
+    // Allocate an array of size maxCount + 1, pre-filling each value to 0
+    const buckets = Array.from({ length: maxCount + 1}, () => 0);
+
+    for (const count of counts.values())
+    {
+        buckets[count]++;
+    }
+
+    let setSize = 0;
+    let numberToRemove = Math.floor(arr.length / 2);
+    let currentBucket = maxCount;
+
+    while (numberToRemove > 0)
+    {
+        const numberWantedFromCurrentBucket =
+            Math.ceil(numberToRemove / currentBucket);
+        const setSizeIncrease = 
+            Math.min(buckets[currentBucket], numberWantedFromCurrentBucket);
+        
+        setSize += setSizeIncrease;
+        numberToRemove -= setSizeIncrease * currentBucket;
+        currentBucket--;
+    }
+
+    return setSize;
+};
