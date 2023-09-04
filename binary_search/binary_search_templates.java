c92227d9cb55d811141a6f3d4a935b8ff147c3cf
Jeffry <jeffryklew@gmail.com>
2023-09-04 17:01:55 -0400

    Implement first index binary search in binary_search_templates.java

--- binary_search/binary_search_templates.java
+++ binary_search/binary_search_templates.java
@@ -26,3 +26,25 @@ public int binarySearch(int[] arr, int target)
     // Target is not in arr but left is at the insertion point
     return left;
 }
+
+public int binarySearchFirstIndex(int[] arr, int target)
+{
+    int left = 0;
+    int right = arr.length;
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] >= target)
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
