b2530d52755898c4ad5ed34adc635cb6ad93c7b4
Jeffry <jeffryklew@gmail.com>
2023-09-04 13:10:58 -0400

    Implement bucket sort discussion solution in reduce_array_size_to_the_half.py

--- greedy/reduce_array_size_to_the_half.py
+++ greedy/reduce_array_size_to_the_half.py
@@ -1,4 +1,5 @@
 import collections
+import math
 
 
 def minSetSizeDS1(arr: list[int]) -> int:
@@ -92,5 +93,32 @@ def minSetSizeDS3(arr: list[int]) -> int:
 
         if total_removed >= (len(arr) // 2):
             break
+    
+    return set_size
+
+
+def minSetSizeDS4(arr: list[int]) -> int:
+    # In Python, we can use the built-in Counter class
+    counts = collections.Counter(arr)
+    max_value = max(counts.values())
+
+    # Put the counts into buckets
+    buckets = [0] * (max_value + 1)
+
+    for count in counts.values():
+        buckets[count] += 1
+
+    # Determine set_size
+    set_size = 0
+    arr_numbers_to_remove = len(arr) // 2
+    bucket = max_value
+
+    while arr_numbers_to_remove > 0:
+        max_needed_from_bucket = math.ceil(arr_numbers_to_remove / bucket)
+        set_size_increase = min(buckets[bucket], max_needed_from_bucket)
+
+        set_size += set_size_increase
+        arr_numbers_to_remove -= set_size_increase * bucket
+        bucket -= 1
     
     return set_size
