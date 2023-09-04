f8ff2a9ec0570e1c9d4afb9ef922ee034ca7278a
Jeffry <jeffryklew@gmail.com>
2023-09-04 17:50:41 -0400

    Implement last index binary search in binary_search_templates.py

--- binary_search/binary_search_templates.py
+++ binary_search/binary_search_templates.py
@@ -31,3 +31,18 @@ def binary_search_first_index(arr: list[int], target: int) -> int:
             left = mid + 1
     
     return left
+
+
+def binary_search_last_index(arr: list[int], target: int) -> int:
+    left = 0
+    right = len(arr)
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if arr[mid] > target:
+            right = mid
+        else:
+            left = mid + 1
+    
+    return left
