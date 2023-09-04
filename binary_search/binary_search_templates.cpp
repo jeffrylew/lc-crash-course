c8286abee2ccc274ebc27e9024ef573ca2fa083b
Jeffry <jeffryklew@gmail.com>
2023-09-04 16:32:27 -0400

    Implement first index binary search in binary_search_templates.cpp

--- binary_search/binary_search_templates.cpp
+++ binary_search/binary_search_templates.cpp
@@ -42,9 +42,44 @@ static int binarySearch(std::vector<int> arr, int target)
 
 } // static int binarySearch( ...
 
+//! @brief Get left-most index of target appearing multiple times in arr
+//! @param[in] arr Sorted vector of integers containing duplicates
+//! @return Left-most index of target if it appears multiple times
+static int binarySearchFirstIndex(std::vector<int> arr, int target)
+{
+    //! @details https://leetcode.com/explore/interview/card/
+    //!          leetcodes-interview-crash-course-data-structures-and-algorithms
+    //!          /710/binary-search/4696/
+
+    int  left {};
+    auto right = static_cast<int>(arr.size());
+
+    while (left < right)
+    {
+        const int mid {left + (right - left) / 2};
+
+        if (arr[mid] >= target)
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+
+} // static int binarySearchFirstIndex( ...
+
 TEST(BinarySearchTest, SampleTest)
 {
     EXPECT_EQ(2, binarySearch({1, 2, 3, 4, 5}, 3));
 
     EXPECT_EQ(1, binarySearch({1, 3, 4, 5, 6}, 2));
+}
+
+TEST(BinarySearchFirstIndexTest, SampleTest)
+{
+    EXPECT_EQ(3, binarySearchFirstIndex({1, 2, 3, 4, 4}, 4));
 }
