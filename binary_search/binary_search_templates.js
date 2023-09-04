55407b69b10c13538908fa572e360e25d578087e
Jeffry <jeffryklew@gmail.com>
2023-09-04 17:27:56 -0400

    Implement binary search template in binary_search_templates.js

---
+++ binary_search/binary_search_templates.js
@@ -0,0 +1,27 @@
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right)
+    {
+        const mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] == target)
+        {
+            // Do something
+            return mid;
+        }
+
+        if (arr[mid] > target)
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    // Target is not in arr but left is at the insertion point
+    return left;
+};
