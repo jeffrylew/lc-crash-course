78079c6d48c870391368486ea49c738bdb16cb2f
Jeffry <jeffryklew@gmail.com>
2023-09-04 13:00:52 -0400

    Implement optimized sorting discussion solution in reduce_array_size_to_the_half.py

--- greedy/reduce_array_size_to_the_half.py
+++ greedy/reduce_array_size_to_the_half.py
@@ -29,3 +29,43 @@ def minSetSizeDS1(arr: list[int]) -> int:
             break
     
     return set_size
+
+
+def minSetSizeDS2(arr: list[int]) -> int:
+    # Store initial input length
+    arr_len = len(arr)
+
+    # Sort the input numbers
+    arr.sort()
+
+    # Space complexity optimization
+    current_run = 1
+    current_run_idx = 0
+    for i in range(1, arr_len):
+        if arr[i] == arr[i - 1]:
+            current_run += 1
+            continue
+
+        arr[current_run_idx] = current_run
+        current_run_idx += 1
+        current_run = 1
+    arr[current_run_idx] = current_run
+    current_run_idx += 1
+
+    del arr[current_run_idx:]
+
+    # Reverse sort the counts
+    arr.sort(reverse=True)
+
+    # Remove numbers until at least half are removed
+    numbers_removed_from_arr = 0
+    set_size = 0
+
+    for count in arr:
+        numbers_removed_from_arr += count
+        set_size += 1
+
+        if numbers_removed_from_arr >= arr_len // 2:
+            break
+    
+    return set_size
