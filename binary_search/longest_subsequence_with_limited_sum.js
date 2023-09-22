9653de1532606ccb9a1bd51fc1dc0d809db28ea1
Jeffry <jeffryklew@gmail.com>
2023-09-22 06:54:52 -0400

    Implement prefix sum discussion solution in longest_subsequence_with_limited_sum.js

--- binary_search/longest_subsequence_with_limited_sum.js
+++ binary_search/longest_subsequence_with_limited_sum.js
@@ -37,3 +37,51 @@ var answerQueriesDS1 = function(nums, queries)
 
     return answer;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var answerQueriesDS2 = function(nums, queries)
+{
+    const binarySearch = (nums, target) => {
+        let left = 0;
+        let right = nums.length;
+
+        while (left < right)
+        {
+            const mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] > target)
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    // Get the prefix sum array of the sorted nums
+    nums.sort((a, b) => a - b);
+
+    for (let i = 1; i < nums.length; i++)
+    {
+        nums[i] += nums[i - 1];
+    }
+
+    // For each query, find its insertion index in the prefix sum array
+    const n = queries.length;
+    let answer = [];
+
+    for (const query of queries)
+    {
+        answer.push(binarySearch(nums, query));
+    }
+
+    return answer;
+};
