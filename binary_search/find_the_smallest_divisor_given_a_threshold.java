606a93886d343f5984adf7040e19cd22b3ab69ba
Jeffry <jeffryklew@gmail.com>
2023-10-10 18:56:50 -0400

    Implement binary search discussion solution in find_the_smallest_divisor_given_a_threshold.java

--- binary_search/find_the_smallest_divisor_given_a_threshold.java
+++ binary_search/find_the_smallest_divisor_given_a_threshold.java
@@ -32,3 +32,49 @@ public int smallestDivisorDS1(int[] nums, int threshold)
 
     return -1;
 }
+
+// Return the sum of division results with divisor
+public int findDivisionSum(int[] nums, int divisor)
+{
+    int result = 0;
+    for (int num : nums)
+    {
+        result += Math.ceil((1.0 * num) / divisor);
+    }
+    return result;
+}
+
+public int smallestDivisorDS2(int[] nums, int threshold)
+{
+    int ans = -1;
+
+    int low = 1;
+    int high = nums[0];
+    for (int num : nums)
+    {
+        high = Math.max(high, num);
+    }
+
+    // Iterate using binary search on all divisors
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int result = findDivisionSum(nums, mid);
+
+        // If current divisor does not exceed threshold then it can be answer
+        // Also try smaller divisors so change search space to left half
+        if (result <= threshold)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            // Otherwise need a bigger divisor to reduce the result sum
+            // so change search space to right half
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
