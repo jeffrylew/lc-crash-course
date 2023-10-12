cf6e702731a14cb9433a1d767959da61a0f95fd8
Jeffry <jeffryklew@gmail.com>
2023-10-12 17:10:25 -0400

    Implement binary search discussion solution in find_the_smallest_divisor_given_a_threshold.js

--- binary_search/find_the_smallest_divisor_given_a_threshold.js
+++ binary_search/find_the_smallest_divisor_given_a_threshold.js
@@ -33,3 +33,48 @@ const smallestDivisorDS1 = function(nums, threshold) {
 
     return -1;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @returns {number}
+ */
+const smallestDivisorDS2 = function(nums, threshold) {
+    const findDivisionSum = (divisor) => {
+        let result = 0;
+
+        for (const num of nums)
+        {
+            result += Math.ceil((1.0 * num) / divisor);
+        }
+
+        return result;
+    }
+
+    let ans = -1;
+    let low = 1;
+    let high = nums.reduce((a, b) => Math.max(a, b), nums[0]);
+
+    // Iterate using binary search on all divisors
+    while (low <= high)
+    {
+        const mid = Math.floor((low + high) / 2);
+        const result = findDivisionSum(nums, mid);
+
+        // If current divisor does not exceed threshold then it can be answer
+        // but also try smaller divisors, change search space to left half
+        if (result <= threshold)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            // Otherwise need a bigger divisor to reduct the result sum
+            // so change search space to right half
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+};
