03907ae13af730b365c73e61330e1817160d8a0e
Jeffry <jeffryklew@gmail.com>
2023-10-14 09:12:59 -0400

    Implement binary search discussion solution in find_the_smallest_divisor_given_a_threshold.py

--- binary_search/find_the_smallest_divisor_given_a_threshold.py
+++ binary_search/find_the_smallest_divisor_given_a_threshold.py
@@ -22,3 +22,33 @@ def smallestDivisorDS1(nums: list[int], threshold: int) -> int:
             return divisor
     
     return -1
+
+def smallestDivisorDS2(nums: list[int], threshold: int) -> int:
+    # Return the sum of division results with divisor
+    def find_division_sum(divisor: int) -> int:
+        result = 0
+        for num in nums:
+            result += math.ceil((1.0 * num) / divisor)
+        return result
+    
+    ans = -1
+    low = 1
+    high = max(nums)
+
+    # Iterate using binary search on all divisors
+    while low <= high:
+        mid = (low + high) // 2
+        result = find_division_sum(mid)
+
+        # If current divisor does not exceed threshold then it can be the answer
+        # but also try smaller divisors - change search space to left half
+        if result <= threshold:
+            ans = mid
+            high = mid - 1
+        
+        # Otherwise need a bigger divisor to reduce result sum
+        # so change search space to right half
+        else:
+            low = mid + 1
+    
+    return ans
