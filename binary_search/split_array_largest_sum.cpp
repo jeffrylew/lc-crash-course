2a708fe02f55ac01ed444999ec06f88a58271671
Jeffry <jeffryklew@gmail.com>
2023-10-20 23:39:38 -0400

    Implement skeleton for binary_search/split_array_largest_sum.cpp

---
+++ binary_search/split_array_largest_sum.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+//! @brief Split nums into k non-empty subarrays so largest sum is minimized
+//! @param[in] nums Vector of ints
+//! @param[in] k    Number of non-empty subarrays to split
+//! @return Minimized largest sum of the split
+static int splitArrayFA(std::vector<int> nums, int k)
+{
+    //! @todo
+}
+
+TEST(SplitArrayTest, SampleTest)
+{
+    EXPECT_EQ(18, splitArrayFA({7, 2, 5, 10, 8}, 2));
+
+    EXPECT_EQ(9, splitArrayFA({1, 2, 3, 4, 5}, 2));
+}
