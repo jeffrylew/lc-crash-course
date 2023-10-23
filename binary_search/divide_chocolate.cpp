3670b4f25e5fcf066f22568d899936856b1ab59c
Jeffry <jeffryklew@gmail.com>
2023-10-22 21:18:27 -0400

    WIP Partially implement first attempt solution in binary_search/divide_chocolate.cpp

--- binary_search/divide_chocolate.cpp
+++ binary_search/divide_chocolate.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <numeric>
 #include <vector>
 
 //! @brief Get max total sweetness of piece by cutting bar optimally
@@ -8,7 +9,34 @@
 //! @return Max total sweetness of piece with minimum total sweetness
 static int maximizeSweetnessFA(std::vector<int> sweetness, int k)
 {
-    //! @todo
+    //! @details
+
+    const auto sweet_size = static_cast<int>(sweetness.size());
+    auto       prefix_sum = sweetness;
+
+    for (int i = 1; i < sweet_size; ++i)
+    {
+        prefix_sum [i] += prefix_sum[i - 1];
+    }
+
+    //! Largest chunk size
+    int largest_chunk {sweet_size - k};
+
+    //! Keep track of maximum of minimum total sweetness
+    int min_total_sweetness {};
+
+    while (largest_chunk > 0)
+    {
+        int left {};
+        for (int right = largest_chunk - 1; right < sweet_size; ++right)
+        {
+            //! @todo Think more
+        }
+
+        --largest_chunk;
+    }
+
+    return min_total_sweetness;
 }
 
 TEST(MaximizeSweetnessTest, SampleTest1)
