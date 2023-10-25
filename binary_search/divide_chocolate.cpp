77af4108324939f3fc845fd09479ec7f14ad3d97
Jeffry <jeffryklew@gmail.com>
2023-10-25 18:21:57 -0400

    Update first attempt solution in binary_search/divide_chocolate.cpp

--- binary_search/divide_chocolate.cpp
+++ binary_search/divide_chocolate.cpp
@@ -11,7 +11,7 @@
 //! @return Max total sweetness of piece with minimum total sweetness
 static int maximizeSweetnessFA(std::vector<int> sweetness, int k)
 {
-    //! @details
+    //! @details This solution is incorrect
 
     const auto sweet_size = static_cast<int>(sweetness.size());
     auto       prefix_sum = sweetness;
@@ -30,7 +30,7 @@ static int maximizeSweetnessFA(std::vector<int> sweetness, int k)
     while (largest_chunk > 0)
     {
         int left {};
-        for (int right = largest_chunk - 1; right < sweet_size; ++right)
+        for (int right = largest_chunk; right < sweet_size; ++right)
         {
             min_total_sweetness =
                 std::min(min_total_sweetness,
@@ -49,9 +49,9 @@ static int maximizeSweetnessFA(std::vector<int> sweetness, int k)
 
 TEST(MaximizeSweetnessTest, SampleTest1)
 {
-    EXPECT_EQ(6, maximizeSweetnessFA({1, 2, 3, 4, 5, 6, 7, 8, 9}, 5));
+    EXPECT_NE(6, maximizeSweetnessFA({1, 2, 3, 4, 5, 6, 7, 8, 9}, 5));
 
     EXPECT_EQ(1, maximizeSweetnessFA({5, 6, 7, 8, 9, 1, 2, 3, 4}, 8));
 
-    EXPECT_EQ(5, maximizeSweetnessFA({1, 2, 2, 1, 2, 2, 1, 2, 2}, 2));
+    EXPECT_NE(5, maximizeSweetnessFA({1, 2, 2, 1, 2, 2, 1, 2, 2}, 2));
 }
