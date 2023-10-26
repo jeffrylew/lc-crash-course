234ad311354a9b3acf8e683bd20dda39a721d6d3
Jeffry <jeffryklew@gmail.com>
2023-10-26 19:41:29 -0400

    Implement second attempt solution in divide_chocolate.java

--- binary_search/divide_chocolate.java
+++ binary_search/divide_chocolate.java
@@ -47,3 +47,57 @@ public int maximizeSweetness(int[] sweetness, int k)
     // target value which is the maximum possible sweetness
     return right;
 }
+
+public int maximizeSweetnessSA(int[] sweetness, int k)
+{
+    // Second attempt solution based off the discussion solution more closely
+    // matches binarySearch() in binary_search_templates.java
+
+    // Initialize the left and right boundaries
+    // left = 1
+    // right = total sweetness / number of people
+    int numPeople = k + 1;
+    int left = Arrays.stream(sweetness).min().getAsInt();
+    int right = Arrays.stream(sweetness).sum() / numPeople;
+
+    int max_min_total_sweetness = 0;
+
+    while (left <= right)
+    {
+        // Get the midle index between left and right boundary indices
+        // curSweetness = total sweetness for the current person
+        // peopleWithChocolate = number of people that have a piece of
+        // chocoloate with sweetness greater than or equal to mid
+        int mid = left + (right - left) / 2;
+        int curSweetness = 0;
+        int peopleWithChocolate = 0;
+
+        // Start assigning chunks
+        for (int chunk_sweetness : sweetness)
+        {
+            curSweetness += chunk_sweetness;
+
+            // If the total sweetness is no less than mid then
+            // move on to assigning chunks for the next person
+            if (curSweetness >= mid)
+            {
+                peopleWithChocolate++;
+                curSweetness = 0;
+            }
+        }
+
+        // Check if we successfully gave everyone a piece of chocolate with
+        // sweetness no less than mid and eliminate the search space by half
+        if (peopleWithChocolate >= numPeople)
+        {
+            max_min_total_sweetness = mid;
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return max_min_total_sweetness;
+}
