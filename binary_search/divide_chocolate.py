6c5655ab043ba742056e97e80f3a2d960305ad3c
Jeffry <jeffryklew@gmail.com>
2023-10-28 08:35:20 -0400

    Implement binary search + greedy discussion solution in binary_search/divide_chocolate.py

---
+++ binary_search/divide_chocolate.py
@@ -0,0 +1,33 @@
+def maximizeSweetness(sweetness: list[int], k: int) -> int:
+    # Initialize the left and right boundaries
+    # left = 1
+    # right = (total sweetness) / (number of people)
+    number_of_people = k + 1
+    left = min(sweetness)
+    right = sum(sweetness) // number_of_people
+
+    while left < right:
+        # Get middle index between left and right boundary indices
+        # cur_sweetness = total sweetness for current person
+        # people_with_chocolate = number of people that have a piece of
+        # chocolate with sweetness greater than or equal to mid
+        mid = (left + right + 1) // 2
+        cur_sweetness = 0
+        people_with_chocolate = 0
+
+        # Start assigning chunks to current person
+        for chunk_sweetness in sweetness:
+            cur_sweetness += chunk_sweetness
+
+            # If total sweetness is no less than mid then can break off current
+            # piece and move on to assigning chunks to the next person
+            if cur_sweetness >= mid:
+                people_with_chocolate += 1
+                cur_sweetness = 0
+
+        if people_with_chocolate >= number_of_people:
+            left = mid
+        else:
+            right = mid - 1
+
+    return right
