c98c97ca5b380c6f98fa9b1d74d2af809e5269f8
Jeffry <jeffryklew@gmail.com>
2023-11-07 19:08:04 -0500

    Implement bottom-up dynamic programming discussion solution in split_array_largest_sum.js

--- binary_search/split_array_largest_sum.js
+++ binary_search/split_array_largest_sum.js
@@ -8,9 +8,9 @@ var splitArrayDS1 = function(nums, k)
     const memo = new Array(1001).fill(-1).map(() => new Array(51).fill(-1));
 
     const nums_len = nums.length;
-    const prefixSum = new Array(nums.length + 1).fill(0);
+    const prefixSum = new Array(nums_len + 1).fill(0);
 
-    for (let i = 0; i < nums.length; i++)
+    for (let i = 0; i < nums_len; i++)
     {
         prefixSum[i + 1] = prefixSum[i] + nums[i];
     }
@@ -58,4 +58,65 @@ var splitArrayDS1 = function(nums, k)
     };
 
     return getMinimumLargestSplitSum(prefixSum, 0, k);
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var splitArrayDS2 = function(nums, k)
+{
+    const memo = new Array(1001).fill(-1).map(() => new Array(51).fill(-1));
+
+    const nums_len = nums.length;
+    const prefixSum = new Array(nums_len + 1).fill(0);
+
+    for (let i = 0; i < nums_len; i++)
+    {
+        prefixSum[i + 1] = prefixSum[i] + nums[i];
+    }
+
+    for (let subarrCount = 1; subarrCount <= k; subarrCount++)
+    {
+        for (let currIdx = 0; currIdx < nums_len; currIdx++)
+        {
+            // Base Case: If there is only one subarray left then all remaining
+            // numbers must go into the current subarray. Return sum of the
+            // remaining numbers
+            if (subarrCount == 1)
+            {
+                memo[currIdx][subarrCount] =
+                    prefixSum[nums_len] - prefixSum[currIdx];
+                continue;
+            }
+
+            // Otherwise, use recurrence relation to determine the min largest
+            // subarray sum between currIdx and end of nums with subarrCount
+            // subarrays remaining
+            let minimumLargestSplitSum = Number.MAX_SAFE_INTEGER;
+            for (let i = currIdx; i <= nums_len - subarrCount; i++)
+            {
+                // Store the sum of the first subarray
+                const firstSplitSum = prefixSum[i + 1] - prefixSum[currIdx];
+
+                // Find the maximum subarray sum for the current first split
+                const largestSplitSum =
+                    Math.max(firstSplitSum, memo[i + 1][subarrCount - 1]);
+
+                // Find the minimum among all possible combinations
+                minimumLargestSplitSum =
+                    Math.min(minimumLargestSplitSum, largestSplitSum);
+
+                if (firstSplitSum >= minimumLargestSplitSum)
+                {
+                    break;
+                }
+            }
+
+            memo[currIdx][subarrCount] = minimumLargestSplitSum;
+        }
+    }
+
+    return memo[0][k];
 };
