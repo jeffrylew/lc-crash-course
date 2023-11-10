698b3a640ce2a52f4e86fa75d50ef43550e70748
Jeffry <jeffryklew@gmail.com>
2023-11-10 07:04:14 -0500

    Implement binary search discussion solution in split_array_largest_sum.java

--- binary_search/split_array_largest_sum.java
+++ binary_search/split_array_largest_sum.java
@@ -122,4 +122,67 @@ class Solution
 
         return memoDS2[0][k];
     }
+
+    private int minimumSubarraysRequired(int[] nums, int maxSumAllowed)
+    {
+        int currentSum = 0;
+        int splitsRequired = 0;
+
+        for (int element : nums)
+        {
+            // Add element only if the sum doesn't exceed maxSumAllowed
+            if (currentSum + element <= maxSumAllowed)
+            {
+                currentSum += element;
+            }
+            else
+            {
+                // If the element addition makes sum more than maxSumAllowed
+                // then increment the splits required and reset sum
+                currentSum = element;
+                splitsRequired++;
+            }
+        }
+
+        // Return the number of subarrays, which is the number of splits + 1
+        return splitsRequired + 1;
+    }
+
+    public int splitArrayDS3(int[] nums, int k)
+    {
+        // Find the sum of all elements and the maximum element
+        int sum = 0;
+        int maxElement = Integer.MIN_VALUE;
+        for (int element : nums)
+        {
+            sum += element;
+            maxElement = Math.max(maxElement, element);
+        }
+
+        // Define the left and right boundaries of binary search
+        int left = maxElement;
+        int right = sum;
+        int minimumLargestSplitSum = 0;
+
+        while (left <= right)
+        {
+            // Find the mid value
+            int maxSumAllowed = left + (right - left) / 2;
+
+            // Find the minimum splits. If splitsRequired is less than
+            // or equal to k then move towards smaller values (left)
+            if (minimumSubarraysRequired(nums, maxSumAllowed) <= k)
+            {
+                right = maxSumAllowed - 1;
+                minimumLargestSplitSum = maxSumAllowed;
+            }
+            else
+            {
+                // Move towards right if splitsRequired is more than k
+                left = maxSumAllowed + 1;
+            }
+        }
+
+        return minimumLargestSplitSum;
+    }
 }
