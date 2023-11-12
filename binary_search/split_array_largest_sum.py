0f766ebfd744b8d3661bd85093ad7aaf453be3b6
Jeffry <jeffryklew@gmail.com>
2023-11-12 08:26:14 -0500

    Implement binary search discussion solution in split_array_largest_sum.py

--- binary_search/split_array_largest_sum.py
+++ binary_search/split_array_largest_sum.py
@@ -77,3 +77,40 @@ def splitArrayDS2(nums: list[int], k: int) -> int:
             memo[curr_idx][subarr_count] = minimum_largest_split_sum
 
     return memo[0][k]
+
+
+def splitArrayDS3(nums: list[int], k: int) -> int:
+    def min_subarrays_required(max_sum_allowed: int) -> int:
+        current_sum = 0
+        splits_required = 0
+
+        for element in nums:
+            # Add element only if the sum doesn't exceed max_sum_allowed
+            if current_sum + element <= max_sum_allowed:
+                current_sum += element
+            else:
+                # If the element addition makes sum greater than max_sum_allowed
+                # then increment the splits required and reset sum
+                current_sum = element
+                splits_required += 1
+
+        # Return the number of subarrays, which is the number of splits + 1
+        return splits_required + 1
+
+    # Define the left and right boundaries of binary search
+    left = max(nums)
+    right = sum(nums)
+    while left <= right:
+        # Find the mid value
+        max_sum_allowed = (left + right) // 2
+
+        # Find the minimum splits. If splits_required is less than
+        # or equal to k then move towards smaller values (left)
+        if min_subarrays_required(max_sum_allowed) <= k:
+            right = max_sum_allowed - 1
+            minimum_largest_split_sum = max_sum_allowed
+        else:
+            # Move towards right if splits_required is more than k
+            left = max_sum_allowed + 1
+
+    return minimum_largest_split_sum
