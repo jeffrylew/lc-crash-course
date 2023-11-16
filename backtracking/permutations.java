715c89d1850a98b1337c84a7621995617b2efc0f
Jeffry <jeffryklew@gmail.com>
2023-11-16 07:13:03 -0500

    Implement backtracking/permutations.java

---
+++ backtracking/permutations.java
@@ -0,0 +1,25 @@
+public void backtrack(List<Integer> curr, List<List<Integer>> ans, int[] nums)
+{
+    if (curr.size() == nums.length)
+    {
+        ans.add(new ArrayList<>(curr));
+        return;
+    }
+
+    for (int num : nums)
+    {
+        if (!curr.contains(num))
+        {
+            curr.add(num);
+            backtrack(curr, ans, nums);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
+
+public List<List<Integer>> permute(int[] nums)
+{
+    List<List<Integer>> ans = new ArrayList<>();
+    backtrack(new ArrayList<>(), ans, nums);
+    return ans;
+}
