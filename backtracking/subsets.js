4c0ab5e6aa2c2fa368c49b65ecf5215831c7ab71
Jeffry <jeffryklew@gmail.com>
2023-11-22 20:50:15 -0500

    Implement backtracking/subsets.js

---
+++ backtracking/subsets.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @returns {number[][]}
+ */
+var permute = function(nums) {
+    const backtrack = curr => {
+        if (curr.length == nums.length)
+        {
+            ans.push([...curr]);
+            return;
+        }
+
+        for (const num of nums)
+        {
+            if (!curr.includes(num))
+            {
+                curr.push(num);
+                backtrack(curr);
+                curr.pop();
+            }
+        }
+    }
+
+    let ans = [];
+    backtrack([]);
+    return ans;
+}
