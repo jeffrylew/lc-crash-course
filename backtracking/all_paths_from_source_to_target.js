2b75600980f85bbb6a2f1e0703b0524533de7f0b
Jeffry <jeffryklew@gmail.com>
2023-12-04 18:39:37 -0500

    Implement top-down dynamic programming solution in all_paths_from_source_to_target.js

--- backtracking/all_paths_from_source_to_target.js
+++ backtracking/all_paths_from_source_to_target.js
@@ -23,3 +23,39 @@ var allPathsSourceTargetDS1 = function(graph) {
     backtrack(0);
     return results;
 };
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTargetDS2 = function(graph) {
+    const allPathsToTarget = (currNode) => {
+        if (memo.has(currNode))
+        {
+            return memo.get(currNode);
+        }
+
+        let results = [];
+        if (currNode == target)
+        {
+            results.push([target]);
+        }
+        else
+        {
+            for (const nextNode of graph[currNode])
+            {
+                for (const path of allPathsToTarget(nextNode))
+                {
+                    results.push([currNode, ...path]);
+                }
+            }
+        }
+
+        memo.set(currNode, results);
+        return results;
+    }
+
+    const target = graph.length - 1;
+    let memo = new Map();
+    return allPathsToTarget(0);
+};
