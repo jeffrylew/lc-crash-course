b211f20e24ec1d6ba7b5874a02310e70a8d9ad08
Jeffry <jeffryklew@gmail.com>
2023-12-18 22:38:49 -0500

    Implement brute force discussion solution in backtracking/generate_parentheses.cpp

--- backtracking/generate_parentheses.cpp
+++ backtracking/generate_parentheses.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <functional>
+#include <queue>
 #include <string>
 #include <vector>
 
@@ -89,12 +90,79 @@ static std::vector<std::string> generateParenthesesFA(int n)
     return combinations;
 }
 
+//! @brief Brute force discussion solution
+//! @param[in] n Number of pairs of parentheses
+//! @return Vector of all combinations of well-formed parentheses
+static std::vector<std::string> generateParenthesesDS1(int n)
+{
+    //! @details https://leetcode.com/problems/generate-parentheses/editorial/
+    //!
+    //!          Time complexity O(2 ^ (2n) * n). We generate all possible
+    //!          strings of length 2n. At each character, there are two choices
+    //!          '(' or ')', which means there are a total of 2 ^ (2n) unique
+    //!          strings. For each string of length 2n, need to iterate through
+    //!          to verify it is a valid combination, which takes O(n) average.
+    //!          Space complexity O(2 ^ (2n) * n). Before dequeueing the first
+    //!          string of length 2n, it has stored 2^(2n - 1) strings of length
+    //!          n - 1, which takes O(2 ^ (2n) * n).
+
+    std::vector<std::string> answer {};
+    std::queue<std::string> queue {""};
+
+    std::function<bool(const std::string&)> isValid =
+        [&](const std::string& pString) {
+            int leftCount {};
+
+            for (const char p : pString)
+            {
+                if (p == '(')
+                {
+                    ++leftCount;
+                }
+                else
+                {
+                    --leftCount;
+                }
+
+                if (leftCount < 0)
+                {
+                    return false;
+                }
+            }
+
+            return leftCount == 0;
+        };
+
+    while (!queue.empty())
+    {
+        const auto curString = queue.front();
+        queue.pop();
+
+        //! If the length of curString is 2 * n,
+        //! add it to 'answer' if it is valid
+        if (static_cast<int>(curString.size()) == 2 * n)
+        {
+            if (isValid(curString))
+            {
+                answer.push_back(curString);
+            }
+            continue;
+        }
+
+        queue.push(curString + ")");
+        queue.push(curString + "(");
+    }
+
+    return answer;
+}
+
 TEST(GenerateParenthesesTest, SampleTest1)
 {
     const std::vector<std::string> expected_output {
         "((()))", "(()())", "(())()", "()(())", "()()()"};
 
     EXPECT_NE(expected_output, generateParenthesesFA(3));
+    EXPECT_EQ(expected_output, generateParenthesesDS1(3));
 }
 
 TEST(generateParenthesesTest, SampleTest2)
@@ -102,4 +170,5 @@ TEST(generateParenthesesTest, SampleTest2)
     const std::vector<std::string> expected_output {"()"};
 
     EXPECT_NE(expected_output, generateParenthesesFA(1));
+    EXPECT_EQ(expected_output, generateParenthesesDS1(1));
 }
