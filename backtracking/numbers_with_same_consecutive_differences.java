51495a40700fee36281d06754b188fd130b4c78f
Jeffry <jeffryklew@gmail.com>
2023-12-23 08:23:27 -0500

    Implement BFS discussion solution in numbers_with_same_consecutive_differences.java

--- backtracking/numbers_with_same_consecutive_differences.java
+++ backtracking/numbers_with_same_consecutive_differences.java
@@ -40,3 +40,45 @@ public int[] numsSameConsecDiffDS1(int n, int k)
     // Convert ArrayList to int[]
     return results.stream().mapToInt(i -> i).toArray();
 }
+
+public int[] numsSameConsecDiffDS2(int n, int k)
+{
+    if (n == 1)
+    {
+        return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    }
+
+    List<Integer> queue = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    for (int level = 1; level < n; ++level)
+    {
+        ArrayList<Integer> nextQueue = new ArrayList<>();
+
+        // Iterate through each number within the level
+        for (Integer num : queue)
+        {
+            Integer tailDigit = num % 10;
+
+            ArrayList<Integer> nextDigits = new ArrayList<>();
+            nextDigits.add(tailDigit + k);
+            if (k != 0)
+            {
+                nextDigits.add(tailDigit - k);
+            }
+
+            for (Integer nextDigit : nextDigits)
+            {
+                if (0 <= nextDigit && nextDigit < 10)
+                {
+                    Integer newNum = num * 10 + nextDigit;
+                    nextQueue.add(newNum);
+                }
+            }
+        }
+
+        // Prepare for the next level
+        queue = nextQueue;
+    }
+
+    return queue.stream().mapToInt(i -> i).toArray();
+}
