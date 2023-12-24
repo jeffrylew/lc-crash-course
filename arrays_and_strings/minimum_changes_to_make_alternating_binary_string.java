18d8da7b8d6bf6cbe3309624f56a69aabc772b9b
Jeffry <jeffryklew@gmail.com>
2023-12-24 11:08:45 -0500

    Implement second discussion solution in minimum_changes_to_make_alternating_binary_string.java

--- arrays_and_strings/minimum_changes_to_make_alternating_binary_string.java
+++ arrays_and_strings/minimum_changes_to_make_alternating_binary_string.java
@@ -31,3 +31,28 @@ public int minOperationsDS1(String s)
 
     return Math.min(start0, start1);
 }
+
+public int minOperationsDS2(String s)
+{
+    int start0 = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            if (s.charAt(i) == '1')
+            {
+                start0++;
+            }
+        }
+        else
+        {
+            if (s.charAt(i) == '0')
+            {
+                start0++;
+            }
+        }
+    }
+
+    return Math.min(start0, s.length() - start0);
+}
