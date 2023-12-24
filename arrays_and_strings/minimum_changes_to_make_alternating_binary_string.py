bf8c14a58a34f7a48e25533f0e0145d8d2d15a7c
Jeffry <jeffryklew@gmail.com>
2023-12-24 11:16:15 -0500

    Implement first discussion solution in minimum_changes_to_make_alternating_binary_string.py

---
+++ arrays_and_strings/minimum_changes_to_make_alternating_binary_string.py
@@ -0,0 +1,17 @@
+def minOperationsDS1(s: str) -> int:
+    start0 = 0
+    start1 = 0
+
+    for i in range(len(s)):
+        if i % 2 == 0:
+            if s[i] == "0":
+                start1 += 1
+            else:
+                start0 += 1
+        else:
+            if s[i] == "1":
+                start1 += 1
+            else:
+                start0 += 1
+
+    return min(start0, start1)
