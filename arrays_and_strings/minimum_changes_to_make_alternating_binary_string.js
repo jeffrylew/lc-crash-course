ecfffbf1afb061611d6ae83c3c90f87646d6bf6f
Jeffry <jeffryklew@gmail.com>
2023-12-24 11:13:38 -0500

    Implement second discussion solution in minimum_changes_to_make_alternating_binary_string.js

--- arrays_and_strings/minimum_changes_to_make_alternating_binary_string.js
+++ arrays_and_strings/minimum_changes_to_make_alternating_binary_string.js
@@ -34,3 +34,31 @@ var minOperationsDS1 = function(s) {
 
     return Math.min(start0, start1);
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperationsDS2 = function(s) {
+    let start0 = 0;
+
+    for (let i = 0; i < s.length; i++)
+    {
+        if (i % 2 == 0)
+        {
+            if (s[i] == '1')
+            {
+                start0++;
+            }
+        }
+        else
+        {
+            if (s[i] == '0')
+            {
+                start0++;
+            }
+        }
+    }
+
+    return Math.min(start0, s.length - start0);
+};
