4ca511ac12bc7f355cf73bb42bd398cb0d1658f2
Jeffry <jeffryklew@gmail.com>
2023-12-23 22:27:45 -0500

    Implement BFS discussion solution in numbers_with_same_consecutive_differences.py

--- backtracking/numbers_with_same_consecutive_differences.py
+++ backtracking/numbers_with_same_consecutive_differences.py
@@ -22,3 +22,29 @@ def numsSameConsecDiffDS1(n: int, k: int) -> list[int]:
         dfs(n - 1, num)
 
     return list(ans)
+
+
+def numsSameConsecDiffDS2(n: int, k: int) -> list[int]:
+    if n == 1:
+        return [i for i in range(10)]
+
+    # Initialize the queue with candidates for the first level
+    queue = [digit for digit in range(1, 10)]
+
+    for level in range(n - 1):
+        next_queue = []
+
+        for num in queue:
+            tail_digit = num % 10
+
+            # Use set() to avoid duplicates when k == 0
+            next_digits = set([tail_digit + k, tail_digit - k])
+
+            for next_digit in next_digits:
+                if 0 <= next_digit < 10:
+                    next_queue.append(num * 10 + next_digit)
+
+        # Start the next level
+        queue = next_queue
+
+    return queue
