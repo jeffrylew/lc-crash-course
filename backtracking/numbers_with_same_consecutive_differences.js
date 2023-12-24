0efb689d314b2b98b4ec3fade31842bbb567c06c
Jeffry <jeffryklew@gmail.com>
2023-12-23 21:29:16 -0500

    Implement BFS discussion solution in numbers_with_same_consecutive_differences.js

--- backtracking/numbers_with_same_consecutive_differences.js
+++ backtracking/numbers_with_same_consecutive_differences.js
@@ -41,3 +41,47 @@ var numsSameConsecDiffDS1 = function(n, k) {
 
     return results;
 };
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[]}
+ */
+var numsSameConsecDiffDS2 = function(n, k) {
+    if (n == 1)
+    {
+        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    }
+
+    let queue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for (let level = 1; level < n; level++)
+    {
+        let next_queue = [];
+
+        // Iterate through each number within the level
+        for (const num of queue)
+        {
+            const tail_digit = num % 10;
+
+            let next_digits = [tail_digit + k];
+            if (k != 0)
+            {
+                next_digits.push(tail_digit - k);
+            }
+
+            for (const next_digit of next_digits)
+            {
+                if (0 <= next_digit && next_digit < 10)
+                {
+                    next_queue.push(num * 10 + next_digit);
+                }
+            }
+        }
+
+        // Prepare for the next level
+        queue = next_queue;
+    }
+
+    return queue;
+}
