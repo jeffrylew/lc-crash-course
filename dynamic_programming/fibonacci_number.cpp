38d9ef6325ff9acac95038d123234bbef5d94b5a
Jeffry <jeffryklew@gmail.com>
2023-12-25 14:10:18 -0500

    Implement dynamic programming fibonacci in fibonacci_number.cpp

--- dynamic_programming/fibonacci_number.cpp
+++ dynamic_programming/fibonacci_number.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <functional>
+#include <unordered_map>
+
 static int fibonacci1(int n)
 {
     //! @details Time complexity O(2 ^ n) since each call creates 2 more calls
@@ -18,12 +21,43 @@ static int fibonacci1(int n)
     return fibonacci1(n - 1) + fibonacci1(n - 2);
 }
 
+static int fibonacci2(int n)
+{
+    //! @details Time complexity O(n) by memoizing results
+    //!          Space complexity O(1)
+
+    std::unordered_map<int, int> memo {};
+
+    std::function<int(int)> fibonacci = [&](int num) -> int {
+        if (num == 0)
+        {
+            return 0;
+        }
+
+        if (num == 1)
+        {
+            return 1;
+        }
+
+        if (memo.find(num) != memo.end())
+        {
+            return memo[num];
+        }
+
+        return memo[num] = fibonacci(num - 1) + fibonacci(num - 2);
+    };
+
+    return fibonacci(n);
+}
+
 TEST(FibonacciTest, SampleTest1)
 {
     EXPECT_EQ(0, fibonacci1(0));
+    EXPECT_EQ(0, fibonacci2(0));
 }
 
 TEST(FibonacciTest, SampleTest2)
 {
     EXPECT_EQ(1, fibonacci1(1));
+    EXPECT_EQ(1, fibonacci2(1));
 }
