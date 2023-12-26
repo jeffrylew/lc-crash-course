4469a85a2544ae1084be5b6f67fb810491f089c6
Jeffry <jeffryklew@gmail.com>
2023-12-25 20:08:12 -0500

    Implement dynamic programming fibonacci in fibonacci_number.js

--- dynamic_programming/fibonacci_number.js
+++ dynamic_programming/fibonacci_number.js
@@ -15,3 +15,29 @@ var fibonacci1 = function(n) {
 
     return fibonacci1(n - 1) + fibonacci1(n - 2);
 };
+
+let memo = new Map();
+
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+var fibonacci2 = function(n) {
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    if (memo.has(n))
+    {
+        return memo.get(n);
+    }
+
+    memo.set(n, fibonacci2(n - 1) + fibonacci2(n - 2));
+    return memo.get(n);
+};
