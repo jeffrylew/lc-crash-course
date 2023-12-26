12ffae84536dd051a8d8d010178ba4e808cf27b2
Jeffry <jeffryklew@gmail.com>
2023-12-25 20:14:05 -0500

    Implement dynamic programming fibonacci in fibonacci_number.py

--- dynamic_programming/fibonacci_number.py
+++ dynamic_programming/fibonacci_number.py
@@ -6,3 +6,20 @@ def fibonacci1(n: int) -> int:
         return 1
 
     return fibonacci1(n - 1) + fibonacci1(n - 2)
+
+
+memo = {}
+
+
+def fibonacci2(n: int) -> int:
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fibonacci2(n - 1) + fibonacci2(n - 2)
+    return memo[n]
