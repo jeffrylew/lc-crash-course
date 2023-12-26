9b7f3584c3b1d04e8da42920464c8241ea3e4713
Jeffry <jeffryklew@gmail.com>
2023-12-25 23:24:10 -0500

    Implement bottom up dynamic programming solution in min_cost_climbing_stairs.java

--- dynamic_programming/min_cost_climbing_stairs.java
+++ dynamic_programming/min_cost_climbing_stairs.java
@@ -28,3 +28,23 @@ class Solution_TopDown
         return dp(cost.length, cost);
     }
 }
+
+public int minCostClimbingStairsBottomUp(int[] cost)
+{
+    int n = cost.length;
+
+    // Step 2
+    int[] dp = new int[n + 1];
+
+    // Step 3: Base case(s) are implicitly defined as they are 0
+
+    // Step 4
+    for (int i = 2; i <= n; i++)
+    {
+        // Step 5
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    // Step 6
+    return dp[n];
+}
