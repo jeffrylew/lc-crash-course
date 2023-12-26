602680ee9a33f96fb84c3362eea815084a3c2879
Jeffry <jeffryklew@gmail.com>
2023-12-25 19:59:05 -0500

    Implement dynamic programming fibonacci in fibonacci_number.java

--- dynamic_programming/fibonacci_number.java
+++ dynamic_programming/fibonacci_number.java
@@ -12,3 +12,26 @@ public int fibonacci1(int n)
 
     return fibonacci1(n - 1) + fibonacci1(n - 2);
 }
+
+Map<Integer, Integer> memo = new HashMap<>();
+
+public int fibonacci2(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    if (memo.containsKey(n))
+    {
+        return memo.get(n);
+    }
+
+    memo.put(n, fibonacci2(n - 1) + fibonacci2(n - 2));
+    return memo.get(n);
+}
