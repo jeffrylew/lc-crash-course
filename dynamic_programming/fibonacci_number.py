ae6feb01097129bb54c5291ac78ed5328c2a66ae
Jeffry <jeffryklew@gmail.com>
2023-12-25 20:16:14 -0500

    Implement bottom up fibonacci in fibonacci_number.py

--- dynamic_programming/fibonacci_number.py
+++ dynamic_programming/fibonacci_number.py
@@ -23,3 +23,15 @@ def fibonacci2(n: int) -> int:
 
     memo[n] = fibonacci2(n - 1) + fibonacci2(n - 2)
     return memo[n]
+
+
+def fibonacci3(n: int) -> int:
+    arr = [0] * (n + 1)
+
+    # Base case - the second Fibonacci number is 1
+    arr[1] = 1
+
+    for i in range(2, n + 1):
+        arr[i] = arr[i - 1] + arr[i - 2]
+
+    return arr[n]
