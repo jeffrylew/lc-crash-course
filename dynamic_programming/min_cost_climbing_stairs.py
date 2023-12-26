9b5bbe8f4214fcd256d27dcfe2d4ea4bc1c129a5
Jeffry <jeffryklew@gmail.com>
2023-12-26 11:39:26 -0500

    Implement bottom up dynamic programming solution in min_cost_climbing_stairs.py

--- dynamic_programming/min_cost_climbing_stairs.py
+++ dynamic_programming/min_cost_climbing_stairs.py
@@ -14,3 +14,20 @@ def minCostClimbingStairsTopDown(cost: list[int]) -> int:
 
     memo = {}
     return dp(len(cost))
+
+
+def minCostClimbingStairsBottomUp(cost: list[int]) -> int:
+    n = len(cost)
+
+    # Step 2
+    dp = [0] * (n + 1)
+
+    # Step 3: Base case(s) are implicitly defined as they are 0
+
+    # Step 4
+    for i in range(2, n + 1):
+        # Step 5
+        dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+
+    # Step 6
+    return dp[n]
