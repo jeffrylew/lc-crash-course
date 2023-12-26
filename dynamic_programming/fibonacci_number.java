58955eef574fe8ed04ee15ba621a97c70fc6bd7b
Jeffry <jeffryklew@gmail.com>
2023-12-25 19:55:55 -0500

    Implement recursive fibonacci in fibonacci_number.java

---
+++ dynamic_programming/fibonacci_number.java
@@ -0,0 +1,14 @@
+public int fibonacci1(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return fibonacci1(n - 1) + fibonacci1(n - 2);
+}
