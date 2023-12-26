12e1943dbe4ae34f56e8715a21e05d13dda7e946
Jeffry <jeffryklew@gmail.com>
2023-12-25 23:36:41 -0500

    Implement bottom up dynamic programming solution in min_cost_climbing_stairs.js

--- dynamic_programming/min_cost_climbing_stairs.js
+++ dynamic_programming/min_cost_climbing_stairs.js
@@ -23,3 +23,22 @@ var minCostClimbingStairsTopDown = function(cost) {
     let memo = new Map();
     return dp(cost.length);
 };
+
+var minCostClimbingStairsBottomUp = function(cost) {
+    const n = cost.length;
+
+    // Step 2
+    let dp = new Array(n + 1).fill(0);
+
+    // Step 3: Base case(s) are implicitly defined as they are 0
+
+    // Step 4
+    for (let i = 2; i <= n; i++)
+    {
+        // Step 5
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    // Step 6
+    return dp[n];
+};
