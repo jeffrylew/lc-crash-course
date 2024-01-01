20c03cd020237cecf5322155d209decb271accdc
Jeffry <jeffryklew@gmail.com>
2024-01-01 15:32:58 -0500

    Implement bottom up dynamic programming solution in house_robber.js

--- dynamic_programming/house_robber.js
+++ dynamic_programming/house_robber.js
@@ -1,3 +1,7 @@
+/**
+ * @param {number[]} nums
+ * @returns {number}
+ */
 var robTopDown = function(nums) {
     const dp = i => {
         // Base cases
@@ -29,4 +33,31 @@ var robTopDown = function(nums) {
 
     let memo = new Array(nums.length).fill(-1);
     return dp(nums.length - 1);
+};
+
+/**
+ * @param {number[]} nums
+ * @returns {number}
+ */
+var robBottomUp = function(nums) {
+    const nums_length = nums.length;
+
+    if (nums_length == 1)
+    {
+        // Prevent out-of-bounds error
+        return nums[0];
+    }
+
+    let dp = new Array(nums_length).fill(0);
+
+    // Base cases
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < nums_length; i++)
+    {
+        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+
+    return dp[nums_length - 1];
 };
