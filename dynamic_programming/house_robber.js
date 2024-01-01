f4a1d6215fe8f77b18cd3e751c4e24a42421f521
Jeffry <jeffryklew@gmail.com>
2024-01-01 00:10:14 -0500

    Implement top down dynamic programming solution in house_robber.js

---
+++ dynamic_programming/house_robber.js
@@ -0,0 +1,32 @@
+var robTopDown = function(nums) {
+    const dp = i => {
+        // Base cases
+        if (i == 0)
+        {
+            return nums[0];
+        }
+
+        if (i == 1)
+        {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        if (memo[i] != -1)
+        {
+            return memo[i];
+        }
+
+        // Recurrence relation
+        memo[i] = Math.max(dp(i - 1), dp(i - 2) + nums[i]);
+        return memo[i];
+    }
+
+    if (nums.length == 1)
+    {
+        // Prevent out of bounds error
+        return nums[0];
+    }
+
+    let memo = new Array(nums.length).fill(-1);
+    return dp(nums.length - 1);
+};
