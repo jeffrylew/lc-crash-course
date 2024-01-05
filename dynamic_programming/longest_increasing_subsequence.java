87795a146bd56c23a8cf91a8a49a2eee67d85e50
Jeffry <jeffryklew@gmail.com>
2024-01-05 15:08:58 -0500

    Implement bottom up dynamic programming solution in longest_increasing_subsequence.java

--- dynamic_programming/longest_increasing_subsequence.java
+++ dynamic_programming/longest_increasing_subsequence.java
@@ -36,3 +36,22 @@ class SolutionTopDown
         return ans;
     }
 }
+
+public int lengthOfLISBottomUp(int[] nums)
+{
+    int nums_length = nums.length;
+    int[] dp = new int[n];
+    Arrays.fill(dp, 1);
+
+    int ans = 1;
+    for (int i = 0; i < nums_length; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+            ans = Math.max(ans, dp[i]);
+        }
+    }
+
+    return ans;
+}
