95cd29224a67f65243969da2d21596fe5054befe
Jeffry <jeffryklew@gmail.com>
2024-01-07 21:57:28 -0500

    Implement top down dynamic programming solution in solving_questions_with_brainpower.cpp

---
+++ dynamic_programming/solving_questions_with_brainpower.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+//! @brief Solve i to earn points[i] but skip next brainpower[i] or skip i
+//! @param[in] questions Reference to 2D vector of {points[i], brainpower[i]}
+//! @return Max points can score
+static long long mostPointsTopDown(
+    const std::vector<std::vector<int>>& questions)
+{
+    //! @details https://leetcode.com/problems/solving-questions-with-brainpower
+    //!
+    //!          Time complexity O(n) where n = questions.size(). There are O(n)
+    //!          states and each state costs O(1) to compute.
+    //!          Space complexity O(n).
+
+    std::vector<long long> memo(questions.size());
+
+    //! Returns max score, index i indicates current question we are on
+    std::function<long long(int, std::vector<long long>&)> dp =
+        [&](int i, std::vector<long long>& memo) {
+            //! Base case at end since moving forward through array
+            if (i >= static_cast<int>(questions.size()))
+            {
+                //! When exam is over, cannot score any more points
+                return 0;
+            }
+
+            if (memo[i] != 0)
+            {
+                return memo[i];
+            }
+
+            const int next_idx {i + questions[i][1] + 1};
+
+            //! Two options
+            //! - Solve question and gain questions[i][0] points. Next question
+            //!   can solve is at index (i + questions[i][1] + 1)
+            //! - Skip question so can move to next question. Score is dp(i + 1)
+            return memo[i] =
+                std::max(questions[i][0] + dp(next_idx, memo), dp(i + 1, memo));
+        };
+
+    return dp(0, memo);
+
+} // static long long mostPointsTopDown( ...
+
+TEST(MostPointsTest, SampleTest1)
+{
+    const std::vector<std::vector<int>> questions {
+        {3, 2}, {4, 3}, {4, 4}, {2, 5}};
+
+    EXPECT_EQ(5, mostPointsTopDown(questions));
+}
+
+TEST(MostPointsTest, SampleTest2)
+{
+    const std::vector<std::vector<int>> questions {
+        {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}};
+
+    EXPECT_EQ(7, mostPointsTopDown(questions));
+}
