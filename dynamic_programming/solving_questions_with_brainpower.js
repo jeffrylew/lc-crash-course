8cd324d46bfe7cd6883fbe458f18ea65651be16a
Jeffry <jeffryklew@gmail.com>
2024-01-11 18:09:36 -0500

    Implement bottom up dynamic programming solution in solving_questions_with_brainpower.js

--- dynamic_programming/solving_questions_with_brainpower.js
+++ dynamic_programming/solving_questions_with_brainpower.js
@@ -22,3 +22,24 @@ var mostPointsTopDown = function(questions) {
     let memo = new Array(questions.length).fill(-1);
     return dp(0);
 };
+
+/**
+ * @param {number[][]} questions
+ * @return {number}
+ */
+var mostPointsBottomUp = function(questions) {
+    const questions_length = questions.length;
+
+    // + 1 to avoid out of bounds
+    let dp = new Array(questions_length + 1).fill(0);
+
+    for (let i = questions_length - 1; i >= 0; i--)
+    {
+        // Need to make sure we don't go out of bounds
+        const next_index = Math.min(i + questions[i][1] + 1, questions_length);
+
+        dp[i] = Math.max(questions[i][0] + dp[next_index], dp[i + 1]);
+    }
+
+    return dp[0];
+};
