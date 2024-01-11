236f87284465fa10188e65776e743a345c877af8
Jeffry <jeffryklew@gmail.com>
2024-01-10 19:10:33 -0500

    Implement bottom up dynamic programming solution in solving_questions_with_brainpower.java

--- dynamic_programming/solving_questions_with_brainpower.java
+++ dynamic_programming/solving_questions_with_brainpower.java
@@ -27,3 +27,23 @@ class SolutionTopDown
         return dp(0, questions);
     }
 }
+
+public long mostPointsBottomUp(int[][] questions)
+{
+    int questions_length = questions.length;
+
+    // + 1 to avoid out of bounds
+    long[] dp = new long[questions_length + 1];
+
+    for (int i = questions_length - 1; i >= 0; i--)
+    {
+        int next_idx = i + questions[i][1] + 1;
+
+        // Need to make sure we don't go out of bounds
+        dp[i] =
+            Math.max(questions[i][0] + dp[Math.min(next_idx, questions_length)],
+                     dp[i + 1]);
+    }
+
+    return dp[0];
+}
