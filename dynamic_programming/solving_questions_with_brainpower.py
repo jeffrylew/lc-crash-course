f87ff9d2c7c722bd0fe793a39674c288bb868a13
Jeffry <jeffryklew@gmail.com>
2024-01-12 20:18:37 -0500

    Implement bottom up dynamic programming solution in solving_questions_with_brainpower.py

--- dynamic_programming/solving_questions_with_brainpower.py
+++ dynamic_programming/solving_questions_with_brainpower.py
@@ -11,3 +11,18 @@ def mostPointsTopDown(questions: list[list[int]]) -> int:
         return max(questions[i][0] + dp(next_index), dp(i + 1))
 
     return dp(0)
+
+
+def mostPointsBottomUp(questions: list[list[int]]) -> int:
+    questions_len = len(questions)
+
+    # + 1 to avoid out of bounds
+    dp = [0] * (questions_len + 1)
+
+    for i in range(questions_len - 1, -1, -1):
+        # Need to make sure we don't go out of bounds
+        next_index = min(i + questions[i][1] + 1, questions_len)
+
+        dp[i] = max(questions[i][0] + dp[next_index], dp[i + 1])
+
+    return dp[0]
