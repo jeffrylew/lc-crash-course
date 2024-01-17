ae3dbc795624d197083d2321533611f651ec9905
Jeffry <jeffryklew@gmail.com>
2024-01-16 20:29:48 -0500

    Implement bottom up dynamic programming solution in climbing_stairs.cpp

--- dynamic_programming/climbing_stairs.cpp
+++ dynamic_programming/climbing_stairs.cpp
@@ -105,11 +105,39 @@ static int climbStairsDS2(int n)
     return dp(0);
 }
 
+//! @brief Bottom up dynamic programming solution
+//! @param[in] n Number of steps to reach the top, can climb 1 or 2 steps
+//! @return Number of distinct ways can climb to the top
+static int climbStairsDS3(int n)
+{
+    //! @details https://leetcode.com/problems/climbing-stairs/editorial/
+    //!
+    //!          Time complexity O(n), single loop can go up to n.
+    //!          Space complexity O(n), dp vector of size n is used.
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    std::vector<int> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; ++i)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
 TEST(ClimbStairsTest, SampleTest1)
 {
     EXPECT_EQ(2, climbStairsFA(2));
     EXPECT_EQ(2, climbStairsDS1(2));
     EXPECT_EQ(2, climbStairsDS2(2));
+    EXPECT_EQ(2, climbStairsDS3(2));
 }
 
 TEST(climbStairsTest, SampleTest2)
@@ -117,6 +145,7 @@ TEST(climbStairsTest, SampleTest2)
     EXPECT_EQ(3, climbStairsFA(3));
     EXPECT_EQ(3, climbStairsDS1(3));
     EXPECT_EQ(3, climbStairsDS2(3));
+    EXPECT_EQ(3, climbStairsDS3(3));
 }
 
 TEST(ClimbStairsTest, SampleTest3)
@@ -125,6 +154,7 @@ TEST(ClimbStairsTest, SampleTest3)
     // EXPECT_EQ(7, climbStairsFA(5));
     EXPECT_EQ(8, climbStairsDS1(5));
     EXPECT_EQ(8, climbStairsDS2(5));
+    EXPECT_EQ(8, climbStairsDS3(5));
 }
 
 TEST(ClimbStairsTest, SampleTest4)
@@ -133,4 +163,5 @@ TEST(ClimbStairsTest, SampleTest4)
     EXPECT_EQ(14, climbStairsFA(6));
     EXPECT_EQ(13, climbStairsDS1(6));
     EXPECT_EQ(13, climbStairsDS2(6));
+    EXPECT_EQ(13, climbStairsDS3(6));
 }
