a0d1132fc52bd0b3b3f59c1b9b05710f20c16ca5
Jeffry <jeffryklew@gmail.com>
2024-01-21 14:27:16 -0500

    Implement bottom up dynamic programming solution in climbing_stairs.java

--- dynamic_programming/climbing_stairs.java
+++ dynamic_programming/climbing_stairs.java
@@ -52,3 +52,25 @@ public class SolutionDS2
         return climb_Stairs(0, n, memo);
     }
 }
+
+public class SolutionDS3
+{
+    public int climbStairs(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        int dp[] = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int curr_step = 3; curr_step <= n; curr_step++)
+        {
+            dp[curr_step] = dp[curr_step - 1] + dp[curr_step - 2];
+        }
+
+        return dp[n];
+    }
+}
