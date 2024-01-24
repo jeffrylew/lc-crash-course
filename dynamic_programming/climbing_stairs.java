2837b9fdb512b429780ce387bc84de97562ac810
Jeffry <jeffryklew@gmail.com>
2024-01-23 19:22:40 -0500

    Implement Binet's Formula solution in climbing_stairs.java

--- dynamic_programming/climbing_stairs.java
+++ dynamic_programming/climbing_stairs.java
@@ -96,3 +96,14 @@ public class SolutionDS4
         return second;
     }
 }
+
+public class SolutionDS5
+{
+    public int climbStairs(int n)
+    {
+        double sqrt = Math.sqrt(5);
+        double phi = (1 + sqrt5) / 2;
+        double psi = (1 - sqrt5) / 2;
+        return (int) ((Math.pow(phi, n + 1) - Math.pow(psi, n + 1)) / sqrt5);
+    }
+}
