e231451e43be87ffaf48806cb4df3b430ec14be9
Jeffry <jeffryklew@gmail.com>
2024-01-25 18:17:32 -0500

    Implement recursion with memoization solution in climbing_stairs.js

--- dynamic_programming/climbing_stairs.js
+++ dynamic_programming/climbing_stairs.js
@@ -19,3 +19,34 @@ var climbStairsDS1 = function(n) {
 
     return climb_Stairs(0);
 };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairsDS2 = function(n) {
+    let memo = new Array(n + 1).fill(0);
+
+    const climb_Stairs = curr_step => {
+        if (curr_step > n)
+        {
+            return 0;
+        }
+
+        if (curr_step == n)
+        {
+            return 1;
+        }
+
+        if (memo[curr_step] > 0)
+        {
+            return memo[curr_step];
+        }
+
+        memo[curr_step] =
+            climb_Stairs(curr_step + 1) + climb_Stairs(curr_step + 2);
+        return memo[curr_step];
+    }
+
+    return climb_Stairs(0);
+};
