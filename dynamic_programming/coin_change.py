7771eac33655dde85c5e00a4ec07e06aab02b9dc
Jeffry <jeffryklew@gmail.com>
2024-02-04 13:13:29 -0500

    Implement bottom up dynamic programming solution coin_change.py

--- dynamic_programming/coin_change.py
+++ dynamic_programming/coin_change.py
@@ -44,3 +44,19 @@ def coinChangeDS2(coins: list[int], amount: int) -> int:
         return -1 if min_count == (amount + 1) else min_count
 
     return get_min_coins(amount)
+
+
+def coinChangeDS3(coins: list[int], amount: int) -> int:
+    dp = [amount + 1] * (amount + 1)
+    dp[0] = 0
+
+    for remaining_amt in range(1, amount + 1):
+        for coin in coins:
+            if remaining_amt - coin < 0:
+                continue
+
+            dp[remaining_amt] = min(
+                dp[remaining_amt], dp[remaining_amt - coin] + 1
+            )
+
+    return -1 if dp[amount] == (amount + 1) else dp[amount]
