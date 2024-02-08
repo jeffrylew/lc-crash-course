ef767bc8dc78aefd2d86881c56e0ed358aede138
Jeffry <jeffryklew@gmail.com>
2024-02-07 20:40:55 -0500

    Implement bottom up dynamic programming solution in longest_common_subsequence.js

--- dynamic_programming/longest_common_subsequence.js
+++ dynamic_programming/longest_common_subsequence.js
@@ -1,3 +1,8 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @returns {number}
+ */
 var longestCommonSubsequenceDS1 = function(text1, text2) {
     const get_lcs = (idx1, idx2) => {
         if (idx1 == text1.length || idx2 == text2.length)
@@ -32,4 +37,38 @@ var longestCommonSubsequenceDS1 = function(text1, text2) {
     }
 
     return get_lcs(0, 0);
+};
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @returns {number}
+ */
+var longestCommonSubsequenceDS2 = function(text1, text2) {
+    const text1_len = text1.length;
+    const text2_len = text2.length;
+
+    let lcs = [];
+    for (let idx1 = 0; idx1 <= text1_len; idx1++)
+    {
+        lcs.push(new Array(text2_len + 1).fill(0));
+    }
+
+    for (let idx1 = text1_len - 1; idx1 >= 0; idx1--)
+    {
+        for (let idx2 = text2_len - 1; idx2 >= 0; idx2--)
+        {
+            if (text1[idx1] == text2[idx2])
+            {
+                lcs[idx1][idx2] = 1 + lcs[idx1 + 1][idx2 + 1];
+            }
+            else
+            {
+                lcs[idx1][idx2] =
+                    Math.max(lcs[idx1 + 1][idx2], lcs[idx1][idx2 + 1]);
+            }
+        }
+    }
+
+    return lcs[0][0];
 };
