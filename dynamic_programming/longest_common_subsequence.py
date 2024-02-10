71a078be4a1f9bf1d1e288aa8d3b2fdbc204a816
Jeffry <jeffryklew@gmail.com>
2024-02-09 21:06:52 -0500

    Implement bottom up dynamic programming solution in longest_common_subsequence.py

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -13,3 +13,18 @@ def longestCommonSubsequenceDS1(text1: str, text2: str) -> int:
         return max(get_lcs(idx1 + 1, idx2), get_lcs(idx1, idx2 + 1))
 
     return get_lcs(0, 0)
+
+
+def longestCommonSubsequenceDS2(text1: str, text2: str) -> int:
+    text1_len = len(text1)
+    text2_len = len(text2)
+    lcs = [[0] * (text2_len + 1) for _ in range(text1_len + 1)]
+
+    for idx1 in range(text1_len - 1, -1, -1):
+        for idx2 in range(text2_len - 1, -1, -1):
+            if text1[idx1] == text2[idx2]:
+                lcs[idx1][idx2] = 1 + lcs[idx1 + 1][idx2 + 1]
+            else:
+                lcs[idx1][idx2] = max(lcs[idx1 + 1][idx2], lcs[idx1][idx2 + 1])
+
+    return lcs[0][0]
