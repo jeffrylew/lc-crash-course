71219edc04a6473746d8c34c5bade981a29d14c8
Jeffry <jeffryklew@gmail.com>
2024-02-12 20:10:14 -0500

    Implement top down dynamic programming solution in best_time_to_buy_and_sell_stock_iv.java

---
+++ dynamic_programming/best_time_to_buy_and_sell_stock_iv.java
@@ -0,0 +1,52 @@
+class SolutionDS1
+{
+    int num_days;
+    int[][][] memo;
+    int[] prices;
+
+    private int get_max_profit(int curr_day, int holding_stock, int remain_tx)
+    {
+        if (curr_day == num_days || remain_tx == 0)
+        {
+            return 0;
+        }
+
+        if (memo[curr_day][holding_stock][remain_tx] != -1)
+        {
+            return memo[curr_day][holding_stock][remain_tx];
+        }
+
+        int max_profit = get_max_profit(curr_day + 1, holding_stock, remain_tx);
+        if (holding_stock == 1)
+        {
+            max_profit = Math.max(max_profit,
+                                  get_max_profit(curr_day + 1, 0, remain_tx - 1)
+                                  + prices[curr_day]);
+        }
+        else
+        {
+            max_profit = Math.max(max_profit,
+                                  get_max_profit(curr_day + 1, 1, remain_tx)
+                                  - prices[curr_day]);
+        }
+
+        memo[curr_day][holding_stock][remain_tx] = max_profit;
+        return max_profit;
+    }
+
+    public int maxProfit(int k, int[] prices)
+    {
+        num_days = prices.length;
+        memo = new int[num_days][2][k + 1];
+        for (int curr_day = 0; curr_day < num_days; curr_day++)
+        {
+            for (int holding_idx = 0; holding_idx < 2; holding_idx++)
+            {
+                Arrays.fill(memo[curr_day][holding_idx], -1);
+            }
+        }
+
+        this.prices = prices;
+        return get_max_profit(0, 0, k);
+    }
+}
