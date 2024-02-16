dd3839cc27f5df771fc845002f1bee0671d03b85
Jeffry <jeffryklew@gmail.com>
2024-02-15 21:09:16 -0500

    Implement bottom up dynamic programming solution in best_time_to_buy_and_sell_stock_iv.js

--- dynamic_programming/best_time_to_buy_and_sell_stock_iv.js
+++ dynamic_programming/best_time_to_buy_and_sell_stock_iv.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @returns {number}
+ */
 var maxProfitDS1 = function(k, prices)
 {
     const get_max_profit = (curr_day, holding_stock, remain_tx) => {
@@ -40,4 +45,49 @@ var maxProfitDS1 = function(k, prices)
     }
 
     return get_max_profit(0, 0, k);
+};
+
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @returns {number}
+ */
+var maxProfitDS2 = function(k, prices) {
+    const num_days = prices.length;
+    let dp = [];
+    for (let curr_day = 0; curr_day <= num_days; curr_day++)
+    {
+        dp.push([]);
+        for (let holding_idx = 0; holding_idx < 2; holding_idx++)
+        {
+            dp[holding_idx].push(new Array(k + 1).fill(0));
+        }
+    }
+
+    for (let curr_day = num_days - 1; curr_day >= 0; curr_day--)
+    {
+        for (let remain_tx = 1; remain_tx <= k; remain_tx++)
+        {
+            for (let holding_stock = 0; holding_stock < 2; holding_stock++)
+            {
+                let max_profit = dp[curr_day + 1][holding_stock][remain_tx];
+                if (holding_stock == 1)
+                {
+                    max_profit = Math.max(max_profit,
+                                          dp[curr_day + 1][0][remain_tx - 1]
+                                          + prices[curr_day]);
+                }
+                else
+                {
+                    max_profit = Math.max(max_profit,
+                                          dp[curr_day + 1][1][remain_tx]
+                                          - prices[curr_day]);
+                }
+
+                dp[curr_day][holding_stock][remain_tx] = max_profit;
+            }
+        }
+    }
+
+    return dp[0][0][k];
 };
