fe0b5468178490552d3dd37659fbf610a37af2f6
Jeffry <jeffryklew@gmail.com>
2024-02-17 18:49:49 -0500

    Implement bottom up dynamic programming solution in best_time_to_buy_and_sell_stock_iv.py

--- dynamic_programming/best_time_to_buy_and_sell_stock_iv.py
+++ dynamic_programming/best_time_to_buy_and_sell_stock_iv.py
@@ -20,3 +20,25 @@ def maxProfitDS1(k: int, prices: list[int]) -> int:
         return max_profit
 
     return get_max_profit(0, False, k)
+
+
+def maxProfitDS2(k: int, prices: list[int]) -> int:
+    num_days = len(prices)
+    dp = [[[0] * (k + 1) for _ in range(2)] for __ in range(num_days + 1)]
+
+    for curr_day in range(num_days - 1, -1, -1):
+        for remain_tx in range(1, k + 1):
+            for holding_stock in range(2):
+                max_profit = dp[curr_day + 1][holding_stock][remain_tx]
+                if holding_stock:
+                    max_profit = max(max_profit,
+                                     dp[curr_day + 1][0][remain_tx - 1]
+                                     + prices[curr_day])
+                else:
+                    max_profit = max(max_profit,
+                                     dp[curr_day + 1][1][remain_tx]
+                                     - prices[curr_day])
+
+                dp[curr_day][holding_stock][remain_tx] = max_profit
+
+    return dp[0][0][k]
