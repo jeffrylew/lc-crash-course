7a3d405d4ab89a0bc2ce72e6fa23214a95324028
Jeffry <jeffryklew@gmail.com>
2024-02-26 20:47:05 -0500

    Implement bottom up dynamic programming solution in maximum_value_of_k_coins_from_piles.py

--- dynamic_programming/maximum_value_of_k_coins_from_piles.py
+++ dynamic_programming/maximum_value_of_k_coins_from_piles.py
@@ -21,3 +21,22 @@ def maxValueOfCoinsDS1(piles: list[list[int]], k: int) -> int:
         return max_value
 
     return get_max_value(0, k)
+
+
+def maxValueOfCoinsDS2(piles: list[list[int]], k: int) -> int:
+    num_piles = len(piles)
+    dp = [[0] * (k + 1) for _ in range(num_piles + 1)]
+
+    for curr_pile in range(num_piles - 1, -1, -1):
+        for remain_coins in range(1, k + 1):
+            # Skip this pile
+            dp[curr_pile][remain_coins] = dp[curr_pile + 1][remain_coins]
+            curr_value = 0
+            for coin in range(min(remain_coins, len(piles[curr_pile]))):
+                curr_value += piles[curr_pile][coin]
+                dp[curr_pile][remain_coins] = max(
+                    dp[curr_pile][remain_coins],
+                    dp[curr_pile + 1][remain_coins - coin - 1]
+                    + curr_value)
+
+    return dp[0][k]
