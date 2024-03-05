eb61ee4f956253f2c8451f89074c23e867a3a2a3
Jeffry <jeffryklew@gmail.com>
2024-03-04 20:09:10 -0500

    Implement dynamic programming solution in best_time_to_buy_and_sell_stock_with_tx_fee.js

---
+++ dynamic_programming/best_time_to_buy_and_sell_stock_with_tx_fee.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfitDS1 = function(prices, fee) {
+    const num_days = prices.length;
+    let free = Array(num_days).fill(0);
+    let hold = Array(num_days).fill(0);
+
+    // In order to hold a stock on day 0, we have
+    // no other choice but to buy it for prices[0]
+    hold[0] = -prices[0];
+
+    for (let curr_day = 1; curr_day < num_days; curr_day++)
+    {
+        free[curr_day] = Math.max(free[curr_day - 1],
+                                  hold[curr_day - 1] + prices[curr_day] - fee);
+        hold[curr_day] = Math.max(hold[curr_day - 1],
+                                  free[curr_day - 1] - prices[curr_day]);
+    }
+
+    return free[num_days - 1];
+};
