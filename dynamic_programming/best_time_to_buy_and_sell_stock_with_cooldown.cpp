2f4ef09f44b2ca16eb159894a48474e3e7202406
Jeffry <jeffryklew@gmail.com>
2024-03-09 22:07:05 -0500

    Implement first attempt top down dynamic programming solution in best_time_to_buy_and_sell_stock_with_cooldown.cpp

--- dynamic_programming/best_time_to_buy_and_sell_stock_with_cooldown.cpp
+++ dynamic_programming/best_time_to_buy_and_sell_stock_with_cooldown.cpp
@@ -1,11 +1,50 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <functional>
 #include <vector>
 
+//! @brief First attempt solution to find max profit with cooldown after selling
+//! @param[in] prices Vector of prices where prices[i] is price on ith day
+//! @return Max profit given a cooldown where you cannot buy the next day
 static int maxProfitFA(std::vector<int> prices)
 {
-    //! @todo
+    //! @details Space complexity O(prices.size())
+    //!          Time complexity O(prices.size())
+
+    std::vector<std::vector<int>> memo(prices.size(), std::vector<int>(2U, 0));
+
+    std::function<int(int, int)> get_max_profit =
+        [&](int curr_day, int holding_stock) -> int {
+            if (curr_day >= static_cast<int>(prices.size()))
+            {
+                return 0;
+            }
+
+            if (memo[curr_day][holding_stock] != 0)
+            {
+                return memo[curr_day][holding_stock];
+            }
+
+            int max_profit {get_max_profit(curr_day + 1, holding_stock)};
+
+            if (holding_stock == 0)
+            {
+                max_profit = std::max(max_profit,
+                                      get_max_profit(curr_day + 1, 1)
+                                      - prices[curr_day]);
+            }
+            else
+            {
+                max_profit = std::max(max_profit,
+                                      get_max_profit(curr_day + 2, 0)
+                                      + prices[curr_day]);
+            }
+
+            return memo[curr_day][holding_stock] = max_profit;
+        };
+
+    return get_max_profit(0, 0);
 }
 
 TEST(MaxProfitTest, SampleTest1)
