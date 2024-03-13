e882d12150ff26ff7cfae2b40494bddb5eacc739
Jeffry <jeffryklew@gmail.com>
2024-03-13 19:39:55 -0400

    Implement second dynamic programming discussion solution in best_time_to_buy_and_sell_stock_with_cooldown.java

--- dynamic_programming/best_time_to_buy_and_sell_stock_with_cooldown.java
+++ dynamic_programming/best_time_to_buy_and_sell_stock_with_cooldown.java
@@ -1,17 +1,44 @@
 public int maxProfitDS1(int[] prices)
 {
     int sold = Integer.MIN_VALUE;
-    int held = Integer.MIN_VALUE;
+    int hold = Integer.MIN_VALUE;
     int reset = 0;
 
     for (int price : prices)
     {
         int pre_sold = sold;
 
-        sold = held + price;
-        held = Math.max(held, reset - price);
+        sold = hold + price;
+        hold = Math.max(hold, reset - price);
         reset = Math.max(reset, pre_sold);
     }
 
     return Math.max(sold, reset);
+}
+
+public int maxProfitDS2(int[] prices)
+{
+    int[] max_profit = new int[prices.length + 2];
+
+    for (int prev_day = prices.length - 1; prev_day >= 0; prev_day--)
+    {
+        int case1 = 0;
+
+        // Case 1) Buy on prev_day and sell on curr_day for prices[curr_day]
+        for (int curr_day = prev_day + 1; curr_day < prices.length; curr_day++)
+        {
+            int profit = (prices[curr_day] - prices[prev_day])
+                         + max_profit[curr_day + 2];
+
+            case1 = Math.max(profit, case1);
+        }
+
+        // Case 2) No transaction with the stock at prices[prev_day]
+        int case2 = max_profit[prev_day + 1];
+
+        // Wrap up the two cases
+        max_profit[prev_day] = Math.max(case1, case2);
+    }
+
+    return max_profit[0];
 }
