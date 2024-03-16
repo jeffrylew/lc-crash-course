efb93075b4032b4dbbffa11387ddbd776c60eca5
Jeffry <jeffryklew@gmail.com>
2024-03-15 23:16:39 -0400

    Implement second dynamic programming discussion solution in best_time_to_buy_and_sell_stock_with_cooldown.py

--- dynamic_programming/best_time_to_buy_and_sell_stock_with_cooldown.py
+++ dynamic_programming/best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -12,3 +12,28 @@ def maxProfitDS1(prices: list[int]) -> int:
         reset = max(reset, pre_sold)
 
     return max(sold, reset)
+
+
+def maxProfitDS2(prices: list[int]) -> int:
+    num_days = len(prices)
+
+    # Pad array with additional zero to simplify the logic
+    max_profit = [0] * (num_days + 2)
+
+    for prev_day in range(num_days - 1, -1, -1):
+        case1 = 0
+
+        # Case 1) Buy and sell the stock
+        for curr_day in range(prev_day + 1, num_days):
+            profit = ((prices[curr_day] - prices[prev_day])
+                      + max_profit[curr_day + 2])
+
+            case1 = max(profit, case1)
+
+        # Case 2) No transaction with the stock at prices[prev_day]
+        case2 = max_profit[prev_day + 1]
+
+        # Wrap up the two cases
+        max_profit[prev_day] = max(case1, case2)
+
+    return max_profit[0]
