08949d6435b5ce07ef8d668420c833f89112353d
Jeffry <jeffryklew@gmail.com>
2024-03-22 20:30:49 -0400

    Implement bottom up dynamic programming solution in unique_paths.java

--- dynamic_programming/unique_paths.java
+++ dynamic_programming/unique_paths.java
@@ -41,3 +41,27 @@ class SolutionDS1
         return dp(m - 1, n - 1);
     }
 }
+
+public int uniquePathsDS2
+{
+    int dp[][] = new int[m][n];
+    dp[0][0] = 1;
+
+    for (int row = 0; row < m; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            if (row > 0)
+            {
+                dp[row][col] += dp[row - 1][col];
+            }
+
+            if (col > 0)
+            {
+                dp[row][col] += dp[row][col - 1];
+            }
+        }
+    }
+
+    return dp[m - 1][n - 1];
+}
