494c633a7f91b07a1e188a5ef41e7c5c2bce191a
Jeffry <jeffryklew@gmail.com>
2024-03-23 09:42:23 -0400

    Implement space optimized bottom up dynamic programming solution in unique_paths.java

--- dynamic_programming/unique_paths.java
+++ dynamic_programming/unique_paths.java
@@ -65,3 +65,30 @@ public int uniquePathsDS2
 
     return dp[m - 1][n - 1];
 }
+
+public int uniquePathsDS3
+{
+    int dp[] = new int[n];
+    dp[0] = 1;
+
+    for (int row = 0; row < m; row++)
+    {
+        int[] nextRow = new int[n];
+        for (int col = 0; col < n; col++)
+        {
+            //! dp[col] is like previous row, dp[row - 1][col]
+            //! nextRow[col] is dp[row][col]
+            nextRow[col] += dp[col];
+            if (col > 0)
+            {
+                //! nextRow[col - 1] is like previous col, dp[row][col - 1]
+                //! nextRow[col] is dp[row][col]
+                nextRow[col] += nextRow[col - 1];
+            }
+        }
+
+        dp = nextRow;
+    }
+
+    return dp[n - 1];
+}
