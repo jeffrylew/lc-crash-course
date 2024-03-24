a8b047ded997bcdcdfceab6d2423b44e4318f598
Jeffry <jeffryklew@gmail.com>
2024-03-24 18:34:39 -0400

    Implement bottom up dynamic programming solution in unique_paths.py

--- dynamic_programming/unique_paths.py
+++ dynamic_programming/unique_paths.py
@@ -18,3 +18,18 @@ def uniquePathsDS1(m: int, n: int) -> int:
         return num_ways
 
     return get_unique_paths(m - 1, n - 1)
+
+
+def uniquePathsDS2(m: int, n: int) -> int:
+    dp = [[0] * n for _ in range(m)]
+    dp[0][0] = 1
+
+    for row in range(m):
+        for col in range(n):
+            if row > 0:
+                dp[row][col] += dp[row - 1][col]
+
+            if col > 0:
+                dp[row][col] += dp[row][col - 1]
+
+    return dp[m - 1][n - 1]
