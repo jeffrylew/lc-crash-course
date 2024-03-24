8e2ce8f2c1ffe57b0f44cfc07a285efa598c8297
Jeffry <jeffryklew@gmail.com>
2024-03-24 18:10:07 -0400

    Implement bottom up dynamic programming solution in unique_paths.js

--- dynamic_programming/unique_paths.js
+++ dynamic_programming/unique_paths.js
@@ -40,3 +40,36 @@ var uniquePathsDS1 = function(m, n)
 
     return dp(m - 1, n - 1);
 };
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @returns {number}
+ */
+var uniquePathsDS2 = function(m, n)
+{
+    let dp = [];
+    for (let row = 0; row < m; row++)
+    {
+        dp.push(new Array(n).fill(0));
+    }
+    dp[0][0] = 1;
+
+    for (let row = 0; row < m; row++)
+    {
+        for (let col = 0; col < n; col++)
+        {
+            if (row > 0)
+            {
+                dp[row][col] += dp[row - 1][col];
+            }
+
+            if (col > 0)
+            {
+                dp[row][col] += dp[row][col - 1];
+            }
+        }
+    }
+
+    return dp[m - 1][n - 1];
+};
