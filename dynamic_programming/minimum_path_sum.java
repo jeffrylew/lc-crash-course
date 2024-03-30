3f85c34bba91c5e0f23b03a7de72609257e0c5ca
Jeffry <jeffryklew@gmail.com>
2024-03-30 15:36:14 -0400

    Implement bottom up dynamic programming solution in minimum_path_sum.java

--- dynamic_programming/minimum_path_sum.java
+++ dynamic_programming/minimum_path_sum.java
@@ -46,3 +46,38 @@ class SolutionDS1
         return dp(num_rows - 1, num_cols - 1);
     }
 }
+
+public int minPathSumDS2(int[][] grid)
+{
+    int num_rows = grid.length;
+    int num_cols = grid[0].length;
+
+    int[][] dp = new int[num_rows][num_cols];
+    dp[0][0] = grid[0][0];
+
+    for (int row = 0; row < num_rows; row++)
+    {
+        for (int col = 0; col < num_cols; col++)
+        {
+            if (row + col == 0)
+            {
+                continue;
+            }
+
+            int min_sum = Integer.MAX_VALUE;
+            if (row > 0)
+            {
+                min_sum = Math.min(min_sum, dp[row - 1][col]);
+            }
+
+            if (col > 0)
+            {
+                min_sum = Math.min(min_sum, dp[row][col - 1]);
+            }
+
+            dp[row][col] = grid[row][col] + min_sum;
+        }
+    }
+
+    return dp[num_rows - 1][num_cols - 1];
+}
