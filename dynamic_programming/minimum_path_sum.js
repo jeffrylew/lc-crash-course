86ed85e8183c25c70f0756586ad1c78a06652547
Jeffry <jeffryklew@gmail.com>
2024-04-01 19:55:46 -0400

    Implement space optimized bottom up dynamic programming solution in minimum_path_sum.js

--- dynamic_programming/minimum_path_sum.js
+++ dynamic_programming/minimum_path_sum.js
@@ -82,3 +82,36 @@ var minPathSumDS2 = function(grid) {
 
     return dp[num_rows - 1][num_cols - 1];
 };
+
+/**
+ * @param {number[][]} grid
+ * @returns {number}
+ */
+var minPathSumDS3 = function(grid) {
+    const num_rows = grid.length;
+    const num_cols = grid[0].length;
+
+    let dp = new Array(num_cols).fill(Number.MAX_SAFE_INTEGER);
+    dp[0] = 0;
+
+    for (let row = 0; row < num_rows; row++)
+    {
+        let next_row = new Array(num_cols).fill(0);
+
+        for (let col = 0; col < num_cols; col++)
+        {
+            next_row[col] = dp[col];
+
+            if (col > 0)
+            {
+                next_row[col] = Math.min(next_row[col], next_row[col - 1]);
+            }
+
+            next_row[col] += grid[row][col];
+        }
+
+        dp = next_row;
+    }
+
+    return dp[num_cols - 1];
+};
