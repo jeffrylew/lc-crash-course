eebd31e8cac833380cb42a7009758d62b525612c
Jeffry <jeffryklew@gmail.com>
2024-04-05 12:39:35 -0400

    Implement dynamic programming discussion solution in unique_paths_ii.cpp

--- dynamic_programming/unique_paths_ii.cpp
+++ dynamic_programming/unique_paths_ii.cpp
@@ -56,7 +56,74 @@ static int uniquePathsWithObstaclesFA(
     };
 
     return get_num_paths(num_rows - 1, num_cols - 1);
-}
+
+} // static int uniquePathsWithObstaclesFA( ...
+
+//! @brief Dynamic programming discussion solution
+//! @param[in] obstacleGrid Reference to m x n int grid with obstacle = 1
+//! @return Number of unique paths from top left to bottom right corner
+static int uniquePathsWithObstaclesDS(
+    const std::vector<std::vector<int>>& obstacleGrid)
+{
+    //! @details https://leetcode.com/problems/unique-paths-ii/editorial/
+    //!
+    //!          Time complexity O(m * n). The grid has size m * n and process
+    //!          each cell once.
+    //!          Space complexity O(m * n), assuming the input cannot be edited.
+    //!          If the input can be edited then can use obstacledGrid to keep
+    //!          track of the number of unique paths at each (row, col) for a
+    //!          space complexity of O(1).
+
+    //! If starting cell has an obstacle then return
+    //! since there are no paths to the destination
+    if (obstacleGrid[0][0] == 1)
+    {
+        return 0;
+    }
+
+    //! Assuming cannot modify input, create a copy
+    auto grid = obstacleGrid;
+
+    //! Number of ways of reaching the starting cell is 1
+    grid[0][0] = 1;
+
+    const auto num_rows = static_cast<int>(grid.size());
+    const auto num_cols = static_cast<int>(grid[0].size());
+
+    //! Fill values for first column
+    for (int row = 1; row < num_rows; ++row)
+    {
+        grid[row][0] = (grid[row][0] == 0 && grid[row - 1][0] == 1) ? 1 : 0;
+    }
+
+    //! Fill values for first row
+    for (int col = 1; col < num_cols; ++col)
+    {
+        grid[0][col] = (grid[0][col] == 0 && grid[0][col - 1] == 1) ? 1 : 0;
+    }
+
+    //! Fill values starting from (1, 1)
+    //! Number of ways of reaching cell[row][col] is
+    //! cell[row - 1][col] + cell[row][col - 1]
+    for (int row = 1; row < num_rows; ++row)
+    {
+        for (int col = 1; col < num_cols; ++col)
+        {
+            if (grid[row][col] == 0)
+            {
+                grid[row][col] = grid[row - 1][col] + grid[row][col - 1];
+            }
+            else
+            {
+                grid[row][col] = 0;
+            }
+        }
+    }
+
+    //! Return value stored in bottom right corner
+    return grid[num_rows - 1][num_cols - 1];
+
+} // static int uniquePathsWithObstaclesDS( ...
 
 TEST(UniquePathsWithObstaclesTest, SampleTest1)
 {
@@ -64,6 +131,7 @@ TEST(UniquePathsWithObstaclesTest, SampleTest1)
         {0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
 
     EXPECT_EQ(2, uniquePathsWithObstaclesFA(obstacleGrid));
+    EXPECT_EQ(2, uniquePathsWithObstaclesDS(obstacleGrid));
 }
 
 TEST(UniquePathsWithObstaclesTest, SampleTest2)
@@ -72,4 +140,5 @@ TEST(UniquePathsWithObstaclesTest, SampleTest2)
         {0, 1}, {0, 0}};
 
     EXPECT_EQ(1, uniquePathsWithObstaclesFA(obstacleGrid));
+    EXPECT_EQ(1, uniquePathsWithObstaclesDS(obstacleGrid));
 }
