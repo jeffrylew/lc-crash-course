43c0a0c30aa1653128bc547bae9359f6ce57eda2
Jeffry <jeffryklew@gmail.com>
2024-04-19 08:05:46 -0400

    Implement top down dynamic programming discussion solution in minimum_falling_path_sum.py

--- dynamic_programming/minimum_falling_path_sum.py
+++ dynamic_programming/minimum_falling_path_sum.py
@@ -23,7 +23,44 @@ def minFallingPathSumDS1(matrix: list[list[int]]) -> int:
         return min(left, middle, right) + matrix[row][col]
 
     minFallingSum = sys.maxsize
-    for col in range(0, num_cols):
+    for col in range(num_cols):
         minFallingSum = min(minFallingSum, findMinFallingPathSum(0, col))
 
     return minFallingSum
+
+
+def minFallingPathSumDS2(matrix: list[list[int]]) -> int:
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    memo = [[sys.maxsize for _ in range(num_cols)] for _ in range(num_rows)]
+
+    def findMinFallingPathSum(row: int, col: int) -> int:
+        # Base cases
+        if col < 0 or col == num_cols:
+            return sys.maxsize
+
+        # Check if reached last row
+        if row == num_rows - 1:
+            return matrix[row][col]
+
+        # Check if results have been calculated before
+        if memo[row][col] != sys.maxsize:
+            return memo[row][col]
+
+        # Calculate min falling path sum starting from each possible next step
+        left = findMinFallingPathSum(row + 1, col - 1)
+        middle = findMinFallingPathSum(row + 1, col)
+        right = findMinFallingPathSum(row + 1, col + 1)
+
+        minSum = min(left, middle, right) + matrix[row][col]
+        memo[row][col] = minSum
+        return minSum
+
+    minFallingSum = sys.maxsize
+
+    # Start a DFS with memoization from each cell in the top row
+    for col in range(num_cols):
+        minFallingSum = min(minFallingSum, findMinFallingPathSum(0, col))
+
+    return minFallingSum
+
