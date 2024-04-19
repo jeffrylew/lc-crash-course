d6f64ea49234591c7b8ed3ab483b202fc44e3cd5
Jeffry <jeffryklew@gmail.com>
2024-04-19 10:47:47 -0400

    Implement bottom up dynamic programming discussion solution in minimum_falling_path_sum.py

--- dynamic_programming/minimum_falling_path_sum.py
+++ dynamic_programming/minimum_falling_path_sum.py
@@ -64,3 +64,24 @@ def minFallingPathSumDS2(matrix: list[list[int]]) -> int:
 
     return minFallingSum
 
+
+def minFallingPathSumDS3(matrix: list[list[int]]) -> int:
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    dp = [[0 for _ in range(num_cols + 1)] for _ in range(num_rows + 1)]
+
+    for row in range(num_rows - 1, -1, -1):
+        for col in range(num_cols):
+            if col == 0:
+                dp[row][col] = (min(dp[row + 1][col], dp[row + 1][col + 1])
+                                + matrix[row][col])
+            elif col == num_cols - 1:
+                dp[row][col] = (min(dp[row + 1][col - 1], dp[row + 1][col])
+                                + matrix[row][col])
+            else:
+                dp[row][col] = (min(dp[row + 1][col - 1],
+                                    dp[row + 1][col],
+                                    dp[row + 1][col + 1])
+                                + matrix[row][col])
+
+    return min(dp[0][:num_cols])
