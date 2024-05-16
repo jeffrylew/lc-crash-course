70402f8485c551881c774ffa75a986d2d39bd029
Jeffry <jeffryklew@gmail.com>
2024-05-16 17:17:48 -0400

    Implement bit shift discussion solution in bonus/hamming_distance.cpp

--- bonus/hamming_distance.cpp
+++ bonus/hamming_distance.cpp
@@ -4,17 +4,37 @@
 //! @param[in] x First int
 //! @param[in] y Second int
 //! @return Hamming distance between x and y
-static int hammingDistanceFA(int x, int y)
+static int hammingDistanceDS1(int x, int y)
 {
-    //! @todo
+    //! @details https://leetcode.com/problems/hamming-distance/editorial/
+    //!
+    //!          Time complexity O(1) since int has fixed size. 32 bit integer
+    //!          would take at most 32 iterations.
+    //!          Space complexity O(1) since a constant size of memory is used
+    //!          regardless of the input.
+
+    int x_xor_y {x ^ y};
+    int hamming_distance {};
+
+    while (x_xor_y != 0)
+    {
+        if (x_xor_y % 2 == 1)
+        {
+            ++hamming_distance;
+        }
+
+        x_xor_y = x_xor_y >> 1;
+    }
+
+    return hamming_distance;
 }
 
 TEST(HammingDistanceTest, SampleTest1)
 {
-    EXPECT_EQ(2, hammingDistanceFA(1, 4));
+    EXPECT_EQ(2, hammingDistanceDS1(1, 4));
 }
 
 TEST(HammingDistanceTest, SampleTest2)
 {
-    EXPECT_EQ(1, hammingDistanceFA(3, 1));
+    EXPECT_EQ(1, hammingDistanceDS1(3, 1));
 }
