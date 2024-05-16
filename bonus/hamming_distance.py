eb6442941b5ba32368264e44dcffddf5eea82b31
Jeffry <jeffryklew@gmail.com>
2024-05-16 17:56:05 -0400

    Implement bit shift discussion solution in bonus/hamming_distance.py

--- bonus/hamming_distance.py
+++ bonus/hamming_distance.py
@@ -1,2 +1,16 @@
 def hammingDistanceDS1(x: int, y: int) -> int:
     return bin(x ^ y).count('1')
+
+
+def hammingDistanceDS2(x: int, y: int) -> int:
+    x_xor_y = x ^ y
+    hamming_distance = 0
+
+    while x_xor_y:
+        # Mask out all bits except the rightmost
+        if x_xor_y & 1:
+            hamming_distance += 1
+
+        x_xor_y = x_xor_y >> 1
+
+    return hamming_distance
