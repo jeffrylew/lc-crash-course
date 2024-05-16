8935ec324d4e8cf7cf76696ec15cc48b621f4dc6
Jeffry <jeffryklew@gmail.com>
2024-05-16 17:53:24 -0400

    Implement the Brian Kernighan discussion solution in bonus/hamming_distance.js

--- bonus/hamming_distance.js
+++ bonus/hamming_distance.js
@@ -20,3 +20,24 @@ var hammingDistanceDS1 = function(x, y)
 
     return hamming_distance;
 };
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistanceDS2 = function(x, y)
+{
+    let x_xor_y = x ^ y;
+    let hamming_distance = 0;
+
+    while (x_xor_y != 0)
+    {
+        hamming_distance++;
+
+        // Remove the rightmost bit of '1'
+        x_xor_y = x_xor_y & (x_xor_y - 1);
+    }
+
+    return hamming_distance;
+};
