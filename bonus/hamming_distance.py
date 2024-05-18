7a6b1d6426c49100e518c60012df8200c554a29a
Jeffry <jeffryklew@gmail.com>
2024-05-17 20:46:24 -0400

    Implement the Brian Kernighan discussion solution in bonus/hamming_distance.py

--- bonus/hamming_distance.py
+++ bonus/hamming_distance.py
@@ -14,3 +14,16 @@ def hammingDistanceDS2(x: int, y: int) -> int:
         x_xor_y = x_xor_y >> 1
 
     return hamming_distance
+
+
+def hammingDistanceDS3(x: int, y: int) -> int:
+    x_xor_y = x ^ y
+    hamming_distance = 0
+
+    while x_xor_y:
+        hamming_distance += 1
+
+        # Remove the rightmost bit of '1'
+        x_xor_y = x_xor_y & (x_xor_y - 1)
+
+    return hamming_distance
