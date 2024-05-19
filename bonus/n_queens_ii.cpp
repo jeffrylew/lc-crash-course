f08fc2cfdda0e985364bc28c228115bc353ec2fe
Jeffry <jeffryklew@gmail.com>
2024-05-19 15:29:53 -0400

    Implement second discussion solution in bonus/n_queens_ii.cpp

--- bonus/n_queens_ii.cpp
+++ bonus/n_queens_ii.cpp
@@ -67,7 +67,68 @@ static int totalNQueensDS1(int n)
 
 } // static int totalNQueensDS1( ...
 
+//! @brief Discussion solution but with visited location ints declared outside
+//! @param[in] n Number of queens to place on an n x n chessboard
+//! @return Number of distinct solutions to the n-queens puzzle
+static int totalNQueensDS2(int n)
+{
+    //! @details https://leetcode.com/problems/n-queens-ii/description/
+
+    //! diagonals: Top left to bottom right. Values in diagonal share row - col
+    //! antiDiagonals: Top right to bottom left. Values share row + col
+    //! cols: Stores the columns in [0, n) already occupied. One queen per col
+    int diagonals {};
+    int antiDiagonals {};
+    int cols {};
+
+    //! row: Consider one row per backtrack call. When row = n, found valid soln
+    const std::function<int(int)> backtrack = [&](int row) {
+        //! Base case: N queens have been placed
+        if (row == n)
+        {
+            return 1;
+        }
+
+        int num_solutions {};
+        for (int col = 0; col < n; ++col)
+        {
+            //! Bit masks. Add n to avoid negative bit shifting
+            const int currDiagonal {1 << (row - col + n)};
+            const int currAntiDiagonal {1 << (row + col)};
+            const int currCol {1 << col};
+
+            //! If the queen is not placeable
+            if (cols & currCol              // col in cols already occupied
+                || diagonals & currDiagonal // currDiagonal already occupied
+                || antiDiagonals & currAntiDiagonal) // Already occupied
+            {
+                continue;
+            }
+
+            //! "Add" the queen to the board
+            cols ^= currCol;
+            diagonals ^= currDiagonal;
+            antiDiagonals ^= currAntiDiagonal;
+
+            //! Move onto the next row with the updated board state
+            num_solutions += backtrack(row + 1);
+
+            //! "Remove" the queen from the board since we have already
+            //! explored all valid paths using the above function call
+            cols ^= currCol;
+            diagonals ^= currDiagonal;
+            antiDiagonals ^= currAntiDiagonal;
+        }
+
+        return num_solutions;
+    };
+
+    return backtrack(0);
+
+} // static int totalNQueensDS2( ...
+
 TEST(TotalNQueensTest, SampleTest1)
 {
     EXPECT_EQ(2, totalNQueensDS1(4));
+    EXPECT_EQ(2, totalNQueensDS2(4));
 }
