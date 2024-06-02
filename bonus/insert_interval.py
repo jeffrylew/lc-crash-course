75341448edf97adc75dc6768459793264095b523
Jeffry <jeffryklew@gmail.com>
2024-06-02 15:54:44 -0400

    Implement binary search discussion solution in bonus/insert_interval.py

--- bonus/insert_interval.py
+++ bonus/insert_interval.py
@@ -23,3 +23,38 @@ def insertDS1(
         idx += 1
 
     return res
+
+
+def insertDS2(
+    intervals: list[list[int]], newInterval: list[int]
+) -> list[list[int]]:
+    # If intervals is empty, return a list containing newInterval
+    if not intervals:
+        return [newInterval]
+
+    num_intervals = len(intervals)
+    left, right = 0, num_intervals - 1
+
+    # Binary search to find the position to insert newInterval
+    while left <= right:
+        mid = (left + right) // 2
+
+        if intervals[mid][0] < newInterval[0]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    # Insert newInterval at the found position
+    intervals.insert(left, newInterval)
+
+    # Merge overlapping intervals
+    res = []
+    for interval in intervals:
+        # If res is empty or there is no overlap, add interval to result
+        if not res or res[-1][1] < interval[0]:
+            res.append(interval)
+        # If there is overlap, update end of last interval in res to merge
+        else:
+            res[-1][1] = max(res[-1][1], interval[1])
+
+    return res
