74ddb9f9572343b4664af2a154fa1704311b1223
Jeffry <jeffryklew@gmail.com>
2024-06-19 18:36:07 -0400

    Implement Bellman Ford discussion solution in cheapest_flights_within_k_stops.java

--- bonus/cheapest_flights_within_k_stops.java
+++ bonus/cheapest_flights_within_k_stops.java
@@ -56,3 +56,36 @@ public int findCheapestPriceDS1(int     n,
 
     return costs[dst] == Integer.MAX_VALUE ? -1 : costs[dst];
 }
+
+public int findCheapestPriceDS2(int     n,
+                                int[][] flights,
+                                int     src,
+                                int     dst,
+                                int     k)
+{
+    // costs from src to all other cities
+    int[] costs = new int[n];
+    Arrays.fill(costs, Integer.MAX_VALUE);
+    costs[src] = 0;
+
+    // Run k + 1 times since want min cost in k stops
+    for (int num_flights = 0; num_flights <= k; num_flights++)
+    {
+        // Create copy of costs array
+        int[] temp = Arrays.copyOf(costs, n);
+
+        for (int[] flight : flights)
+        {
+            if (costs[flight[0]] != Integer.MAX_VALUE)
+            {
+                temp[flight[1]] = Math.min(temp[flight[1]],
+                                           costs[flight[0]] + flight[2]);
+            }
+        }
+
+        // Copy temp array into costs
+        costs = temp;
+    }
+
+    return costs[dst] == Integer.MAX_VALUE ? -1 : costs[dst];
+}
