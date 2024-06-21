614cd218e78eb952598f71fa285cd071497dd02f
Jeffry <jeffryklew@gmail.com>
2024-06-21 06:59:04 -0400

    Implement Bellman Ford discussion solution in cheapest_flights_within_k_stops.py

--- bonus/cheapest_flights_within_k_stops.py
+++ bonus/cheapest_flights_within_k_stops.py
@@ -30,3 +30,25 @@ def findCheapestPriceDS1(
         stops += 1
 
     return -1 if costs[dst] == float('inf') else costs[dst]
+
+
+def findCheapestPriceDS2(
+    n: int, flights: list[list[int]], src: int, dst: int, k: int
+) -> int:
+    # Costs from src to all other cities
+    costs = [float('inf')] * n
+    costs[src] = 0
+
+    # Run only k + 1 times since want minimum cost in k stops
+    for _ in range(k + 1):
+        # Create copy of costs array
+        temp = costs.copy()
+
+        for flight in flights:
+            if costs[flight[0]] != float('inf'):
+                temp[flight[1]] = min(temp[flight[1]],
+                                      costs[flight[0]] + flight[2])
+
+        costs = temp
+
+    return -1 if costs[dst] == float('inf') else costs[dst]
