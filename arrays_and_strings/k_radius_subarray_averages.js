02edd64135b43d4b1f80c8bba3495002d65b02a0
Jeffry <jeffryklew@gmail.com>
2024-06-26 19:14:54 -0400

    Implement sliding window discussion solution in arrays_and_strings/k_radius_subarray_averages.js

--- arrays_and_strings/k_radius_subarray_averages.js
+++ arrays_and_strings/k_radius_subarray_averages.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number[]}
+ */
 var getAveragesDS1 = function(nums, k)
 {
     // When a single element is considered then its average is the num itself
@@ -33,6 +38,50 @@ var getAveragesDS1 = function(nums, k)
 
         const subArraySum = prefix[rightBound + 1] - prefix[leftBound];
         averages[idx] = Math.floor(subArraySum / windowSize);
+    }
+
+    return averages;
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number[]}
+ */
+var getAveragesDS2 = function(nums, k)
+{
+    // When a single element is considered then its average is the num itself
+    if (k === 0)
+    {
+        return nums;
+    }
+
+    const windowSize = 2 * k + 1;
+    const nums_len = nums.length;
+    const averages = new Array(nums_len).fill(-1);
+
+    // Case when all indices do not have k elements on either side
+    if (windowSize > nums_len)
+    {
+        return averages;
+    }
+
+    // Get sum of first window in nums vector
+    let windowSum = 0;
+    for (let idx = 0; idx < windowSize; ++idx)
+    {
+        windowSum += nums[idx];
+    }
+    averages[k] = Math.floor(windowSum / windowSize);
+
+    // Iterate on rest of indices that have at least k elements on either side
+    for (let idx = windowSize; idx < nums_len; ++idx)
+    {
+        // Remove discarded element and add new element for current window sum
+        // idx is index of newly inserted element
+        // (idx - windowSize) is index of last removed element
+        windowSum = windowSum - nums[idx - windowSize] + nums[idx];
+        averages[idx - k] = Math.floor(windowSum / windowSize);
     }
 
     return averages;
