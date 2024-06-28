d0a5c55f9274e54fcdb296d76b42bbe3f2284827
Jeffry <jeffryklew@gmail.com>
2024-06-28 10:30:09 -0400

    Implement extra vector discussion solution in hashing/contiguous_array.cpp

--- hashing/contiguous_array.cpp
+++ hashing/contiguous_array.cpp
@@ -90,16 +90,60 @@ static int findMaxLengthDS1(std::vector<int> nums)
 
 } // static int findMaxLengthDS1( ...
 
+//! @brief Extra vector discussion solution
+//! @param[in] nums Vector of binary integers 0 and 1
+//! @return Max length of a contiguous subarray with an equal number of 0 and 1
+static int findMaxLengthDS2(std::vector<int> nums)
+{
+    //! @details https://leetcode.com/problems/contiguous-array/editorial/
+    //!
+    //!          Comments say this solution is hard to understand and I agree.
+    //!          Makes no sense, I won't implement this for the other languages.
+    //!          Time complexity O(N) where N = nums.size(). The complete vector
+    //!          is traversed only once.
+    //!          Space complexity O(N) for vector arr_of_counts sized 2N + 1.
+
+    const auto nums_size = static_cast<int>(nums.size());
+
+    //! Initialize with -2 except at index nums_size, which is initialized to -1
+    //! Discussion solution doesn't explain why
+    std::vector<int> arr_of_counts(2U * nums.size() + 1U, -2);
+    arr_of_counts[nums_size] = -1;
+
+    int max_len {};
+    int count {};
+
+    for (int idx = 0; idx < nums_size; ++idx)
+    {
+        count = count + (nums[idx] == 0 ? -1 : 1);
+
+        const int idx_in_2n_plus_1 {count + nums_size};
+        if (arr_of_counts[idx_in_2n_plus_1] >= -1)
+        {
+            max_len = std::max(max_len, idx - arr_of_counts[idx_in_2n_plus_1]);
+        }
+        else
+        {
+            arr_of_counts[idx_in_2n_plus_1] = idx;
+        }
+    }
+
+    return max_len;
+
+} // static int findMaxLengthDS2( ...
+
 TEST(FindMaxLengthTest, SampleTest1)
 {
     EXPECT_EQ(2, findMaxLengthFA({0, 1}));
     EXPECT_EQ(2, findMaxLengthDS1({0, 1}));
+    EXPECT_EQ(2, findMaxLengthDS2({0, 1}));
 }
 
 TEST(FindMaxLengthTest, SampleTest2)
 {
     EXPECT_EQ(2, findMaxLengthFA({0, 1, 0}));
     EXPECT_EQ(2, findMaxLengthDS1({0, 1, 0}));
+    EXPECT_EQ(2, findMaxLengthDS2({0, 1, 0}));
 }
 
 TEST(FindMaxLengthTest, SampleTest3)
@@ -109,4 +153,5 @@ TEST(FindMaxLengthTest, SampleTest3)
     EXPECT_EQ(0, findMaxLengthFA(nums));
     EXPECT_NE(6, findMaxLengthFA(nums));
     EXPECT_EQ(6, findMaxLengthDS1(nums));
+    EXPECT_EQ(6, findMaxLengthDS2(nums));
 }
