08f510b0f16ebe520200ccf56c3b73398d0c7028
Jeffry <jeffryklew@gmail.com>
2024-06-29 11:12:50 -0400

    Implement hashmap discussion solution in hashing/contiguous_array.py

--- hashing/contiguous_array.py
+++ hashing/contiguous_array.py
@@ -16,3 +16,20 @@ def findMaxLengthDS1(nums: list[int]) -> int:
                 max_len = max(max_len, end - start + 1)
 
     return max_len
+
+
+def findMaxLengthDS3(nums: list[int]) -> int:
+    count_idx = {0: -1}
+
+    max_len = 0
+    count = 0
+
+    for idx in range(len(nums)):
+        count += (-1 if nums[idx] == 0 else 1)
+
+        if count in count_idx:
+            max_len = max(max_len, idx - count_idx[count])
+        else:
+            count_idx[count] = idx
+
+    return max_len
