91ae552a6cf3af0ec8c6824d4aeb8a4557b217cf
Jeffry <jeffryklew@gmail.com>
2024-07-01 07:09:30 -0400

    Implement extra array discussion solution in hashing/contiguous_array.js

--- hashing/contiguous_array.js
+++ hashing/contiguous_array.js
@@ -36,6 +36,38 @@ var findMaxLengthDS1 = function(nums)
  * @param {number[]} nums
  * @return {number}
  */
+var findMaxLengthDS2 = function(nums)
+{
+    const nums_len = nums.length;
+
+    let arr_of_counts = new Array(2 * nums_len + 1).fill(-2);
+    arr_of_counts[nums_len] = -1;
+
+    let max_len = 0;
+    let count = 0;
+
+    for (let idx = 0; idx < nums_len; idx++)
+    {
+        count = count + (nums[idx] == 0 ? -1 : 1);
+
+        const idx_in_2n_plus_1 = count + nums_len;
+        if (arr_of_counts[idx_in_2n_plus_1] >= -1)
+        {
+            max_len = Math.max(max_len, idx - arr_of_counts[idx_in_2n_plus_1]);
+        }
+        else
+        {
+            arr_of_counts[idx_in_2n_plus_1] = idx;
+        }
+    }
+
+    return max_len;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var findMaxLengthDS3 = function(nums)
 {
     let count_idx = new Map();
