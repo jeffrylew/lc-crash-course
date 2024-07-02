f1bdfddabde57509fa25aebc8934d63fbaa52e3a
Jeffry <jeffryklew@gmail.com>
2024-07-02 19:53:01 -0400

    Implement extra array discussion solution in hashing/contiguous_array.py

--- hashing/contiguous_array.py
+++ hashing/contiguous_array.py
@@ -18,6 +18,26 @@ def findMaxLengthDS1(nums: list[int]) -> int:
     return max_len
 
 
+def findMaxLengthDS2(nums: list[int]) -> int:
+    nums_len = len(nums)
+    arr_of_counts = [-2] * (2 * nums_len + 1)
+    arr_of_counts[nums_len] = -1
+
+    max_len = 0
+    count = 0
+
+    for idx in range(nums_len):
+        count += -1 if nums[idx] == 0 else 1
+
+        idx_in_2n_plus_1 = count + nums_len
+        if arr_of_counts[idx_in_2n_plus_1] >= -1:
+            max_len = max(max_len, idx - arr_of_counts[idx_in_2n_plus_1])
+        else:
+            arr_of_counts[idx_in_2n_plus_1] = idx
+
+    return max_len
+
+
 def findMaxLengthDS3(nums: list[int]) -> int:
     count_idx = {0: -1}
 
